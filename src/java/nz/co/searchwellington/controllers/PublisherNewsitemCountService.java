ae9df8855f363f2f3c7030633d14bbd1c86aa9c2
Tony McCrae <tony@wellington.gen.nz>
2009-04-20 22:01:00 +0000

    Publisher newsitem counter hooked into the logged in user filter.

--- src/java/nz/co/searchwellington/controllers/PublisherNewsitemCountService.java
+++ src/java/nz/co/searchwellington/controllers/PublisherNewsitemCountService.java
@@ -10,11 +10,16 @@ import org.apache.log4j.Logger;
 public class PublisherNewsitemCountService {
 	
 	Logger log = Logger.getLogger(PublisherNewsitemCountService.class);
-	  
-	// TODO make count showBroken conditional based on loggedInUserFilter.
+
+	private LoggedInUserFilter loggedInFilter;
+	
+	public PublisherNewsitemCountService(LoggedInUserFilter loggedInFilter) {
+		this.loggedInFilter = loggedInFilter;
+	}
+
 	public int getNewsitemCount(Website publisher) {
 		int count = publisher.getNewsitems().size();
-		boolean showBroken = false;
+		boolean showBroken = loggedInFilter.getLoggedInUser() != null;
 		if (!showBroken && count > 0) {
 			log.info("Manually counting " + count + " ok newsitems for publisher: " + publisher.getName());
 			count = 0;
@@ -22,8 +27,7 @@ public class PublisherNewsitemCountService {
 				if (newsitem.getHttpStatus() == HttpServletResponse.SC_OK) {
 					count++;
 				}
-			}
-			
+			}			
 		}
 		return count;
 	}
