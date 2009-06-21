ce538f316bdfc9206957ab51e4d9265888c34743
Tony McCrae <tony@wellington.gen.nz>
2009-06-21 20:02:11 +0000

    Remove unused open auth spike.

--- src/java/nz/co/searchwellington/controllers/LoginController.java
+++ src/java/nz/co/searchwellington/controllers/LoginController.java
@@ -1,7 +1,5 @@
 package nz.co.searchwellington.controllers;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,15 +7,6 @@ import nz.co.searchwellington.model.User;
 import nz.co.searchwellington.repositories.UserRepository;
 
 import org.apache.log4j.Logger;
-import org.openid4java.OpenIDException;
-import org.openid4java.consumer.ConsumerException;
-import org.openid4java.consumer.ConsumerManager;
-import org.openid4java.consumer.VerificationResult;
-import org.openid4java.discovery.DiscoveryInformation;
-import org.openid4java.discovery.Identifier;
-import org.openid4java.message.AuthRequest;
-import org.openid4java.message.AuthSuccess;
-import org.openid4java.message.ParameterList;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -26,18 +15,14 @@ import org.springframework.web.servlet.view.RedirectView;
 public class LoginController extends MultiActionController {
     
     Logger log = Logger.getLogger(LoginController.class);
-  
-    public ConsumerManager manager;
-    
+      
     private UserRepository userDAO;
     private UrlStack urlStack;
 
 
-    protected LoginController(UserRepository userDAO, UrlStack urlStack) throws ConsumerException {
+    protected LoginController(UserRepository userDAO, UrlStack urlStack) {
         this.userDAO = userDAO;
-        this.urlStack = urlStack;
-        
-        manager = new ConsumerManager();
+        this.urlStack = urlStack;        
     }
    
 
@@ -81,96 +66,6 @@ public class LoginController extends MultiActionController {
     }
     
     
-    // TODO make safe all failure modes.
-    public ModelAndView auth(HttpServletRequest request, HttpServletResponse response) {        
-        ModelAndView mv = new ModelAndView();        
-        
-        String usersOpenIDUrl = request.getParameter("openid");
-                                
-        try {
-            // configure the return_to URL where your application will receive
-            // the authentication responses from the OpenID provider
-            String returnToUrl = "http://www.wellington.gen.nz/openid/verify";
-         
-            // perform discovery on the user-supplied identifier
-            log.info("Users url is: " + usersOpenIDUrl);
-            List discoveries = manager.discover(usersOpenIDUrl);
-
-            // attempt to associate with the OpenID provider
-            // and retrieve one service endpoint for authentication
-            DiscoveryInformation discovered = manager.associate(discoveries);
-
-            // store the discovery information in the user's session
-            request.getSession().setAttribute("openid-disc", discovered);
-
-            // obtain a AuthRequest message to be sent to the OpenID provider
-            AuthRequest authReq = manager.authenticate(discovered, returnToUrl);
-          
-            // Option 1: GET HTTP-redirect to the OpenID Provider endpoint
-            // The only method supported in OpenID 1.x
-            // redirect-URL usually limited ~2048 bytes
-            final String redirectionUrl = authReq.getDestinationUrl(true);
-            log.info("Redirecting to: " + redirectionUrl);
-            mv.setView(new RedirectView(redirectionUrl));
-            return mv;
-         
-        } catch (OpenIDException e) {
-            // present error to the user            
-        }
-               
-        return null;
-        
-    }
-    
-    
-    
-    public ModelAndView verify(HttpServletRequest request, HttpServletResponse response) {        
-        ModelAndView mv = new ModelAndView();
-        
-        try {
-            // extract the parameters from the authentication response
-            // (which comes in as a HTTP request from the OpenID provider)
-            ParameterList responseParameters = new ParameterList(request.getParameterMap());
-    
-            // retrieve the previously stored discovery information
-            DiscoveryInformation discovered = (DiscoveryInformation) request.getSession().getAttribute("openid-disc");
-    
-            // extract the receiving URL from the HTTP request
-            StringBuffer receivingURL = request.getRequestURL();
-            String queryString = request.getQueryString();
-            if (queryString != null && queryString.length() > 0)
-                receivingURL.append("?").append(request.getQueryString());
-    
-                // verify the response; ConsumerManager needs to be the same
-                // (static) instance used to place the authentication request
-                VerificationResult verification = manager.verify(
-                    receivingURL.toString(),
-                    responseParameters, discovered);
-    
-                // examine the verification result and extract the verified identifier
-                Identifier verified = verification.getVerifiedId();
-                if (verified != null) {
-                    AuthSuccess authSuccess = (AuthSuccess) verification.getAuthResponse();
-                    log.info("Verified correctly.");
-                    log.info("Verified indentifer: " + verified.getIdentifier());
-                    log.info("Auth success identity: " + authSuccess.getIdentity());
-                    
-                    //setRedirect(mv, request); 
-                    return mv; // success
-                }
-        } catch (Exception e) {
-            log.error(e);
-        }
-            
-        log.info("Failed to verfied.");
-        //setRedirect(mv, request); 
-        return mv;
-        }
-    
-    
-    
-    
-    @SuppressWarnings("unchecked")
     public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
         
         ModelAndView modelAndView = new ModelAndView();
