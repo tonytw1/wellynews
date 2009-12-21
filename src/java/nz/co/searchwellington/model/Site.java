9e321b9659831ae0b7091d70841f65461970778a
Tony McCrae <tony@ditonics.com>
2009-12-21 09:21:14 +0000

    Adding site to the model; needed to allow more than one copy to run in the same instance/db.

---
+++ src/java/nz/co/searchwellington/model/Site.java
@@ -0,0 +1,19 @@
+package nz.co.searchwellington.model;
+
+public class Site {
+	
+	private String areaname;
+	
+	public Site(String areaname) {	
+		this.areaname = areaname;
+	}
+	
+	public String getAreaname() {
+		return areaname;
+	}
+	
+	public void setAreaname(String areaname) {
+		this.areaname = areaname;
+	}
+		
+}
