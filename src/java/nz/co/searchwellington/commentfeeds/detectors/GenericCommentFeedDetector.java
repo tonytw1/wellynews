ba851b7ede21beb5408508761d696792b8662223
Tony McCrae <tony@ditonics.com>
2009-12-27 14:47:05 +0000

    Added auckland trains comment feed detector; moved all regex based comment detectors to generic comment feed detector.

---
+++ src/java/nz/co/searchwellington/commentfeeds/detectors/GenericCommentFeedDetector.java
@@ -0,0 +1,23 @@
+package nz.co.searchwellington.commentfeeds.detectors;
+
+public class GenericCommentFeedDetector implements CommentFeedDetector {
+	
+	private String regex;
+	
+	public GenericCommentFeedDetector() {		
+	}
+
+	
+	public boolean isValid(String url) {		
+		return url.matches(regex);				
+	}
+	
+	public String getRegex() {
+		return regex;
+	}
+
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+
+}
--- src/java/nz/co/searchwellington/commentfeeds/detectors/NewswireCommentFeedDetector.java
+++
@@ -1,9 +0,0 @@
-package nz.co.searchwellington.commentfeeds.detectors;
-
-public class NewswireCommentFeedDetector implements CommentFeedDetector {
-	
-	public boolean isValid(String url) {		
-		return url.matches("^http://www.newswire.co.nz/\\d{4}/\\d{2}/.*?/feed/$");				
-	}
-	
-}
