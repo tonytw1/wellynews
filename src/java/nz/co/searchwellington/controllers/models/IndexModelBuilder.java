f807ed8910eabe094cee0caf9812b9293eb0dc40
Tony McCrae <tony@ditonics.com>
2010-01-06 08:57:59 +0000

    Missed files.

---
+++ src/java/nz/co/searchwellington/controllers/models/IndexModelBuilder.java
@@ -0,0 +1,84 @@
+package nz.co.searchwellington.controllers.models;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import nz.co.searchwellington.controllers.RssUrlBuilder;
+import nz.co.searchwellington.model.Resource;
+import nz.co.searchwellington.repositories.ContentRetrievalService;
+import nz.co.searchwellington.utils.GoogleMapsDisplayCleaner;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+
+public class IndexModelBuilder extends AbstractModelBuilder implements ModelBuilder {
+
+	private static final int NUMBER_OF_COMMENTED_TO_SHOW = 2;
+	
+	private static Logger log = Logger.getLogger(IndexModelBuilder.class);
+	
+	ContentRetrievalService contentRetrievalService;
+	RssUrlBuilder rssUrlBuilder;
+	GoogleMapsDisplayCleaner googleMapsDisplayCleaner;
+			
+	public IndexModelBuilder(ContentRetrievalService contentRetrievalService, RssUrlBuilder rssUrlBuilder, GoogleMapsDisplayCleaner googleMapsDisplayCleaner) {
+		this.contentRetrievalService = contentRetrievalService;
+		this.rssUrlBuilder = rssUrlBuilder;
+		this.googleMapsDisplayCleaner = googleMapsDisplayCleaner;
+	}
+
+	@Override
+	public boolean isValid(HttpServletRequest request) {
+		return request.getPathInfo().matches("^/index$");
+	}
+
+	@Override
+	public ModelAndView populateContentModel(HttpServletRequest request, boolean showBroken) {
+		if (isValid(request)) {
+			log.info("Building justin page model");
+			ModelAndView mv = new ModelAndView();		
+			final List<Resource> latestNewsitems = contentRetrievalService.getLatestNewsitems(MAX_NEWSITEMS);                
+			mv.addObject("main_content", latestNewsitems);			
+			setRss(mv, rssUrlBuilder.getBaseRssTitke(), rssUrlBuilder.getBaseRssUrl());
+			return mv;
+		}
+		return null;
+	}
+
+	
+	@Override
+	public void populateExtraModelConent(HttpServletRequest request, boolean showBroken, ModelAndView mv) {		
+		populateCommentedNewsitems(mv);
+		populateSecondaryJustin(mv);
+		populateGeocoded(mv);
+		// ARCHIVE / ARCHIVE links
+	}
+
+	
+	private void populateCommentedNewsitems(ModelAndView mv) {
+		final List<Resource> recentCommentedNewsitems = contentRetrievalService.getCommentedNewsitems(MAX_NUMBER_OF_COMMENTED_TO_SHOW, 0);
+		if (recentCommentedNewsitems.size() <= NUMBER_OF_COMMENTED_TO_SHOW) {
+			mv.addObject("commented_newsitems", recentCommentedNewsitems);
+		} else {
+			mv.addObject("commented_newsitems", recentCommentedNewsitems.subList(0, NUMBER_OF_COMMENTED_TO_SHOW));
+		}
+		mv.addObject("commented_newsitems_moreurl", "comment");
+	}
+	
+	
+	private void populateGeocoded(ModelAndView mv) {
+        List<Resource> geocoded = contentRetrievalService.getGeocoded(MAX_NUMBER_OF_GEOTAGGED_TO_SHOW);
+        if (geocoded.size() > 0) {
+            mv.addObject("geocoded", googleMapsDisplayCleaner.dedupe(geocoded));
+        }
+    }
+	
+	private void populateSecondaryJustin(ModelAndView mv) {
+		mv.addObject("secondary_heading", "Just In");
+		mv.addObject("secondary_description", "New additions.");
+		mv.addObject("secondary_content", contentRetrievalService.getLatestWebsites(4));   
+		mv.addObject("secondary_content_moreurl", "justin");        
+	}
+	
+}
