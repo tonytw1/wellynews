9afe50d6668dc6e3c7669331461d8e68837ec876
Tony McCrae <tony@ditonics.com>
2010-01-20 09:36:06 +0000

    Log out the sizes of the application level ehcache; how much ram is this costing?

--- src/java/nz/co/searchwellington/controllers/admin/AdminIndexController.java
+++ src/java/nz/co/searchwellington/controllers/admin/AdminIndexController.java
@@ -5,27 +5,42 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
 import nz.co.searchwellington.controllers.BaseMultiActionController;
 import nz.co.searchwellington.repositories.keystore.KeyStore;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 
 public class AdminIndexController extends BaseMultiActionController {
 
+    private static Logger log = Logger.getLogger(AdminIndexController.class);
+	
 	private KeyStore keystore;
+	private CacheManager manager;
 
 	
-
-	public AdminIndexController(KeyStore keystore) {
+	public AdminIndexController(KeyStore keystore, CacheManager manager) {
 		this.keystore = keystore;
+		this.manager = manager;
 	}
 
 
-	public ModelAndView index(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("heading", "Admin index");
+		
 		mv.addObject("keystorecount", Long.toString(keystore.size()));
+
+		for (String cacheName : manager.getCacheNames()) {
+			Ehcache cache = manager.getEhcache(cacheName);
+			log.info("Cache memsize '" + cacheName + "':" + Long.toString(cache.getMemoryStoreSize()));
+			log.info("Cache hits '" + cacheName + "':" + Long.toString(cache.getHitCount()));
+			log.info("Cache size '" + cacheName + "':" + Long.toString(cache.getSize()));
+			log.info("Cache disksize '" + cacheName + "':" + Long.toString(cache.getDiskStoreSize()));			
+		}
+		
 		mv.setViewName("adminindex");
 		return mv;
 	}
