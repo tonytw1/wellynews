fc95f66e4b0466edf1681079ab1990fac1fe1881
Tony McCrae <tony@Arragon.local>
2010-02-11 22:39:56 +0000

    Feeds model builder.

---
+++ src/java/nz/co/searchwellington/controllers/models/FeedsModelBuilder.java
@@ -0,0 +1,46 @@
+ package nz.co.searchwellington.controllers.models;
+
+import javax.servlet.http.HttpServletRequest;
+
+import nz.co.searchwellington.repositories.ContentRetrievalService;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+
+public class FeedsModelBuilder extends AbstractModelBuilder implements ModelBuilder {
+	
+	Logger log = Logger.getLogger(FeedsModelBuilder.class);
+    	
+	private ContentRetrievalService contentRetrievalService;
+	
+	
+	public FeedsModelBuilder(ContentRetrievalService contentRetrievalService) {		
+		this.contentRetrievalService = contentRetrievalService;
+	}
+	
+
+	public boolean isValid(HttpServletRequest request) {
+		return request.getPathInfo().matches("^/feeds(/(rss|json))?$");
+	}
+
+	public ModelAndView populateContentModel(HttpServletRequest request, boolean showBroken) {
+		if (isValid(request)) {
+			log.info("Building feed page model");
+			ModelAndView mv = new ModelAndView();
+			
+			mv.addObject("heading", "Feeds");        		
+			mv.addObject("description", "Incoming feeds");
+			//mv.addObject("link", urlBuilder.getJustinUrl());
+			
+			mv.addObject("main_content", contentRetrievalService.getAllFeeds());
+			mv.setViewName("rssfeeds");
+			return mv;
+		}
+		return null;
+	}
+
+	
+	public void populateExtraModelConent(HttpServletRequest request, boolean showBroken, ModelAndView mv) {		
+	}
+      
+}
