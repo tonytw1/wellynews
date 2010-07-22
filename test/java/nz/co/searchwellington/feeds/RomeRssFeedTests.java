fcb7d7602d0f373f9a5531344b2b6518a90197f2
Tony McCrae <tony@Morden.local>
2010-07-22 18:32:06 +0100

    Bringing back another test.

--- test/java/nz/co/searchwellington/feeds/RomeRssFeedTests.java
+++ test/java/nz/co/searchwellington/feeds/RomeRssFeedTests.java
@@ -2,33 +2,43 @@ package nz.co.searchwellington.feeds;
 
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
+import nz.co.searchwellington.model.Geocode;
+import nz.co.searchwellington.model.Newsitem;
+import nz.co.searchwellington.model.NewsitemImpl;
+import nz.co.searchwellington.views.RomeRssFeed;
+import nz.co.searchwellington.views.RssItemMaker;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 
-public class RomeRssFeedTests extends TestCase {
+import com.sun.syndication.feed.synd.SyndEntry;
 
-	/*
-	public void testShouldRenderRss() throws Exception { 
-		Newsitem newsitem = new NewsitemImpl();
-		newsitem.setName("test item");
+
+public class RomeRssFeedTests extends TestCase {
 	
+	Newsitem newsitem;
+		
+	public void setUp() throws Exception {	
+		newsitem = new NewsitemImpl();
+		newsitem.setName("test item");
+		newsitem.setDescription("test");
+	}
+		
+	public void testShouldRenderRssItem() throws Exception {
 		List<Newsitem> content = new ArrayList<Newsitem>();
 		content.add(newsitem);
-		Document document = createOutputAndParseBackDocument(newsitem, content);
+		Document document = createOutputAndParseBackDocument(content);
 		
-        List items = document.selectNodes( "//rss/channel/item" );
-	    assertEquals(1, items.size());
+		List<?> items = document.selectNodes( "//rss/channel/item" );
+		assertEquals(1, items.size());
 	}
-	*/
-		
-	/*
-	public void testShouldRenderGeocodeInRss() throws Exception { 
-		Newsitem newsitem = new NewsitemImpl();
-		newsitem.setName("test item");		
+	
+	public void testShouldRenderGeocodeInRss() throws Exception {
 		Geocode geocode = new Geocode("119 Farringdon Road, London");
         geocode.setLatitude(3);
         geocode.setLongitude(45);
@@ -36,26 +46,26 @@ public class RomeRssFeedTests extends TestCase {
 		
 		List<Newsitem> content = new ArrayList<Newsitem>();
 		content.add(newsitem);
-		Document document = createOutputAndParseBackDocument(newsitem, content);
+		Document document = createOutputAndParseBackDocument(content);
 
-		List items = document.selectNodes( "//rss/channel/item/geo:lat" );
+		List<?> items = document.selectNodes( "//rss/channel/item/geo:lat" );
 	    assertEquals(1, items.size());        
         items = document.selectNodes( "//rss/channel/item/geo:long" );
         assertEquals(1, items.size());
 	}
-	*/
-
-	/*
-	private Document createOutputAndParseBackDocument(Newsitem newsitem, List<Newsitem> content) throws DocumentException {
-		List<RssFeedable> rssContent = new ArrayList<Resource>();
-		rssContent.addAll((Collection<? extends RssFeedable>) content);
+	
+	private Document createOutputAndParseBackDocument(List<Newsitem> content) throws DocumentException {
+		List<SyndEntry> rssContent = new ArrayList<SyndEntry>();		
+		RssItemMaker rssItemMaker = new RssItemMaker();	// TODO this in in the wrong place implies this should be part of RssView
+		for (Newsitem newsitem: content) {
+			rssContent.add(rssItemMaker.getNewsitemRssItem(newsitem));
+		}
 		RomeRssFeed feed = new RomeRssFeed("a", "b", "c", rssContent);
-		String output = feed.outputAsXml();
-        Document document = parse(output);
+		String xml = feed.outputAsXml();
+        Document document = parse(xml);
 		return document;
 	}
-	*/
-		
+	
 	// TODO duplicated with the google site map tests.
 	private Document parse(String xml) throws DocumentException {
 		Reader stringReader = new StringReader(xml);
