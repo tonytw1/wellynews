345a7af59c27fda81d45eadb62946176c60cf82a
Tony McCrae <tony@Morden-2.local>
2010-08-20 07:38:43 +0100

    Fixed test by setting up all the constructor injections; suggests the isValid could be it's own class?

--- test/java/nz/co/searchwellington/controllers/models/NewsitemPageModelBuilderTest.java
+++ test/java/nz/co/searchwellington/controllers/models/NewsitemPageModelBuilderTest.java
@@ -2,12 +2,33 @@ package nz.co.searchwellington.controllers.models;
 
 import junit.framework.TestCase;
 
+import nz.co.searchwellington.controllers.LoggedInUserFilter;
+import nz.co.searchwellington.repositories.ContentRetrievalService;
+import nz.co.searchwellington.repositories.HandTaggingDAO;
+import nz.co.searchwellington.tagging.TaggingReturnsOfficerService;
+import nz.co.searchwellington.widgets.TagWidgetFactory;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 public class NewsitemPageModelBuilderTest extends TestCase {
 	
+	@Mock ContentRetrievalService contentRetrievalService;
+	@Mock TaggingReturnsOfficerService taggingReturnsOfficerService;
+	@Mock TagWidgetFactory tagWidgetFactory;
+	@Mock HandTaggingDAO handTaggingDAO;
+	@Mock LoggedInUserFilter loggedInUserFilter;
+		
+	@Override
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
 	public void testIsValid() throws Exception {
-		ModelBuilder builder = new NewsitemPageModelBuilder(null);
+		ModelBuilder builder = new NewsitemPageModelBuilder(contentRetrievalService, taggingReturnsOfficerService, tagWidgetFactory, handTaggingDAO, loggedInUserFilter);
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setPathInfo("/wellington-city-council/2010/feb/01/something-about-rates");
 		assertTrue(builder.isValid(request));
