10808fba6790b2c2f4f8b24c3dae9da6c54309d0
Tony McCrae <tony@Morden.local>
2011-05-18 08:12:05 +0100

    Removing unused content retrieval service reference.

--- src/java/nz/co/searchwellington/controllers/TagController.java
+++ src/java/nz/co/searchwellington/controllers/TagController.java
@@ -2,13 +2,11 @@ package nz.co.searchwellington.controllers;
 
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import nz.co.searchwellington.controllers.models.ContentModelBuilderService;
-import nz.co.searchwellington.model.Resource;
 import nz.co.searchwellington.repositories.ContentRetrievalService;
 import nz.co.searchwellington.repositories.TagDAO;
 
@@ -18,26 +16,21 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import com.sun.syndication.io.FeedException;
 
-
 public class TagController extends MultiActionController {
 	
     static Logger log = Logger.getLogger(TagController.class);
    
     private ContentModelBuilderService contentModelBuilder;
-    private ContentRetrievalService contentRetrievalService;
     private TagDAO tagDAO;
     private UrlStack urlStack;
-
 
     public TagController(UrlStack urlStack,
 			ContentModelBuilderService contentModelBuilder,
 			ContentRetrievalService contentRetrievalService, TagDAO tagDAO) {
 		this.urlStack = urlStack;
 		this.contentModelBuilder = contentModelBuilder;
-		this.contentRetrievalService = contentRetrievalService;
 		this.tagDAO = tagDAO;
 	}
-    
     
 	public ModelAndView normal(HttpServletRequest request, HttpServletResponse response) throws IllegalArgumentException, FeedException, IOException {
         log.info("Starting normal content");                                  
@@ -55,13 +48,11 @@ public class TagController extends MultiActionController {
 		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		return null;
     }
-
 	
 	private boolean isHtmlView(ModelAndView mv) {
 		return mv.getViewName() != null;
 	}
-	
-	
+		
 	// TODO this should be in model builders
     private void addCommonModelElements(ModelAndView mv, boolean showBroken) throws IOException {
 		mv.addObject("top_level_tags", tagDAO.getTopLevelTags());      
