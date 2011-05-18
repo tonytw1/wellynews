16fb8a107d265c272228e4c6a479b9ef26984bab
Tony McCrae <tony@Morden.local>
2011-05-18 08:18:31 +0100

    Cleanup.

--- src/java/nz/co/searchwellington/model/HandTagging.java
+++ src/java/nz/co/searchwellington/model/HandTagging.java
@@ -2,24 +2,21 @@ package nz.co.searchwellington.model;
 
 public class HandTagging {
 
-	int id;
-	Resource resource;
-	User user;
-	Tag tag;
-	
-	
+	private int id;
+	private Resource resource;
+	private User user;
+	private Tag tag;
+		
 	public HandTagging() {
 	}
-
-
+	
 	public HandTagging(int id, Resource resource, User user, Tag tag) {
 		this.id = id;
 		this.resource = resource;
 		this.user = user;
 		this.tag = tag;
 	}
-
-
+	
 	public int getId() {
 		return id;
 	}
@@ -27,28 +24,29 @@ public class HandTagging {
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
-
-
-
+	
 	public Resource getResource() {
 		return resource;
 	}
+	
 	public void setResource(Resource resource) {
 		this.resource = resource;
 	}
+	
 	public User getUser() {
 		return user;
 	}
+	
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
 	public Tag getTag() {
 		return tag;
 	}
+	
 	public void setTag(Tag tag) {
 		this.tag = tag;
-	}	
+	}
 	
 }
