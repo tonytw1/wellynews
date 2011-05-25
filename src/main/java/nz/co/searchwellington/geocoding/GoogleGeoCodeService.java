c153d0d90b5ea66f0e3365b83ab4d3ef4532845d
Tony McCrae <tony>
2011-05-25 08:21:48 +0100

    Switch out geocode libraries. Provides limiting to specific regions.

--- src/main/java/nz/co/searchwellington/geocoding/GoogleGeoCodeService.java
+++ src/main/java/nz/co/searchwellington/geocoding/GoogleGeoCodeService.java
@@ -1,8 +1,14 @@
 package nz.co.searchwellington.geocoding;
 
-import geo.google.GeoAddressStandardizer;
-import geo.google.GeoException;
-import geo.google.datamodel.GeoAddress;
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderAddressComponent;
+import com.google.code.geocoder.model.GeocoderLocationType;
+import com.google.code.geocoder.model.GeocoderRequest;
+import com.google.code.geocoder.model.GeocoderResult;
+import com.google.code.geocoder.model.GeocoderStatus;
+
 import nz.co.searchwellington.model.Geocode;
 
 import org.apache.log4j.Logger;
@@ -10,35 +16,44 @@ import org.apache.log4j.Logger;
 public class GoogleGeoCodeService {
 
     private static Logger log = Logger.getLogger(GoogleGeoCodeService.class);
-
-    private String apiKey;
-        
+    
     public GoogleGeoCodeService() {      
     }
     
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-    
-    // TODO Set scope to NZL / Welly if possible. Get all matches and interate for the one within X miles of Te Papa?
-    public void resolveAddress(Geocode geocode) {	// TODO shouldn't know about geocode model object?
+    public void resolveAddress(Geocode geocode) {
         if (geocode != null && geocode.getAddress() != null) {
-            try {
-                GeoAddressStandardizer st = new GeoAddressStandardizer(apiKey);
-                GeoAddress geoaddress = st.standardizeToGeoAddress(geocode.getAddress());
-                if (geoaddress.getCoordinate() != null) {
-                    geocode.setLatitude(geoaddress.getCoordinate().getLatitude());
-                    geocode.setLongitude(geoaddress.getCoordinate().getLongitude());
-                    log.info("Resolved '" + geocode.getAddress() + "' to " + geocode.getLatitude() + ", " + geocode.getLongitude());
-                }
-    
-            } catch (GeoException e) {
-                log.warn(e.getStatus());
-                log.warn(e.getMessage());
-            }
+        	resolveAddressOfGeocode(geocode);
         } else {
             log.warn("No geocode address to resolve.");
         }
     }
-
+    
+	private void resolveAddressOfGeocode(Geocode geocode) {
+		final Geocoder geocoder = new Geocoder();
+		final String addressString = geocode.getAddress();				
+		GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(addressString).setRegion("nz").setLanguage("en").getGeocoderRequest();
+		GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+		log.info("Address '" + addressString + "' resolved to: " + geocoderResponse);
+		if (geocoderResponse.getStatus().equals(GeocoderStatus.OK)) {
+			final GeocoderResult firstMatch = geocoderResponse.getResults().get(0);
+			if (isSuitableMatchInCorrectRegion(firstMatch)) {
+				geocode.setLatitude(firstMatch.getGeometry().getLocation().getLat().doubleValue());
+				geocode.setLongitude(firstMatch.getGeometry().getLocation().getLng().doubleValue());
+			}
+		}
+	}
+	
+	private boolean isSuitableMatchInCorrectRegion(final GeocoderResult firstMatch) {
+		final GeocoderLocationType locationType = firstMatch.getGeometry().getLocationType();
+		if (locationType.equals(GeocoderLocationType.RANGE_INTERPOLATED) || locationType.equals(GeocoderLocationType.ROOFTOP)) {
+			for (GeocoderAddressComponent addressComponent : firstMatch.getAddressComponents()) {
+				if (addressComponent.getTypes().contains("administrative_area_level_1") && addressComponent.getLongName().equals("Wellington")) {
+					return true;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+    
 }
