55b25954323ccb5b5e01e789d39413628f3f420a
Tony McCrae <tony>
2011-06-22 09:05:49 +0100

    Test fixage - move test file to standard layout.

--- src/test/java/nz/co/searchwellington/htmlparsing/IcalLinkExtractorTest.java
+++ src/test/java/nz/co/searchwellington/htmlparsing/IcalLinkExtractorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.net.URL;
 import java.util.Set;
 
 import org.junit.Test;
@@ -11,8 +12,9 @@ import org.junit.Test;
 public class IcalLinkExtractorTest extends ExtractorTestCase {
 
 	@Test
-	public void shouldFindIcalLink() throws Exception {		
-		File contentFile = new File("test/java/nz/co/searchwellington/htmlparsing/wellington_brass_band_front.htm"); 
+	public void shouldFindIcalLink() throws Exception {
+		URL resource = this.getClass().getClassLoader().getResource("wellington_brass_band_front.htm");		
+		File contentFile = new File(resource.getFile()); 
 		StringBuffer content = loadContent(contentFile);
 		
 		IcalEventsLinkExtractor extractor = new IcalEventsLinkExtractor();
