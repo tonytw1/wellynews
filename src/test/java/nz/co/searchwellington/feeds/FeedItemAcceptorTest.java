f8107f4529735ed71540e3f962e7b7cf6a7d0c81
Tony McCrae <tony>
2011-06-29 12:11:36 +0100

    Test coverage around feed item acceptance.

---
+++ src/test/java/nz/co/searchwellington/feeds/FeedItemAcceptorTest.java
@@ -0,0 +1,54 @@
+package nz.co.searchwellington.feeds;
+
+import java.sql.Date;
+
+import nz.co.searchwellington.model.Feed;
+import nz.co.searchwellington.model.FeedNewsitem;
+import nz.co.searchwellington.model.Newsitem;
+import nz.co.searchwellington.model.Website;
+import nz.co.searchwellington.modification.ContentUpdateService;
+import nz.co.searchwellington.tagging.AutoTaggingService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class FeedItemAcceptorTest {
+
+	@Mock RssfeedNewsitemService rssfeedNewsitemService;
+	@Mock AutoTaggingService autoTagger;   
+	@Mock ContentUpdateService contentUpdateService;
+	
+	@Mock FeedNewsitem feednewsitem;
+	@Mock Feed feed;
+	@Mock FeedNewsitem feedNewsitem;
+	@Mock Newsitem newsitem;
+	@Mock Website publisher;
+	
+	private FeedItemAcceptor feedItemAcceptor;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(feednewsitem.getFeed()).thenReturn(feed);
+		Mockito.when(feed.getPublisher()).thenReturn(publisher);
+		Mockito.when(newsitem.getName()).thenReturn("HEADLINE");
+		Mockito.when(rssfeedNewsitemService.makeNewsitemFromFeedItem(Mockito.any(FeedNewsitem.class))).thenReturn(newsitem);
+		feedItemAcceptor = new FeedItemAcceptor(rssfeedNewsitemService, autoTagger, contentUpdateService);
+	}
+	
+	@Test
+	public void shouldSetAcceptedTimeWhenAccepting() throws Exception {
+		feedItemAcceptor.acceptFeedItem(feednewsitem);
+		Mockito.verify(newsitem).setAccepted(Mockito.any(Date.class));
+	}
+	
+	@Test
+	public void shouldFlattenLoudHeadlinesWhenAccepting() throws Exception {		
+		feedItemAcceptor.acceptFeedItem(feednewsitem);
+		Mockito.verify(newsitem).setName("Headline");
+	}
+	
+}
