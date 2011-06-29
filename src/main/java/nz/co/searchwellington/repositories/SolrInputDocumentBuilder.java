e0d72464ced68c316c9065a716e49a39ca6afe3a
Tony McCrae <tony>
2011-06-29 12:17:23 +0100

    Logging around geotag substitution.

--- src/main/java/nz/co/searchwellington/repositories/SolrInputDocumentBuilder.java
+++ src/main/java/nz/co/searchwellington/repositories/SolrInputDocumentBuilder.java
@@ -123,15 +123,17 @@ public class SolrInputDocumentBuilder {
 	
 	private void processGeotags(Resource resource, Set<Tag> indexTagsForResource, SolrInputDocument inputDocument) {
 		Geocode geocode = resource.getGeocode();
-		if (!(geocode != null && geocode.isValid())) {
-			geocode = getGeotagFromFirstResourceTagWithLocation(indexTagsForResource);
-		}		
-		applyGeotagToIndexDocument(inputDocument, geocode);		
+		if (geocode != null && geocode.isValid()) {
+			applyGeotagToIndexDocument(inputDocument, geocode);
+		} else {
+			applyGeotagToIndexDocument(inputDocument, getGeotagFromFirstResourceTagWithLocation(indexTagsForResource));			
+		}
 	}
 	
 	private Geocode getGeotagFromFirstResourceTagWithLocation(Set<Tag> indexTagsForResource) {
 		for (Tag tag : indexTagsForResource) {
 			if (tag.getGeocode() != null && tag.getGeocode().isValid()) {
+				log.info("Found subsitute geotag for resource on resource index tag: " + tag.getName());
 				return tag.getGeocode();
 			}
 		}
@@ -140,6 +142,7 @@ public class SolrInputDocumentBuilder {
 	
 	private void applyGeotagToIndexDocument(SolrInputDocument inputDocument, final Geocode geocode) {
 		if (geocode != null && geocode.isValid()) {
+			log.info("Appling geotag '" + geocode.getAddress() + "' to resource");
 			inputDocument.addField("geotagged", true);
 			inputDocument.addField("address", geocode.getAddress());
 			inputDocument.addField("position", geocode.getLatitude() + "," + geocode.getLongitude());
