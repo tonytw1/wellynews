4dac51f3e81462e6fd669b546f8d67f850e25c24
Tony McCrae <tony>
2011-07-04 13:58:50 +0100

    Schema change required for hydrating of retweets.

---
+++ src/main/java/nz/co/searchwellington/repositories/solr/indexing/SolrTweetsHandler.java
@@ -0,0 +1,18 @@
+package nz.co.searchwellington.repositories.solr.indexing;
+
+import java.util.List;
+
+import nz.co.searchwellington.model.Twit;
+
+import org.apache.solr.common.SolrInputDocument;
+
+public class SolrTweetsHandler {
+
+	public SolrInputDocument processTweets(List<Twit> tweets, SolrInputDocument inputDocument) {
+		for (Twit tweet : tweets) {
+			inputDocument.addField("tweet_author", tweet.getAuthor());
+			inputDocument.addField("tweet_text", tweet.getText());
+		}
+		return inputDocument;
+	}
+}
