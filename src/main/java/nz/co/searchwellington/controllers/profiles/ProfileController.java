ce6ac8da1fdd71fe79f6c99d3c4050c423694ec5
Tony McCrae <tony>
2011-07-05 09:23:44 +0100

    Formatting.

--- src/main/java/nz/co/searchwellington/controllers/profiles/ProfileController.java
+++ src/main/java/nz/co/searchwellington/controllers/profiles/ProfileController.java
@@ -18,29 +18,24 @@ import org.springframework.web.servlet.view.RedirectView;
 
 public class ProfileController extends BaseMultiActionController {
     
-    Logger log = Logger.getLogger(ProfileController.class);
+	private Logger log = Logger.getLogger(ProfileController.class);
 
     private UserRepository userDAO;
     private LoggedInUserFilter loggerInUserFilter;
 	private UrlBuilder urlBuilder;
 	private TagDAO tagDAO;
 	private ContentRetrievalService contentRetrievalService;
-        
-	    
+	
 	public ProfileController() {
 	}
-	
-	
-	public ProfileController(UserRepository userDAO,
-			LoggedInUserFilter loggerInUserFilter, UrlBuilder urlBuilder, TagDAO tagDAO, ContentRetrievalService contentRetrievalService) {
+		
+	public ProfileController(UserRepository userDAO, LoggedInUserFilter loggerInUserFilter, UrlBuilder urlBuilder, TagDAO tagDAO, ContentRetrievalService contentRetrievalService) {
 		this.userDAO = userDAO;
 		this.loggerInUserFilter = loggerInUserFilter;
 		this.urlBuilder = urlBuilder;
 		this.tagDAO = tagDAO;
 		this.contentRetrievalService = contentRetrievalService;
 	}
-
-	
 	
 	@Transactional
     public ModelAndView all(HttpServletRequest request, HttpServletResponse response) {
@@ -50,9 +45,7 @@ public class ProfileController extends BaseMultiActionController {
         mv.addObject("profiles", userDAO.getActiveUsers());        
         return mv;
     }
-
-
-
+	
 	@Transactional
     public ModelAndView edit(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mv = new ModelAndView("editProfile");    
@@ -63,67 +56,61 @@ public class ProfileController extends BaseMultiActionController {
         mv.addObject("user", loggedInUser);
         
         return mv;
-    }
-	
-	
-	
-	
-	  @Transactional
-	  public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {	      
-		  User loggedInUser = loggerInUserFilter.getLoggedInUser();		  
-		  if (loggedInUser != null) {
-			  if (request.getParameter("profilename") != null && isProfilenameValid(request.getParameter("profilename"))) {
-				  loggedInUser.setProfilename(request.getParameter("profilename"));
-			  }
+    }	
+	  
+	@Transactional
+	public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {	      
+		final User loggedInUser = loggerInUserFilter.getLoggedInUser();		  
+		if (loggedInUser != null) {
+			if (request.getParameter("profilename") != null && isProfilenameValid(request.getParameter("profilename"))) {
+				loggedInUser.setProfilename(request.getParameter("profilename"));
+			}
 			  
-			  loggedInUser.setName(request.getParameter("name"));			
-			  loggedInUser.setBio(request.getParameter("bio"));			  			  
-			  loggedInUser.setUrl(request.getParameter("url"));			  
-			  userDAO.saveUser(loggedInUser);
-		  }
+			loggedInUser.setName(request.getParameter("name"));			
+			loggedInUser.setBio(request.getParameter("bio"));			  			  
+			loggedInUser.setUrl(request.getParameter("url"));			  
+			userDAO.saveUser(loggedInUser);
+		  	}
 		  
-		  return new ModelAndView(new RedirectView(urlBuilder.getProfileUrl(loggedInUser)));
-	  }
-
+		return new ModelAndView(new RedirectView(urlBuilder.getProfileUrl(loggedInUser)));
+	}
 	
-	  @Transactional
-	  public ModelAndView view(HttpServletRequest request, HttpServletResponse response) {	        
-	        String path = request.getPathInfo();	       
-	        if (path.matches("^/profiles/.*$")) {
-	        	final String profilename = path.split("/")[2];	        	
-		        
-		        User user = userDAO.getUserByProfileName(profilename);
-	        	if (user != null) {
-	        		ModelAndView mv = new ModelAndView("viewProfile");
-	        		User loggedInUser = loggerInUserFilter.getLoggedInUser();
-	        		if (loggedInUser != null && loggedInUser.getId() == user.getId()) {
-	        			mv = new ModelAndView("profile");
-	        		}
+	@Transactional
+	public ModelAndView view(HttpServletRequest request, HttpServletResponse response) {
+		final String path = request.getPathInfo();
+		if (path.matches("^/profiles/.*$")) {
+			final String profilename = path.split("/")[2];
+
+			User user = userDAO.getUserByProfileName(profilename);
+			if (user != null) {
+				ModelAndView mv = new ModelAndView("viewProfile");
+				User loggedInUser = loggerInUserFilter.getLoggedInUser();
+				if (loggedInUser != null && loggedInUser.getId() == user.getId()) {
+					mv = new ModelAndView("profile");
+				}
 				
-	        		mv.addObject("heading", "User profile");
-	        		mv.addObject("top_level_tags", tagDAO.getTopLevelTags());
-	        		
-	        		log.info("Put user onto model: " + user.getOpenId());
-	        		mv.addObject("profileuser", user);
-	        		
-	        		mv.addObject("submitted", contentRetrievalService.getOwnedBy(user, MAX_NEWSITEMS));
-	        		mv.addObject("tagged", contentRetrievalService.getTaggedBy(user, MAX_NEWSITEMS));
-	        		return mv;
-	        	}
-	        }
-	        	
-	        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			return null;
-	  }
-	  
-	  
-	  private boolean isProfilenameValid(String profilename) {
-		  if (profilename.matches("[a-z|A-Z|0-9]+")) {
-			  if (userDAO.getUserByProfileName(profilename) == null) {
-				  return true;
-			  }
-		  }
-		  return false;
-	  }	
-	    
+				mv.addObject("heading", "User profile");
+				mv.addObject("top_level_tags", tagDAO.getTopLevelTags());
+
+				log.info("Put user onto model: " + user.getOpenId());
+				mv.addObject("profileuser", user);
+
+				mv.addObject("submitted", contentRetrievalService.getOwnedBy(user, MAX_NEWSITEMS));
+				mv.addObject("tagged", contentRetrievalService.getTaggedBy(user, MAX_NEWSITEMS));
+				return mv;
+			}
+		}
+		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		return null;
+	}
+
+	private boolean isProfilenameValid(String profilename) {
+		if (profilename.matches("[a-z|A-Z|0-9]+")) {
+			if (userDAO.getUserByProfileName(profilename) == null) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
