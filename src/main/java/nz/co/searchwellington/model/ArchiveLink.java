fb96b70d5e82c7fe1063bfda313e94f3bdcd8588
Tony McCrae <tony@eelpieconsulting.co.uk>
2011-07-19 18:54:10 +0100

    Sonar warning - equals without hashcode. Drop unused setters while we're in here.

--- src/main/java/nz/co/searchwellington/model/ArchiveLink.java
+++ src/main/java/nz/co/searchwellington/model/ArchiveLink.java
@@ -7,52 +7,46 @@ public class ArchiveLink {
     private Date month;
     private int count;
     
-    
-    
-    @Override
-    public boolean equals(Object obj) {      
-        if (obj instanceof ArchiveLink) {
-            return ((ArchiveLink) obj).getMonth().equals(this.getMonth());
-        }
-        return false;
-    }
-    
-    
     public ArchiveLink(Date month, int count) {       
         this.month = month;
         this.count = count;
     }
-
-
-
+    
     public int getCount() {
         return count;
     }
-
 
     public int getYear() {       
         return month.getYear()+ 1900;
     }
     
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-
-
     public Date getMonth() {
         return month;
     }
     
-    public void setMonth(Date month) {
-        this.month = month;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((month == null) ? 0 : month.hashCode());
+		return result;
+	}
 
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArchiveLink other = (ArchiveLink) obj;
+		if (month == null) {
+			if (other.month != null)
+				return false;
+		} else if (!month.equals(other.month))
+			return false;
+		return true;
+	}
 	
-    
-   
-    
-           
 }
