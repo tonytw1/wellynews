ce52a301ed241d873afe31ee31b7f380f72bdee5
Tony McCrae <tony>
2011-07-23 19:15:48 +0100

    Phase 1 of Operation "Get the caches out of the JVM". Introduce a memcached backed cache.
    The production install of this system runs on a memory constrained VM. JVM madness has been known to bring down the whole VM. Moving the caches out of the JVM might help us control the RAM usage better, allowing two instances of the application to run in the same JVM.

---
+++ src/test/java/nz/co/searchwellington/caching/MemcachedCacheFunctionalTest.java
@@ -0,0 +1,50 @@
+package nz.co.searchwellington.caching;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nz.co.searchwellington.model.FeedNewsitem;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MemcachedCacheFunctionalTest {
+
+	private static final int TTL = 60;
+	private static final String KEY = "testkey3";
+	private static final String FEED_KEY = "feedkey2";
+	private static final Object TEST_STRING = "Test String";
+	private static final String HEADLINE = "Test headline";
+
+	private MemcachedCache cache;
+	
+	@Before
+	public void setup() {
+		cache = new MemcachedCache();
+		cache.setMemcacheUrls("dev.local:11211");
+	}
+	
+	@Test
+	public void canRoundTripSimpleContentThroughTheCache() throws Exception {		
+		cache.put(KEY, 3600, TEST_STRING);		
+		assertEquals(TEST_STRING, cache.get(KEY));		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void canRoundTripMoreInterestingObjectsThroughTheCache() throws Exception {
+		List<FeedNewsitem> feednewsItems = new ArrayList<FeedNewsitem>();
+		FeedNewsitem feedNewsitem = new FeedNewsitem(1, HEADLINE, null, null, null, null, null);		
+		feednewsItems.add(feedNewsitem);
+		assertEquals(HEADLINE, feednewsItems.get(0).getName());
+
+		cache.put(FEED_KEY, TTL, feednewsItems);
+		
+		List<FeedNewsitem> retrieved = (List<FeedNewsitem>) cache.get(FEED_KEY);
+		assertEquals(1, retrieved.size());
+		assertEquals(HEADLINE, retrieved.get(0).getName());
+	}
+	
+}
