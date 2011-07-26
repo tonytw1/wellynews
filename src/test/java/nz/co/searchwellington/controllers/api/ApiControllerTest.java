ae1ec6899f209ac423091fc5ce459ad4dc147e79
Tony McCrae <tony>
2011-07-26 08:51:09 +0100

    Restore api controller test.

--- src/test/java/nz/co/searchwellington/controllers/api/ApiControllerTest.java
+++ src/test/java/nz/co/searchwellington/controllers/api/ApiControllerTest.java
@@ -1,49 +1,58 @@
 package nz.co.searchwellington.controllers.api;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import junit.framework.TestCase;
 import nz.co.searchwellington.controllers.LoggedInUserFilter;
 import nz.co.searchwellington.controllers.admin.AdminRequestFilter;
 import nz.co.searchwellington.feeds.RssfeedNewsitemService;
-import nz.co.searchwellington.model.Newsitem;
+import nz.co.searchwellington.model.FeedNewsitem;
 import nz.co.searchwellington.model.User;
 import nz.co.searchwellington.modification.ContentUpdateService;
 import nz.co.searchwellington.repositories.ResourceRepository;
 import nz.co.searchwellington.repositories.SupressionService;
 import nz.co.searchwellington.tagging.AutoTaggingService;
 
-public class ApiControllerTest extends TestCase {
+import org.apache.struts.mock.MockHttpServletResponse;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class ApiControllerTest {
 	
-	ResourceRepository	resourceDAO = mock(ResourceRepository.class);
-	AdminRequestFilter	requestFilter = mock(AdminRequestFilter.class);
-	LoggedInUserFilter	loggedInUserFilter = mock(LoggedInUserFilter.class);
-	SupressionService supressionService = mock(SupressionService.class);
-	RssfeedNewsitemService rssNewsitemService = mock(RssfeedNewsitemService.class);
-	ContentUpdateService contentUpdateService = mock(ContentUpdateService.class);
-	AutoTaggingService autoTaggingService = mock(AutoTaggingService.class);
+	@Mock ResourceRepository resourceDAO;
+	@Mock AdminRequestFilter requestFilter;
+	@Mock LoggedInUserFilter loggedInUserFilter;
+	@Mock SupressionService supressionService;
+	@Mock RssfeedNewsitemService rssNewsitemService;
+	@Mock ContentUpdateService contentUpdateService;
+	@Mock AutoTaggingService autoTaggingService;
+	
+	@Mock FeedNewsitem acceptedFeedNewsitem;
+	@Mock User adminUser;
 
-	Newsitem acceptedNewsitem = mock(Newsitem.class);
-	User adminUser = mock(User.class);
-
-	protected void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		when(adminUser.getName()).thenReturn("An admin user");
 		when(adminUser.isAdmin()).thenReturn(true);
 		when(loggedInUserFilter.getLoggedInUser()).thenReturn(adminUser);
 	}
 	
-	// TODO reenable
-	public void testShouldAcceptFeedItemByUrl() throws Exception {	
-		/*
+	@Test
+	public void authorisedUsersCanRemotelyAcceptFeedsItemByUrl() throws Exception {
 		ApiController controller = new ApiController(resourceDAO, requestFilter, loggedInUserFilter, supressionService, rssNewsitemService, contentUpdateService, null, autoTaggingService, null);
 		
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		
 		request.setParameter("url", "http://test/233");
-		when(rssNewsitemService.getFeedNewsitemByUrl("http://test/233")).thenReturn(acceptedNewsitem);
+		when(rssNewsitemService.getFeedNewsitemByUrl("http://test/233")).thenReturn(acceptedFeedNewsitem);
 		controller.accept(request, response);
-		verify(contentUpdateService).update(acceptedNewsitem);		
-		 */
+
+		Mockito.verify(autoTaggingService).autotag(acceptedFeedNewsitem);	// TODO Post accept steps should be shared with the normal accept method!
+		Mockito.verify(contentUpdateService).update(acceptedFeedNewsitem);		
 	}
 	
 }
