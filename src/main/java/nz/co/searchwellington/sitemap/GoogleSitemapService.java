08078c21c54dd644201d86864f35b016a868ed30
Tony McCrae <tony>
2011-08-26 10:20:08 +0100

    Don't output all of the possible pagination options into the sitemap - just the tag front pages.

--- src/main/java/nz/co/searchwellington/sitemap/GoogleSitemapService.java
+++ src/main/java/nz/co/searchwellington/sitemap/GoogleSitemapService.java
@@ -8,7 +8,6 @@ import nz.co.searchwellington.repositories.ContentRetrievalService;
 import nz.co.searchwellington.repositories.TagDAO;
 import nz.co.searchwellington.urls.UrlBuilder;
 
-import org.apache.log4j.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -17,24 +16,19 @@ import org.dom4j.QName;
 
 public class GoogleSitemapService {
     
-    private static final int MAX_NEWSITEMS = 30;	// TODO duplicated - move to a paginator class
-
-	Logger log = Logger.getLogger(GoogleSitemapService.class);    
-    private static final String NAMESPACE = "http://www.sitemaps.org/schemas/sitemap/0.9";
+	private static final String NAMESPACE = "http://www.sitemaps.org/schemas/sitemap/0.9";
     
     private ContentRetrievalService contentRetrivalService;
     private DateFormatter dateFormatter;
     private UrlBuilder urlBuilder;
     private TagDAO tagDAO;
     
-
     public GoogleSitemapService(ContentRetrievalService contentRetrivalService, DateFormatter dateFormatter, UrlBuilder urlBuilder, TagDAO tagDAO) {        
         this.contentRetrivalService = contentRetrivalService;
         this.dateFormatter = dateFormatter;
         this.urlBuilder = urlBuilder;
         this.tagDAO = tagDAO;
     }
-    
     
     public String render(String siteLocation) {
         Element urlset = DocumentHelper.createElement("urlset");
@@ -45,7 +39,6 @@ public class GoogleSitemapService {
         }
         return document.asXML();
     }
-
     
     private void addTagUrl(Element root, Tag tag, String siteLocation) {              
         final String url = urlBuilder.getTagUrl(tag);     
@@ -54,20 +47,12 @@ public class GoogleSitemapService {
         if (lastUpdated != null) {
         	lastmod = dateFormatter.formatW3CDate(lastUpdated);
         }
-        addUrlElement(root, url, lastmod);
-        
-        
-        int tagNewsitemCount = contentRetrivalService.getTaggedNewitemsCount(tag);
-        for (int i = 2; ((i -1)  * MAX_NEWSITEMS) <= tagNewsitemCount; i =i + 1) {
-        	 addUrlElement(root, url + "?page=" + i, null);	
-        }
+        addUrlElement(root, url, lastmod);       
     }
-
-
+    
 	private void addUrlElement(Element root, final String url, final String dateString) {
 		Element tagElement = root.addElement(new QName("url", new Namespace("sitemap", NAMESPACE)));   
         tagElement.addNamespace("sitemap", NAMESPACE);
-
         Element locElement = tagElement.addElement(new QName("loc", new Namespace("sitemap", NAMESPACE)));
 		locElement.setText(url);
         
