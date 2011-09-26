f98da20d6d85353c1cb0c3f4d89f6f1a5c1d95af
Tony McCrae <tony@eelpieconsulting.co.uk>
2011-09-26 10:36:14 +0100

    Final.

--- src/main/java/nz/co/searchwellington/feeds/FeednewsItemToNewsitemService.java
+++ src/main/java/nz/co/searchwellington/feeds/FeednewsItemToNewsitemService.java
@@ -14,7 +14,7 @@ public class FeednewsItemToNewsitemService {
 	// TODO merge with addSuppressAndLocalCopyInformation?
 	public Newsitem makeNewsitemFromFeedItem(Feed feed, FeedNewsitem feedNewsitem) {
 		// TODO why are we newing up an instance of our superclass?
-	    String description =  feedNewsitem.getDescription() != null ? feedNewsitem.getDescription() : "";
+	    final String description =  feedNewsitem.getDescription() != null ? feedNewsitem.getDescription() : "";
 		Newsitem newsitem = new NewsitemImpl(0, feedNewsitem.getName(), feedNewsitem.getUrl(), description, feedNewsitem.getDate(), feed.getPublisher(), new HashSet<DiscoveredFeed>(), null, new HashSet<Twit>());
 	    newsitem.setImage(feedNewsitem.getImage());
 	    newsitem.setFeed(feed);
