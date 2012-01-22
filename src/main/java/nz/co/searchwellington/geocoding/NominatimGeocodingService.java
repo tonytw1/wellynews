0e6d5627266791d2ce7a19279cee0c0d6cb32e3b
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-01-22 17:48:40 +0100

    Experimenting with Open Street Map Nominatim. Does an excellent job of resolving buildings - which is what alot of newsitems refer to.

---
+++ src/main/java/nz/co/searchwellington/geocoding/NominatimGeocodingService.java
@@ -0,0 +1,38 @@
+package nz.co.searchwellington.geocoding;
+
+import java.io.IOException;
+import java.util.List;
+
+import nz.co.searchwellington.model.Geocode;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
+
+import fr.dudie.nominatim.client.JsonNominatimClient;
+import fr.dudie.nominatim.client.NominatimClient;
+import fr.dudie.nominatim.model.Address;
+
+public class NominatimGeocodingService implements GeoCodeService {
+	
+	private static Logger log = Logger.getLogger(NominatimGeocodingService.class);
+
+	@Override
+	public Geocode resolveAddress(String address) {
+		HttpClient httpClient = new DefaultHttpClient();
+		NominatimClient nominatimClient = new JsonNominatimClient(httpClient, "tony@wellington.gen.nz");
+		try {
+			List<Address> results = nominatimClient.search(address);
+			if (!results.isEmpty()) {
+				Address firstResult = results.get(0);
+				return new Geocode(firstResult.getDisplayName(), firstResult.getLatitude(), firstResult.getLongitude());
+			}
+			
+		} catch (IOException e) {
+			log.warn("Exception while searching for '" + address + "': " + e.getMessage());
+		}
+		
+		return null;
+	}
+
+}
