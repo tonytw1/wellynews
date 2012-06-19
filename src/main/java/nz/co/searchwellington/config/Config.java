755d3c0ba9eb5f1264ad037bc1d48d30c0e3bbfd
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-06-19 14:19:12 +0100

    private fields.

--- src/main/java/nz/co/searchwellington/config/Config.java
+++ src/main/java/nz/co/searchwellington/config/Config.java
@@ -2,16 +2,15 @@ package nz.co.searchwellington.config;
 
 public class Config {
     
-    int id;  
-    String statsTracking;
-    String flickrPoolGroupId;
-    String useClickthroughCounter;
+    private int id;  
+    private String statsTracking;
+    private String flickrPoolGroupId;
+    private String useClickthroughCounter;
     private boolean twitterListenerEnabled;
 	private boolean feedReadingEnabled;
     
     public Config() {
     }
-    
     
     public int getId() {
         return id;
@@ -24,43 +23,35 @@ public class Config {
     public String getStatsTracking() {
         return statsTracking;
     }
-
-
+    
     public void setStatsTracking(String statsTracking) {
         this.statsTracking = statsTracking;
     }
-
-
+    
     public String getFlickrPoolGroupId() {
         return flickrPoolGroupId;
     }
-
-
+    
     public void setFlickrPoolGroupId(String flickrPoolGroupOId) {
         this.flickrPoolGroupId = flickrPoolGroupOId;
     }
-
-
+    
     public String getUseClickthroughCounter() {
         return useClickthroughCounter;
     }
-
-
+    
     public void setUseClickthroughCounter(String useClickthroughCounter) {
         this.useClickthroughCounter = useClickthroughCounter;
     }
-
-
+    
 	public void setTwitterListenerEnabled(boolean twitterListenerEnabled) {
 		this.twitterListenerEnabled = twitterListenerEnabled;
 	}
-
-
+	
 	public boolean isTwitterListenerEnabled() {
 		return twitterListenerEnabled;
 	}
-
-
+	
 	public boolean isFeedReadingEnabled() {
 		return feedReadingEnabled;
 	}
