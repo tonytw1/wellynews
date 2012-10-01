edc102bebc91b6538befdfe4b965b351e1a5e321
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-10-01 21:06:13 +0100

    Remove unused TC references.

--- src/main/java/nz/co/searchwellington/controllers/admin/AdminIndexController.java
+++ src/main/java/nz/co/searchwellington/controllers/admin/AdminIndexController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import nz.co.searchwellington.controllers.BaseMultiActionController;
-import nz.co.searchwellington.repositories.keystore.KeyStore;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +14,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AdminIndexController extends BaseMultiActionController {
 	
-	private KeyStore keystore;
-	
-	public AdminIndexController(KeyStore keystore) {
-		this.keystore = keystore;
+	public AdminIndexController() {
 	}
 	
 	@RequestMapping("/admin/index")
 	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		final ModelAndView mv = new ModelAndView("adminindex");
 		mv.addObject("heading", "Admin index");	
-		mv.addObject("keystorecount", Long.toString(keystore.size()));		
 		return mv;
 	}
 
