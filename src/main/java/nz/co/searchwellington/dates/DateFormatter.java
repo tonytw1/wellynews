fc4c67d87aacf93657a2c93ce9d4897f440370cc
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-10-02 22:09:23 +0100

    Removing unused and common methods.

--- src/main/java/nz/co/searchwellington/dates/DateFormatter.java
+++ src/main/java/nz/co/searchwellington/dates/DateFormatter.java
@@ -1,11 +1,9 @@
 package nz.co.searchwellington.dates;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Component;
@@ -27,59 +25,11 @@ public class DateFormatter {
         }
         return null;
     }
-           
-    public static String formatDateToTimeZone(Date startDate) {
-        final String TIMEZONE_ID = "Pacific/Auckland";
-        DateTimeFormatter fmt = DateTimeFormat.forPattern("d MMM yyy h:mma");
-        DateTime now = new DateTime(startDate);
-        DateTime localTime = now.withZone(DateTimeZone.forID(TIMEZONE_ID));
-        return (fmt.print(localTime));
-    }
     
     public String formatW3CDate(Date startDate) {   
         DateTimeFormatter fmt = DateTimeFormat.forPattern(W3C_DATETIME_FORMAT);
         DateTime now = new DateTime(startDate);
         return (fmt.print(now));
-    }
-    
-    public String timeSince(Date then) {        
-        Date now = Calendar.getInstance().getTime();
-        final long deltaInMills = now.getTime() - then.getTime();
-
-        if (deltaInMills > 0) {
-            if (deltaInMills < 60 * 1000) {
-                return "less than 1 minute ago";
-            } else if (deltaInMills < 120 * 1000) {
-                return "1 minute ago";
-            } else if (deltaInMills < 60 * 1000 * 60) {
-                float minutes = (deltaInMills / (1000 * 60));
-                return (Math.round(minutes) + " minutes ago");
-            } else if (deltaInMills < 60 * 1000 * 60 * 24){
-                int hours = Math.round((deltaInMills / (1000 * 60 * 60)));
-                if (hours == 1) {
-                    return hours + " hour ago";
-                } else {
-                    return hours + " hours ago";
-                }
-            } else if (deltaInMills < 60 * 1000 * 60 * 24 * 7){
-                int days = Math.round((deltaInMills / (1000 * 60 * 60 * 24)));
-                if (days == 1) {
-                    return days + " day ago";
-                } else {
-                    return days + " days ago";
-                }
-            } else {
-                int weeks = Math.round((deltaInMills / (1000 * 60 * 60 * 24 * 7)));
-                // TODO we have a plural class for this somewhere?
-                // No it's in vm not java.
-                if (weeks == 1) {
-                    return weeks + " week ago";
-                } else {
-                    return weeks + " weeks ago";
-                }
-            }
-        }
-        return null;
     }
     
 }
--- src/test/java/nz/co/searchwellington/dates/DateFormatterTest.java
+++ src/test/java/nz/co/searchwellington/dates/DateFormatterTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
 
-import org.joda.time.DateTime;
-import org.junit.Test;
-
 public class DateFormatterTest {
     
 	DateFormatter dateFormatter = new DateFormatter();        
@@ -24,29 +21,6 @@ public class DateFormatterTest {
         
         final String formatted = dateFormatter.formatW3CDate(now.getTime());
         assertEquals("2008-06-09T08:10:22+01:00", formatted);
-    }
-    
-	@Test
-    public void testShouldGiveNiceTimeDeltas() throws Exception {    	
-    	DateTime now = new DateTime();
-    	
-    	DateTime lessThanOneMinuteAgo = now.minusSeconds(30);
-    	assertEquals("less than 1 minute ago", dateFormatter.timeSince(lessThanOneMinuteAgo.toDate()));
-    	
-    	DateTime fiveMinutesBefore = now.minusMinutes(5);
-        assertEquals("5 minutes ago", dateFormatter.timeSince(fiveMinutesBefore.toDate()));   
-       
-        DateTime anHourAgo = now.minusHours(1);
-        assertEquals("1 hour ago", dateFormatter.timeSince(anHourAgo.toDate()));
-        
-        DateTime twoHoursAgo = now.minusHours(2);
-        assertEquals("2 hours ago", dateFormatter.timeSince(twoHoursAgo.toDate()));
-                
-        DateTime oneDayAgo = now.minusDays(1);
-        assertEquals("1 day ago", dateFormatter.timeSince(oneDayAgo.toDate()));
-        
-        DateTime oneWeekAgo = now.minusWeeks(1);
-        assertEquals("1 week ago", dateFormatter.timeSince(oneWeekAgo.toDate()));                        
     }
     
 }
