432889f96834bbd5b159bc1983f001515c917dc3
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-10-05 17:09:54 +0100

    Autowiring types correct.

--- src/main/java/nz/co/searchwellington/geocoding/osm/CachingNominatimGeocodingService.java
+++ src/main/java/nz/co/searchwellington/geocoding/osm/CachingNominatimGeocodingService.java
@@ -2,31 +2,29 @@ package nz.co.searchwellington.geocoding.osm;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import nz.co.searchwellington.geocoding.CachingServiceWrapper;
 import nz.co.searchwellington.model.Geocode;
 import nz.co.searchwellington.model.OsmId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class CachingNominatimGeocodingService implements GeoCodeService {
 	
-	private CachingServiceWrapper<String, List<Geocode>> resolveAddressCachingWrapper;
-	private CachingServiceWrapper<OsmId, Geocode> resolveOsmIdCachingWrapper;
+	private NominatimResolveAddressCachingServiceWrapper resolveAddressCachingWrapper;
+	private NominatimResolveOsmIdCachingServiceWrapper resolveOsmIdCachingWrapper;
 	
 	@Autowired
-	public CachingNominatimGeocodingService(CachingServiceWrapper<String, List<Geocode>> resolveAddressCachingWrapper,
-			CachingServiceWrapper<OsmId, Geocode> resolveOsmIdCachingWrapper) {
+	public CachingNominatimGeocodingService(NominatimResolveAddressCachingServiceWrapper resolveAddressCachingWrapper, NominatimResolveOsmIdCachingServiceWrapper resolveOsmIdCachingWrapper) {
 		this.resolveAddressCachingWrapper = resolveAddressCachingWrapper;
 		this.resolveOsmIdCachingWrapper = resolveOsmIdCachingWrapper;
 	}
-
+	
 	@Override
 	public List<Geocode> resolveAddress(String address) {
 		return resolveAddressCachingWrapper.callService(address);
 	}
-
+	
 	@Override
 	public Geocode resolveOsmId(OsmId osmId) {
 		return resolveOsmIdCachingWrapper.callService(osmId);
