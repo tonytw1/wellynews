975dbfc8f35d13d79eeedd9869fd19652c9a369a
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-10-26 22:45:22 +0100

    Autowiring.

--- src/main/java/nz/co/searchwellington/repositories/SolrQueryBuilderFactory.java
+++ src/main/java/nz/co/searchwellington/repositories/SolrQueryBuilderFactory.java
@@ -1,7 +1,10 @@
 package nz.co.searchwellington.repositories;
 
+import org.springframework.stereotype.Component;
+
 import nz.co.searchwellington.repositories.solr.SolrQueryBuilder;
 
+@Component
 public class SolrQueryBuilderFactory {
 
 	public SolrQueryBuilder makeNewBuilder() {
