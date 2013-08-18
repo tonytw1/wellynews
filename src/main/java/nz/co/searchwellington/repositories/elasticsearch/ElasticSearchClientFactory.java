07e3b7d331826cdcc1dcc440bab7b3d3552a3682
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-08-19 11:04:06 +1200

    Elastic search cluster connections details are configurable.

--- src/main/java/nz/co/searchwellington/repositories/elasticsearch/ElasticSearchClientFactory.java
+++ src/main/java/nz/co/searchwellington/repositories/elasticsearch/ElasticSearchClientFactory.java
@@ -4,11 +4,11 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 import org.apache.log4j.Logger;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.node.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,27 +16,27 @@ public class ElasticSearchClientFactory {
 	
 	private static Logger log = Logger.getLogger(ElasticSearchClientFactory.class);
 	
-	private String clusterName = "elasticsearch";
-	private String unicastHosts = "ubuntu.local";
+	private final String clusterName;
+	private final String unicastHosts;
 	
 	private Client client;
 	
-	public void setClusterName(String clusterName) {
+	@Autowired
+	public ElasticSearchClientFactory(@Value("#{config['elasticsearch.cluster']}") String clusterName,
+			@Value("#{config['elasticsearch.unicasthosts']}") String unicastHosts) {
+		super();
 		this.clusterName = clusterName;
-	}
-	
-	public void setUnicastHosts(String unicastHosts) {
 		this.unicastHosts = unicastHosts;
 	}
-	
+
 	public synchronized Client getClient() {
 		if (client == null) {
-			client = connectToCluster();
+			client = connectToUnicastCluster();
 		}
 		return client;
 	}
 
-	private Client connectToCluster() {
+	private Client connectToUnicastCluster() {
 		if (client == null) {			
 			log.info("Connecting to elastic search cluster: " + clusterName + ", unicast hosts: " + unicastHosts);
 			final Settings settings = ImmutableSettings.settingsBuilder()
@@ -49,10 +49,6 @@ public class ElasticSearchClientFactory {
 			client = node.client();
 		}
 		return client;
-	}
-	
-	private Client connectToNamedMachine(String host) {
-		return new TransportClient().addTransportAddress(new InetSocketTransportAddress(host, 9300));		
 	}
 	
 }
