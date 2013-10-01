65305ef51a9f380acf91cdbc64ed6e75fb868db6
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-10-01 11:54:49 +0100

    Fixes build.

--- src/test/java/nz/co/searchwellington/SpringContextIT.java
+++ src/test/java/nz/co/searchwellington/SpringContextIT.java
@@ -1,10 +1,8 @@
 package nz.co.searchwellington;
 
 import static org.junit.Assert.assertNotNull;
-import nz.co.searchwellington.feeds.rss.RssNewsitemPrefetcher;
 
 import org.junit.Test;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,14 +12,6 @@ public class SpringContextIT {
 	public void canLoadContext() throws Exception {
 		 ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		 assertNotNull(context);
-	}
-	
-	@Test
-	public void canAutowire() throws Exception {
-		final ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");		
-		Object autowire = context.getAutowireCapableBeanFactory().autowire(RssNewsitemPrefetcher.class, AutowireCapableBeanFactory.AUTOWIRE_CONSTRUCTOR, true);
-		System.out.println(autowire);
-		assertNotNull(autowire);
 	}
 	
 }
