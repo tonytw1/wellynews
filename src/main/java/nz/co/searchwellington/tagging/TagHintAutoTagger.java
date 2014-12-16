65dbcd625e48c0b5f8485203fdd8b29283d656f4
tony <tony@eelpieconsulting.co.uk>
2014-12-16 18:11:23 +0000

    Clean ups.

--- src/main/java/nz/co/searchwellington/tagging/TagHintAutoTagger.java
+++ src/main/java/nz/co/searchwellington/tagging/TagHintAutoTagger.java
@@ -4,6 +4,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import nz.co.searchwellington.model.Resource;
 import nz.co.searchwellington.model.Tag;
 import nz.co.searchwellington.repositories.TagDAO;
@@ -17,7 +21,9 @@ public class TagHintAutoTagger {
 
 	private static Logger log = Logger.getLogger(TagHintAutoTagger.class);
 
-	private TagDAO tagDAO;
+	private static final Splitter COMMA_SPLITTER = Splitter.on(",");
+
+	private final TagDAO tagDAO;
 
 	@Autowired
 	public TagHintAutoTagger(TagDAO tagDAO) {
@@ -25,35 +31,32 @@ public class TagHintAutoTagger {
 	}
 
 	public Set<Tag> suggestTags(Resource resource) {
-		Set<Tag> suggestedTags = new HashSet<Tag>();
-
-		List<Tag> tags = tagDAO.getAllTags();
-		for (Tag tag : tags) {
-			String tagHints = tag.getAutotagHints();
-			if (tagHints != null && !tagHints.trim().equals("")) {
-				String[] hints = tagHints.split(",");
-				if (hints.length > 0) {
-					for (int i = 0; i < hints.length; i++) {
-						String hint = hints[i].trim();
-						if (checkForMatch(resource, hint)) {
-							log.info("Suggesting tag '" + tag.getDisplayName() + "' for resource: " + resource.getName());
-							suggestedTags.add(tag);
-						}
-					}
-				}
+		final Set<Tag> suggestedTags = Sets.newHashSet();
+		for (Tag tag : tagDAO.getAllTags()) {
+			if (!Strings.isNullOrEmpty(tag.getAutotagHints())) {
+				final List<String> hints = Lists.newArrayList(COMMA_SPLITTER.split(tag.getAutotagHints()));
+				suggestedTags.addAll(process(resource, tag, hints));
 			}
 		}
 		return suggestedTags;
 
 	}
 
+	private Set<Tag> process(Resource resource, Tag tag, List<String> hints) {
+		final Set<Tag> suggestedTags = Sets.newHashSet();
+		for(String hint : hints) {
+			if (checkForMatch(resource, hint)) {
+				log.info("Suggesting tag '" + tag.getDisplayName() + "' for resource: " + resource.getName());
+				suggestedTags.add(tag);
+            }
+        }
+		return suggestedTags;
+	}
+
 	private boolean checkForMatch(Resource resource, String hint) {
-		if (!hint.trim().equals("")) {
-			boolean headlineMatchesHint = resource.getName().toLowerCase().contains(hint.toLowerCase());
-			boolean BodyMatchesTag = resource.getDescription().toLowerCase().contains(hint.toLowerCase());
-			return headlineMatchesHint || BodyMatchesTag;
-		}
-		return false;
+		final boolean headlineMatchesHint = resource.getName().toLowerCase().contains(hint.toLowerCase());
+		final boolean bodyMatchesTag = resource.getDescription().toLowerCase().contains(hint.toLowerCase());
+		return headlineMatchesHint || bodyMatchesTag;
 	}
 
 }
