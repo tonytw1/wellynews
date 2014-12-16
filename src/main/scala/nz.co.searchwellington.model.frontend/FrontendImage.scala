3aa273347e785e04e16d1cc8c7722244e478c302
tony <tony@eelpieconsulting.co.uk>
2014-12-16 17:07:01 +0000

    Trying to move second class to scala.

---
+++ src/main/scala/nz.co.searchwellington.model.frontend/FrontendImage.scala
@@ -0,0 +1,8 @@
+package nz.co.searchwellington.model.frontend
+
+/**
+ * Created by tony on 16/12/14.
+ */
+class FrontendImage {
+
+}
--- src/main/scala/nz.co.searchwellington.model.frontend/FrontendTag.scala
+++ src/main/scala/nz.co.searchwellington.model.frontend/FrontendTag.scala
@@ -2,6 +2,6 @@ package nz.co.searchwellington.model.frontend
 
 class FrontendTag(var id: String, var name: String) {
 
-  def getId = id;
-    
+  def getId = id
+
 }
