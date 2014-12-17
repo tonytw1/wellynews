69c659911670cb8b221b83a1bf26a77377956e84
tony <tony@eelpieconsulting.co.uk>
2014-12-17 15:19:42 +0000

    Use yeild to remove the Java append to collection pattern.

--- src/main/scala/nz/co/searchwellington/tagging/TagHintAutoTagger.scala
+++ src/main/scala/nz/co/searchwellington/tagging/TagHintAutoTagger.scala
@@ -1,7 +1,6 @@
 package nz.co.searchwellington.tagging
 
 import com.google.common.base.{Splitter, Strings}
-import com.google.common.collect.Sets
 import nz.co.searchwellington.model.Resource
 import nz.co.searchwellington.model.Tag
 import nz.co.searchwellington.repositories.TagDAO
@@ -12,23 +11,22 @@ class TagHintAutoTagger(tagDAO: TagDAO) {
   val commaSplitter: Splitter = Splitter.on(",")
 
   def suggestTags(resource: Resource) : java.util.Set[Tag] = {
-    val suggestedTags: java.util.Set[Tag] = Sets.newHashSet();
-    for (
-      tag <- tagDAO.getAllTags()
-      if Strings.isNullOrEmpty(tag.getAutotagHints)) {
-        suggestedTags.addAll(process(resource, tag, commaSplitter.split(tag.getAutotagHints()).toList));
-    }
-    return suggestedTags
+    def suggestedTags = for {
+        tag <- tagDAO.getAllTags()
+        if matches(resource, tag)
+      } yield tag;
+    return suggestedTags.toSet
   }
 
-  def process(resource: Resource, tag: Tag, hints: List[String]) : java.util.Set[Tag] = {
-    val suggestedTags: java.util.Set[Tag] = Sets.newHashSet();
-    hints.foreach(hint =>
-      if (checkForMatch(resource, hint)) {
-        suggestedTags.add(tag);
-      }
-    )
-    return suggestedTags;
+  def matches(resource: Resource, tag: Tag) : Boolean = {
+    if (!Strings.isNullOrEmpty(tag.getAutotagHints)) {
+      commaSplitter.split(tag.getAutotagHints()).toList.foreach(hint =>
+        if (checkForMatch(resource, hint)) {
+          return true;
+        }
+      )
+    }
+    return false;
   }
 
   def checkForMatch(resource: Resource, hint: String) : Boolean = {
