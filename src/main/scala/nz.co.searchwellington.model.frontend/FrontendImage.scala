1865a115441d94b27434e4cc94feed3a7878e87a
tony <tony@eelpieconsulting.co.uk>
2014-12-17 10:44:55 +0000

    Jackson getters/setters and null constructor.

--- src/main/scala/nz.co.searchwellington.model.frontend/FrontendImage.scala
+++ src/main/scala/nz.co.searchwellington.model.frontend/FrontendImage.scala
@@ -2,6 +2,11 @@ package nz.co.searchwellington.model.frontend
 
 class FrontendImage(var url: String) {
 
+  def this() {
+    this("")
+  }
+
   def getUrl = url
+  def setUrl (newUrl: String) = {url = newUrl}
 
 }
--- src/main/scala/nz.co.searchwellington.model.frontend/FrontendTag.scala
+++ src/main/scala/nz.co.searchwellington.model.frontend/FrontendTag.scala
@@ -2,6 +2,14 @@ package nz.co.searchwellington.model.frontend
 
 class FrontendTag(var id: String, var name: String) {
 
+  def this() {
+    this("", "")
+  }
+
   def getId = id
+  def setId (newId: String) = {id = newId}
+
+  def getName = name
+  def setName(newName: String) = {name = newName}
 
 }
