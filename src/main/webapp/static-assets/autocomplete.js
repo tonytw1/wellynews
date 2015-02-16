58aacfb8145a15f1d82649a4883648f3d86cc7a5
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-02-16 18:27:50 +0000

    Caching

---
+++ src/main/webapp/static-assets/autocomplete.js
@@ -0,0 +1,53 @@
+$(function() {  	
+
+	$("#tagName").autocomplete({
+		source: "http://wellington.gen.nz/ajax/tags"});
+	
+    	$("#publisherName").autocomplete({
+            source: "http://wellington.gen.nz/ajax/publishers"});
+    
+	$("#addTag").click(function() {
+		var tagname = $('#tagName').val();
+		$('#tags').append('<option value="' + tagname + '">' + tagname + '</options>');
+		$('#tagName').val("");
+	});
+			
+	$("#removeTags").click(function() {
+		$('#tags option:selected').remove();			
+	});
+			
+	$("#editForm").submit(function() {
+		$('#tags option').attr('selected', 'selected');
+	});				 
+	
+	$("#geocode").autocomplete({
+		source: function( request, response ) {
+			$.ajax({
+				url: "http://nominatim-ac.eelpieconsulting.co.uk/search",
+				cache: true,
+				dataType: "jsonp",
+				jsonpCallback: 'placeAutocomplete',
+				data: {
+  					q: request.term,
+                                        country: 'nz',
+                                        lat: -41.2889,
+                                        lon: 174.7772
+				},
+				success: function( data ) {
+					response( $.map( data, function( item ) {
+						return {
+							label: (item.address + " (" + item.classification + "/" + item.type + ")"),
+							value: item.address,
+							osmId: item.osmId,
+							osmType: item.osmType
+						}
+					}));
+				}
+			});
+		},			
+		select: function( event, ui ) {
+			$("#selectedGeocode").val( ui.item ? ui.item.osmId + "/" + ui.item.osmType : "");
+		}
+	});
+
+});
