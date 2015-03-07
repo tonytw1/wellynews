360f23692aff1b4276fc447cdec64652f270ab47
tony <tony@eelpieconsulting.co.uk>
2015-03-07 21:17:42 +0000

    Test clean ups; not very functional - probably should move to builder classes.

--- src/test/scala/nz/co/searchwellington/tagging/PlaceAutoTaggerTest.scala
+++ src/test/scala/nz/co/searchwellington/tagging/PlaceAutoTaggerTest.scala
@@ -10,33 +10,43 @@ import nz.co.searchwellington.model.NewsitemImpl
 import nz.co.searchwellington.model.Tag
 import nz.co.searchwellington.repositories.TagDAO
 
+import scala.collection.JavaConverters._
+
 class PlaceAutoTaggerTest extends TestCase {
+
   private var placesTag: Tag = null
   private var aroValleyTag: Tag = null
-  private var placeAutoTagger: PlaceAutoTagger = null
   private var aroValleyNewsitem: Newsitem = null
+
   private var tagDAO: TagDAO = mock(classOf[TagDAO])
+
+  private var placeAutoTagger: PlaceAutoTagger = null
 
   @throws(classOf[Exception])
   protected override def setUp {
-    placesTag = new Tag(1, "places", "Places", null, new HashSet[Tag], 0, false, false)
-    aroValleyTag = new Tag(2, "arovalley", "Aro Valley", placesTag, new HashSet[Tag], 0, false, false)
+    placesTag = new Tag().name("places").displayName("Places");
+    aroValleyTag = new Tag().name("arovalley").displayName("Aro Valley").parent(placesTag);
     placesTag.addChild(aroValleyTag)
-    placeAutoTagger = new PlaceAutoTagger(tagDAO)
+
     when(tagDAO.loadTagByName("places")).thenReturn(placesTag)
+    placeAutoTagger = new PlaceAutoTagger(tagDAO)
   }
 
   @throws(classOf[Exception])
   def testShouldTagNewsitemsWithPlaceTags {
     aroValleyNewsitem = new NewsitemImpl(1, "Test newsitem", null, ".. Student flats in the Aro Valley... Test", null, null, null, null, null)
-    val suggestedTags: Set[Tag] = placeAutoTagger.suggestTags(aroValleyNewsitem)
+
+    val suggestedTags = placeAutoTagger.suggestTags(aroValleyNewsitem)
+
     assertTrue(suggestedTags.contains(aroValleyTag))
   }
 
   @throws(classOf[Exception])
   def testPlaceAutoTaggingShouldBeCaseInsensitive {
     aroValleyNewsitem = new NewsitemImpl(1, "Test newsitem", null, ".. Student flats in the aro valley... Test", null, null, null, null, null)
-    val suggestedTags: Set[Tag] = placeAutoTagger.suggestTags(aroValleyNewsitem)
+
+    val suggestedTags = placeAutoTagger.suggestTags(aroValleyNewsitem)
     assertTrue(suggestedTags.contains(aroValleyTag))
   }
+
 }
