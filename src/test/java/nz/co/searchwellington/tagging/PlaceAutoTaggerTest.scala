5a94dc268e503892be7e85c23b8adb867fb74fd1
tony <tony@eelpieconsulting.co.uk>
2015-03-07 17:54:40 +0000

    Convert tests to scala so that the real code can return Scala immutable Sets.

---
+++ src/test/java/nz/co/searchwellington/tagging/PlaceAutoTaggerTest.scala
@@ -0,0 +1,42 @@
+package nz.co.searchwellington.tagging
+
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.when
+import java.util.HashSet
+import java.util.Set
+import junit.framework.TestCase
+import nz.co.searchwellington.model.Newsitem
+import nz.co.searchwellington.model.NewsitemImpl
+import nz.co.searchwellington.model.Tag
+import nz.co.searchwellington.repositories.TagDAO
+
+class PlaceAutoTaggerTest extends TestCase {
+  private var placesTag: Tag = null
+  private var aroValleyTag: Tag = null
+  private var placeAutoTagger: PlaceAutoTagger = null
+  private var aroValleyNewsitem: Newsitem = null
+  private var tagDAO: TagDAO = mock(classOf[TagDAO])
+
+  @throws(classOf[Exception])
+  protected override def setUp {
+    placesTag = new Tag(1, "places", "Places", null, new HashSet[Tag], 0, false, false)
+    aroValleyTag = new Tag(2, "arovalley", "Aro Valley", placesTag, new HashSet[Tag], 0, false, false)
+    placesTag.addChild(aroValleyTag)
+    placeAutoTagger = new PlaceAutoTagger(tagDAO)
+    when(tagDAO.loadTagByName("places")).thenReturn(placesTag)
+  }
+
+  @throws(classOf[Exception])
+  def testShouldTagNewsitemsWithPlaceTags {
+    aroValleyNewsitem = new NewsitemImpl(1, "Test newsitem", null, ".. Student flats in the Aro Valley... Test", null, null, null, null, null)
+    val suggestedTags: Set[Tag] = placeAutoTagger.suggestTags(aroValleyNewsitem)
+    assertTrue(suggestedTags.contains(aroValleyTag))
+  }
+
+  @throws(classOf[Exception])
+  def testPlaceAutoTaggingShouldBeCaseInsensitive {
+    aroValleyNewsitem = new NewsitemImpl(1, "Test newsitem", null, ".. Student flats in the aro valley... Test", null, null, null, null, null)
+    val suggestedTags: Set[Tag] = placeAutoTagger.suggestTags(aroValleyNewsitem)
+    assertTrue(suggestedTags.contains(aroValleyTag))
+  }
+}
---
+++ src/test/java/nz/co/searchwellington/tagging/TagHintAutoTaggerTest.scala
@@ -0,0 +1,40 @@
+package nz.co.searchwellington.tagging
+
+import com.google.common.collect.Lists
+import nz.co.searchwellington.model.NewsitemImpl
+import nz.co.searchwellington.model.Resource
+import nz.co.searchwellington.model.Tag
+import nz.co.searchwellington.repositories.TagDAO
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.util.List
+import java.util.Set
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.mockito.Mockito.when
+
+class TagHintAutoTaggerTest {
+  @Mock private var tagDAO: TagDAO = null
+  private var tagHintAutoTagger: TagHintAutoTagger = null
+
+  @Before def setup {
+    MockitoAnnotations.initMocks(this)
+    tagHintAutoTagger = new TagHintAutoTagger(tagDAO)
+  }
+
+  @Test def shouldMatchTitlesWhichContainAutotaggingHint {
+    val tag: Tag = new Tag
+    tag.setAutotagHints("fox,animal")
+    val anotherTag: Tag = new Tag
+    anotherTag.setAutotagHints("cat")
+    val allTags: List[Tag] = Lists.newArrayList(tag, anotherTag)
+    when(tagDAO.getAllTags).thenReturn(allTags)
+    val resource: Resource = new NewsitemImpl
+    resource.setName("The quick brown fox jumped over the lazy dog")
+    val suggestions: Set[Tag] = tagHintAutoTagger.suggestTags(resource)
+    assertTrue(suggestions.contains(tag))
+    assertFalse(suggestions.contains(anotherTag))
+  }
+}
