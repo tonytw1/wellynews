d28113bd1470347a6bd282201de29ff39481e431
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-03-27 09:35:27 +0000

    Address should never be null.
    Missing return immediately in the compose address method. What's a good pattern for doing that in Scala?

--- src/main/scala/nz/co/searchwellington/feeds/PlaceToGeocodeMapper.scala
+++ src/main/scala/nz/co/searchwellington/feeds/PlaceToGeocodeMapper.scala
@@ -7,17 +7,12 @@ import uk.co.eelpieconsulting.common.geo.model.Place
 @Component class PlaceToGeocodeMapper {
 
   def mapPlaceToGeocode(place: Place): Geocode = {
-    var address: String = composeAddress(place)
-    if (address == null) {
-      return null
-    }
-
+    val address: String = composeAddress(place)
     val geocode: Geocode = new Geocode(address,
         if (place.getLatLong != null) place.getLatLong.getLatitude else null,
         if (place.getLatLong != null) place.getLatLong.getLongitude else null,
         if (place.getOsmId != null) place.getOsmId.getId else null,
         if (place.getOsmId != null) place.getOsmId.getType.toString else null)
-
     geocode
   }
 
