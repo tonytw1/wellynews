45640638fbf2328b2eecdf6e0c55f993ecaef740
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-05-20 21:28:02 +0100

    This is the correct static folder for packaging in the war file.

--- src/main/webapp/static-assets/autocomplete.js
+++ src/main/webapp/static-assets/autocomplete.js
@@ -1,53 +1,64 @@
 $(function() {  	
 
-	$("#tagName").autocomplete({
-		source: "http://wellington.gen.nz/ajax/tags"});
+	if ($('#tagName').length) {
+		$("#tagName").autocomplete({
+			source: "http://wellington.gen.nz/ajax/tags"});
+	}
 	
-    	$("#publisherName").autocomplete({
-            source: "http://wellington.gen.nz/ajax/publishers"});
-    
-	$("#addTag").click(function() {
-		var tagname = $('#tagName').val();
-		$('#tags').append('<option value="' + tagname + '">' + tagname + '</options>');
-		$('#tagName').val("");
-	});
-			
-	$("#removeTags").click(function() {
-		$('#tags option:selected').remove();			
-	});
-			
-	$("#editForm").submit(function() {
-		$('#tags option').attr('selected', 'selected');
-	});				 
+	if ($('#publisherName').length) {
+	    	$("#publisherName").autocomplete({
+        	    source: "http://wellington.gen.nz/ajax/publishers"});
+    	}
 	
-	$("#geocode").autocomplete({
-		source: function( request, response ) {
-			$.ajax({
-				url: "http://nominatim-ac.eelpieconsulting.co.uk/search",
-				cache: true,
-				dataType: "jsonp",
-				jsonpCallback: 'placeAutocomplete',
-				data: {
-  					q: request.term,
-                                        country: 'nz',
-                                        lat: -41.2889,
-                                        lon: 174.7772
-				},
-				success: function( data ) {
-					response( $.map( data, function( item ) {
-						return {
-							label: (item.address + " (" + item.classification + "/" + item.type + ")"),
-							value: item.address,
-							osmId: item.osmId,
-							osmType: item.osmType
-						}
-					}));
-				}
-			});
-		},			
-		select: function( event, ui ) {
-			$("#selectedGeocode").val( ui.item ? ui.item.osmId + "/" + ui.item.osmType : "");
-		}
-	});
+	if ($('#addTag').length) {
+		$("#addTag").click(function() {
+			var tagname = $('#tagName').val();
+			$('#tags').append('<option value="' + tagname + '">' + tagname + '</options>');
+			$('#tagName').val("");
+		});
+	}
 
+	if ($('#removeTags').length) {	
+		$("#removeTags").click(function() {
+			$('#tags option:selected').remove();			
+		});
+	}
+	
+	if ($('#editForm').length) {
+		$("#editForm").submit(function() {
+			$('#tags option').attr('selected', 'selected');
+		});	 
+	}
+
+	if ($('#geocode').length) {
+		$("#geocode").autocomplete({
+			source: function( request, response ) {
+				$.ajax({
+					url: "http://nominatim-ac.eelpieconsulting.co.uk/search",
+					cache: true,
+					dataType: "jsonp",
+					jsonpCallback: 'placeAutocomplete',
+					data: {
+  						q: request.term,
+                                	        country: 'nz',
+                                        	lat: -41.2889,
+                                        	lon: 174.7772
+					},
+					success: function( data ) {
+						response( $.map( data, function( item ) {
+							return {
+								label: (item.address + " (" + item.classification + "/" + item.type + ")"),
+								value: item.address,
+								osmId: item.osmId,
+								osmType: item.osmType
+							}
+						}));
+					}
+				});
+			},			
+			select: function( event, ui ) {
+				$("#selectedGeocode").val( ui.item ? ui.item.osmId + "/" + ui.item.osmType : "");
+			}
+		});
+	}
 });
