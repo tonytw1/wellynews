e31015d804cf5d45d07536722e470f63a1cd872b
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-05-20 18:42:59 +0100

    Back hauled from live.

--- elasticsearch/mappings.bash
+++ elasticsearch/mappings.bash
@@ -1,13 +1,149 @@
-curl -XPUT 'http://localhost:9200/searchwellington/resources/_mapping' -d '
-{ "content" : { "properties" : { 
-	"id" : {"type" : "string", "index" : "not_analyzed" },
-   	"type" : {"type" : "string", "index" : "not_analyzed" },
-   	"name" : {"type" : "string", "index" : "not_analyzed" },
-   	"tags.id" : {"type" : "string", "index" : "not_analyzed" },
-   	"publisherName" : {"type" : "string", "index" : "not_analyzed" },
-   	"place.osmId.id" : {"type" : "string", "index" : "not_analyzed" },
-   	"place.osmId.type" : {"type" : "string", "index" : "not_analyzed" },
-   	"location" : {"type" : "geo_point"},
-   	"urlWords" : {"type" : "string", "index" : "not_analyzed" }   	
-	} } }
+curl -XPUT 'http://localhost:9200/searchwellington/resources/_mapping' -d '{
+         "resources" : {
+            "properties" : {
+               "tags.id" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "held" : {
+                  "type" : "boolean"
+               },
+               "date" : {
+                  "format" : "dateOptionalTime",
+                  "type" : "date"
+               },
+               "acceptedByProfilename" : {
+                  "type" : "string"
+               },
+               "place" : {
+                  "properties" : {
+                     "latLong" : {
+                        "properties" : {
+                           "longitude" : {
+                              "type" : "double"
+                           },
+                           "latitude" : {
+                              "type" : "double"
+                           }
+                        }
+                     },
+                     "address" : {
+                        "type" : "string"
+                     },
+                     "osmId" : {
+                        "properties" : {
+                           "type" : {
+                              "type" : "string"
+                           },
+                           "id" : {
+                              "type" : "long"
+                           }
+                        }
+                     }
+                  }
+               },
+               "author" : {
+                  "type" : "string"
+               },
+               "latestItemDate" : {
+                  "type" : "long"
+               },
+               "handTags" : {
+                  "properties" : {
+                     "name" : {
+                        "type" : "string"
+                     },
+                     "id" : {
+                        "type" : "string"
+                     }
+                  }
+               },
+               "urlWords" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "url" : {
+                  "type" : "string"
+               },
+               "id" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "publisherName" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "owner" : {
+                  "type" : "string"
+               },
+               "latLong" : {
+                  "properties" : {
+                     "longitude" : {
+                        "type" : "double"
+                     },
+                     "latitude" : {
+                        "type" : "double"
+                     }
+                  }
+               },
+               "location" : {
+                  "type" : "geo_point"
+               },
+               "name" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "frontendImage" : {
+                  "properties" : {
+                     "url" : {
+                        "type" : "string"
+                     }
+                  }
+               },
+               "description" : {
+                  "type" : "string"
+               },
+               "tags" : {
+                  "properties" : {
+                     "name" : {
+                        "type" : "string"
+                     },
+                     "id" : {
+                        "type" : "string"
+                     }
+                  }
+               },
+               "imageUrl" : {
+                  "type" : "string"
+               },
+               "acceptedFromFeedName" : {
+                  "type" : "string"
+               },
+               "accepted" : {
+                  "type" : "long"
+               },
+               "type" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "httpStatus" : {
+                  "type" : "long"
+               },
+               "place.osmId.id" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "webUrl" : {
+                  "type" : "string"
+               },
+               "place.osmId.type" : {
+                  "index" : "not_analyzed",
+                  "type" : "string"
+               },
+               "headline" : {
+                  "type" : "string"
+               }
+            }
+         }
+}
 '
