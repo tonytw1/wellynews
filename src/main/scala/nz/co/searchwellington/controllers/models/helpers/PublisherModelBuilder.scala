5832f4fe9cb50098ab5641c2ac54755caaab33c8
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-06-09 22:11:18 +0100

    Moves to Scala.

---
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/PublisherModelBuilder.scala
@@ -0,0 +1,95 @@
+package nz.co.searchwellington.controllers.models.helpers
+
+import java.util.List
+import javax.servlet.http.HttpServletRequest
+
+import nz.co.searchwellington.controllers.models.GeotaggedNewsitemExtractor
+import nz.co.searchwellington.controllers.{RelatedTagsService, RssUrlBuilder}
+import nz.co.searchwellington.model.frontend.{FrontendNewsitem, FrontendResource, FrontendWebsite}
+import nz.co.searchwellington.model.{Tag, TagContentCount, Website}
+import nz.co.searchwellington.repositories.ContentRetrievalService
+import nz.co.searchwellington.urls.UrlBuilder
+import nz.co.searchwellington.views.GeocodeToPlaceMapper
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.ModelAndView
+
+@Component class PublisherModelBuilder @Autowired() (rssUrlBuilder: RssUrlBuilder,
+                                                     relatedTagsService: RelatedTagsService,
+                                                     contentRetrievalService: ContentRetrievalService,
+                                                     urlBuilder: UrlBuilder,
+                                                     geotaggedNewsitemExtractor: GeotaggedNewsitemExtractor,
+                                                     geocodeToPlaceMapper: GeocodeToPlaceMapper,
+                                                     commonAttributesModelBuilder: CommonAttributesModelBuilder){
+
+  private var logger: Logger = Logger.getLogger(classOf[PublisherModelBuilder])
+
+  def isValid(request: HttpServletRequest): Boolean = {
+    val tag: Tag = request.getAttribute("tag").asInstanceOf[Tag]
+    val publisher: Website = request.getAttribute("publisher").asInstanceOf[Website]
+    val isPublisherPage: Boolean = publisher != null && tag == null
+    return isPublisherPage
+  }
+
+  def populateContentModel(request: HttpServletRequest): ModelAndView = {
+    if (isValid(request)) {
+      logger.info("Building publisher page model")
+      val publisher: Website = request.getAttribute("publisher").asInstanceOf[Website]
+      val page: Int = commonAttributesModelBuilder.getPage(request)
+      return populatePublisherPageModelAndView(publisher, page)
+    }
+    return null
+  }
+
+  def populateExtraModelContent(request: HttpServletRequest, mv: ModelAndView) {
+    val publisher: Website = request.getAttribute("publisher").asInstanceOf[Website]
+    mv.addObject("feeds", contentRetrievalService.getPublisherFeeds(publisher))
+    mv.addObject("watchlist", contentRetrievalService.getPublisherWatchlist(publisher))
+    populateGeotaggedItems(mv)
+    val relatedTagLinks: List[TagContentCount] = relatedTagsService.getRelatedLinksForPublisher(publisher)
+    if (relatedTagLinks.size > 0) {
+      mv.addObject("related_tags", relatedTagLinks)
+    }
+    mv.addObject("latest_newsitems", contentRetrievalService.getLatestNewsitems(5))
+  }
+
+  def getViewName(mv: ModelAndView): String = {
+    return "publisher"
+  }
+
+  private def populatePublisherPageModelAndView(publisher: Website, page: Int): ModelAndView = {
+    val mv: ModelAndView = new ModelAndView
+    mv.addObject("heading", publisher.getName)
+    mv.addObject("description", publisher.getName + " newsitems")
+    mv.addObject("link", urlBuilder.getPublisherUrl(publisher.getName))
+    val frontendPublisher: FrontendWebsite = new FrontendWebsite
+    frontendPublisher.setName(publisher.getName)
+    frontendPublisher.setUrlWords(publisher.getUrlWords)
+    frontendPublisher.setUrl(publisher.getUrl)
+    if (publisher.getGeocode != null) {
+      frontendPublisher.setPlace(geocodeToPlaceMapper.mapGeocodeToPlace(publisher.getGeocode))
+    }
+    mv.addObject("publisher", frontendPublisher)
+    mv.addObject("location", frontendPublisher.getPlace)
+    val startIndex: Int = commonAttributesModelBuilder.getStartIndex(page)
+    val mainContentTotal: Long = contentRetrievalService.getPublisherNewsitemsCount(publisher)
+    if (mainContentTotal > 0) {
+      val publisherNewsitems: List[FrontendResource] = contentRetrievalService.getPublisherNewsitems(publisher, CommonAttributesModelBuilder.MAX_NEWSITEMS, startIndex)
+      mv.addObject("main_content", publisherNewsitems)
+      commonAttributesModelBuilder.setRss(mv, rssUrlBuilder.getRssTitleForPublisher(publisher), rssUrlBuilder.getRssUrlForPublisher(publisher))
+      commonAttributesModelBuilder.populatePagination(mv, startIndex, mainContentTotal)
+    }
+    return mv
+  }
+
+  @SuppressWarnings(Array("unchecked")) private def populateGeotaggedItems(mv: ModelAndView) {
+    val mainContent: List[FrontendNewsitem] = mv.getModel.get("main_content").asInstanceOf[List[FrontendNewsitem]]
+    if (mainContent != null) {
+      val geotaggedNewsitems: List[FrontendNewsitem] = geotaggedNewsitemExtractor.extractGeotaggedItems(mainContent)
+      if (!geotaggedNewsitems.isEmpty) {
+        mv.addObject("geocoded", geotaggedNewsitems)
+      }
+    }
+  }
+}
---
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/PublisherModelBuilderTest.scala
@@ -0,0 +1,62 @@
+package nz.co.searchwellington.controllers.models.helpers
+
+import java.util
+import java.util.List
+
+import nz.co.searchwellington.controllers.models.GeotaggedNewsitemExtractor
+import nz.co.searchwellington.controllers.{RelatedTagsService, RssUrlBuilder}
+import nz.co.searchwellington.model.Website
+import nz.co.searchwellington.model.frontend.{FrontendNewsitem, FrontendResource}
+import nz.co.searchwellington.repositories.ContentRetrievalService
+import nz.co.searchwellington.urls.UrlBuilder
+import nz.co.searchwellington.views.GeocodeToPlaceMapper
+import org.junit.Assert.assertEquals
+import org.junit.{Before, Test}
+import org.mockito.{Mock, Mockito, MockitoAnnotations}
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.web.servlet.ModelAndView
+import uk.co.eelpieconsulting.common.geo.model.Place
+
+class PublisherModelBuilderTest {
+
+  @Mock private[models] var rssUrlBuilder: RssUrlBuilder = null
+  @Mock private[models] var urlBuilder: UrlBuilder = null
+  @Mock private[models] var relatedTagsService: RelatedTagsService = null
+  @Mock private[models] var contentRetrievalService: ContentRetrievalService = null
+  @Mock private[models] var geotaggedNewsitemExtractor: GeotaggedNewsitemExtractor = null
+  @Mock private[models] var geocodeToPlaceMapper: GeocodeToPlaceMapper = null
+  @Mock private[models] var commonAttributesModelBuilder: CommonAttributesModelBuilder = null
+  @Mock private[models] var publisher: Website = null
+  @Mock private[models] var newsitem: FrontendNewsitem = null
+  @Mock private[models] var geotaggedNewsitem: FrontendNewsitem = null
+  @Mock private[models] var geotag: Place = null
+
+  private[models] var request: MockHttpServletRequest = null
+
+  @Before def setup {
+    MockitoAnnotations.initMocks(this)
+    request = new MockHttpServletRequest
+    request.setAttribute("publisher", publisher)
+    Mockito.when(geotaggedNewsitem.getPlace).thenReturn(geotag)
+  }
+
+  @SuppressWarnings(Array("unchecked"))
+  @Test
+  @throws(classOf[Exception])
+  def shouldHightlightPublishersGeotaggedContent {
+    val publisherNewsitems: List[FrontendNewsitem] = new util.ArrayList();
+    publisherNewsitems.add(newsitem)
+    publisherNewsitems.add(geotaggedNewsitem)
+    val geotaggedNewsitems: List[FrontendNewsitem] = new util.ArrayList();
+    geotaggedNewsitems.add(geotaggedNewsitem)
+    Mockito.when(geotaggedNewsitemExtractor.extractGeotaggedItems(publisherNewsitems)).thenReturn(geotaggedNewsitems)
+    val modelBuilder = new PublisherModelBuilder(rssUrlBuilder, relatedTagsService, contentRetrievalService, urlBuilder, geotaggedNewsitemExtractor, geocodeToPlaceMapper, commonAttributesModelBuilder)
+    val mv: ModelAndView = new ModelAndView
+    mv.addObject("main_content", publisherNewsitems)
+    modelBuilder.populateExtraModelContent(request, mv)
+    val geotaggedPublisherNewsitems: List[FrontendResource] = mv.getModel.get("geocoded").asInstanceOf[List[FrontendResource]]
+    assertEquals(geotaggedPublisherNewsitems, geotaggedNewsitems)
+    assertEquals(geotaggedNewsitem, geotaggedPublisherNewsitems.get(0))
+  }
+
+}
