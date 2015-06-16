63b54813761549eda57a6a8189bd80a2e4f62dd2
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-06-16 08:50:28 +0100

    Moved to Scala.

---
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/PublisherTagCombinerModelBuilder.scala
@@ -0,0 +1,84 @@
+package nz.co.searchwellington.controllers.models.helpers
+
+import java.util.List
+import javax.servlet.http.HttpServletRequest
+import nz.co.searchwellington.controllers.RelatedTagsService
+import nz.co.searchwellington.controllers.RssUrlBuilder
+import nz.co.searchwellington.controllers.models.helpers.CommonAttributesModelBuilder
+import nz.co.searchwellington.model.Tag
+import nz.co.searchwellington.model.TagContentCount
+import nz.co.searchwellington.model.Website
+import nz.co.searchwellington.model.frontend.FrontendResource
+import nz.co.searchwellington.model.frontend.FrontendWebsite
+import nz.co.searchwellington.repositories.ContentRetrievalService
+import nz.co.searchwellington.urls.UrlBuilder
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.ModelAndView
+
+@Component object PublisherTagCombinerModelBuilder {
+  private[models] var logger: Logger = Logger.getLogger(classOf[PublisherTagCombinerModelBuilder])
+}
+
+@Component class PublisherTagCombinerModelBuilder extends ModelBuilder {
+  private var contentRetrievalService: ContentRetrievalService = null
+  private var rssUrlBuilder: RssUrlBuilder = null
+  private var urlBuilder: UrlBuilder = null
+  private var relatedTagsService: RelatedTagsService = null
+  private var commonAttributesModelBuilder: CommonAttributesModelBuilder = null
+
+  @Autowired def this(contentRetrievalService: ContentRetrievalService, rssUrlBuilder: RssUrlBuilder, urlBuilder: UrlBuilder, relatedTagsService: RelatedTagsService, commonAttributesModelBuilder: CommonAttributesModelBuilder) {
+    this()
+    this.contentRetrievalService = contentRetrievalService
+    this.rssUrlBuilder = rssUrlBuilder
+    this.urlBuilder = urlBuilder
+    this.relatedTagsService = relatedTagsService
+    this.commonAttributesModelBuilder = commonAttributesModelBuilder
+  }
+
+  def isValid(request: HttpServletRequest): Boolean = {
+    val tag: Tag = request.getAttribute("tag").asInstanceOf[Tag]
+    val publisher: Website = request.getAttribute("publisher").asInstanceOf[Website]
+    return publisher != null && tag != null
+  }
+
+  def populateContentModel(request: HttpServletRequest): ModelAndView = {
+    if (isValid(request)) {
+      PublisherTagCombinerModelBuilder.logger.info("Building publisher tag combiner page model")
+      val tag: Tag = request.getAttribute("tag").asInstanceOf[Tag]
+      val publisher: Website = request.getAttribute("publisher").asInstanceOf[Website]
+      val mv: ModelAndView = new ModelAndView
+      val frontendPublisher: FrontendWebsite = new FrontendWebsite
+      frontendPublisher.setName(publisher.getName)
+      frontendPublisher.setUrlWords(publisher.getUrlWords)
+      mv.addObject("publisher", frontendPublisher)
+      mv.addObject("heading", publisher.getName + " and " + tag.getDisplayName)
+      mv.addObject("description", "")
+      mv.addObject("link", urlBuilder.getPublisherCombinerUrl(publisher.getName, tag))
+      populatePublisherTagCombinerNewsitems(mv, publisher, tag)
+      return mv
+    }
+    return null
+  }
+
+  def populateExtraModelContent(request: HttpServletRequest, mv: ModelAndView) {
+    val publisher: Website = request.getAttribute("publisher").asInstanceOf[Website]
+    val relatedTagLinks: List[TagContentCount] = relatedTagsService.getRelatedLinksForPublisher(publisher)
+    if (relatedTagLinks.size > 0) {
+      mv.addObject("related_tags", relatedTagLinks)
+    }
+  }
+
+  def getViewName(mv: ModelAndView): String = {
+    return "publisherCombiner"
+  }
+
+  private def populatePublisherTagCombinerNewsitems(mv: ModelAndView, publisher: Website, tag: Tag) {
+    val publisherNewsitems: List[FrontendResource] = contentRetrievalService.getPublisherTagCombinerNewsitems(publisher, tag, CommonAttributesModelBuilder.MAX_NEWSITEMS)
+    mv.addObject("main_content", publisherNewsitems)
+    if (publisherNewsitems.size > 0) {
+      commonAttributesModelBuilder.setRss(mv, rssUrlBuilder.getRssTitleForPublisherCombiner(publisher, tag), rssUrlBuilder.getRssUrlForPublisherCombiner(publisher, tag))
+    }
+  }
+}
