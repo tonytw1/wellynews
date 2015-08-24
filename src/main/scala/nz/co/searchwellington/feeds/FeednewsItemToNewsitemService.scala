e95a91f10c3196d16c8c97bf8344f9764c506f75
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-08-24 22:01:28 +0100

    Clean up.

--- src/main/scala/nz/co/searchwellington/feeds/FeednewsItemToNewsitemService.scala
+++ src/main/scala/nz/co/searchwellington/feeds/FeednewsItemToNewsitemService.scala
@@ -9,26 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import uk.co.eelpieconsulting.common.geo.model.Place
 
-@Component object FeednewsItemToNewsitemService {
+@Component
+class FeednewsItemToNewsitemService @Autowired() (textTrimmer: TextTrimmer, placeToGeocodeMapper: PlaceToGeocodeMapper) {
+
   private val MAXIMUM_BODY_LENGTH: Int = 400
-}
-
-@Component class FeednewsItemToNewsitemService {
-  private var placeToGeocodeMapper: PlaceToGeocodeMapper = null
-  private var textTrimmer: TextTrimmer = null
-
-  @Autowired def this(textTrimmer: TextTrimmer, placeToGeocodeMapper: PlaceToGeocodeMapper) {
-    this()
-    this.textTrimmer = textTrimmer
-    this.placeToGeocodeMapper = placeToGeocodeMapper
-  }
 
   def makeNewsitemFromFeedItem(feed: Feed, feedNewsitem: FrontendFeedNewsitem): Newsitem = {
-    val description: String = composeDescription(feedNewsitem)
-    val newsitem: Newsitem = new NewsitemImpl(0, feedNewsitem.getName, feedNewsitem.getUrl, description, feedNewsitem.getDate, feed.getPublisher, new HashSet[DiscoveredFeed])
+
+    val newsitem: Newsitem = new NewsitemImpl(0, feedNewsitem.getName, feedNewsitem.getUrl, composeDescription(feedNewsitem), feedNewsitem.getDate, feed.getPublisher, new HashSet[DiscoveredFeed])
     newsitem.setImage(if (feedNewsitem.getFrontendImage != null) new Image(feedNewsitem.getFrontendImage.getUrl, null) else null)
     newsitem.setFeed(feed)
     newsitem.setPublisher(feed.getPublisher)
+
     val place: Place = feedNewsitem.getPlace
     if (place != null) {
       newsitem.setGeocode(placeToGeocodeMapper.mapPlaceToGeocode(place))
@@ -41,7 +33,8 @@ import uk.co.eelpieconsulting.common.geo.model.Place
 
   private def composeDescription(feedNewsitem: FrontendFeedNewsitem): String = {
     var description: String = if (feedNewsitem.getDescription != null) feedNewsitem.getDescription else ""
-    description = textTrimmer.trimToCharacterCount(description, FeednewsItemToNewsitemService.MAXIMUM_BODY_LENGTH)
+    description = textTrimmer.trimToCharacterCount(description, MAXIMUM_BODY_LENGTH)
     return description
   }
+
 }
