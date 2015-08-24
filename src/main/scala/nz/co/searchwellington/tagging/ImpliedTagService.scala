515d2b4e12d76c2d4b22df2ba5dc4a21deb216a0
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-08-24 22:01:18 +0100

    Moves to Scala - gets smaller.

---
+++ src/main/scala/nz/co/searchwellington/tagging/ImpliedTagService.scala
@@ -0,0 +1,15 @@
+package nz.co.searchwellington.tagging
+
+import nz.co.searchwellington.model.{Newsitem, Resource, Tag}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class ImpliedTagService @Autowired() (taggingReturnsOfficerService: TaggingReturnsOfficerService) {
+
+  def alreadyHasTag(resource: Resource, tag: Tag): Boolean = {
+    val isNewsitemWhosPublisherAlreadyHasThisTag = resource.getType == "N" && resource.asInstanceOf[Newsitem].getPublisher != null && taggingReturnsOfficerService.getHandTagsForResource(resource.asInstanceOf[Newsitem].getPublisher).contains(tag)
+    return isNewsitemWhosPublisherAlreadyHasThisTag || taggingReturnsOfficerService.getHandTagsForResource(resource).contains(tag)
+  }
+  
+}
