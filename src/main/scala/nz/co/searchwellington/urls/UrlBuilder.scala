4c2634fbb5fafcc68b88f05040e9a33bdc3643aa
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-08-24 22:01:38 +0100

    Bug fix.

--- src/main/scala/nz/co/searchwellington/urls/UrlBuilder.scala
+++ src/main/scala/nz/co/searchwellington/urls/UrlBuilder.scala
@@ -38,10 +38,6 @@ class UrlBuilder @Autowired() (siteInformation: SiteInformation, urlWordsGenerat
     return siteInformation.getUrl + "/feed/" + feed.getUrlWords
   }
 
-  def getFeedUrl(feed: Feed): String = {
-    return siteInformation.getUrl + "/feed/" + feed.getUrlWords
-  }
-
   def getFeedUrlFromFeedName(feedname: String): String = {
     return siteInformation.getUrl + "/feed/" + urlWordsGenerator.makeUrlWordsFromName(feedname)
   }
