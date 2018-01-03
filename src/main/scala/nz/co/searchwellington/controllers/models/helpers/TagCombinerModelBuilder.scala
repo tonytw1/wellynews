e45463341b52faf5b12baff7968d298bb49f30df
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-01-03 08:00:49 +0000

    Breaking opinionated change; isValid should really be an Option return on the model builder?

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/TagCombinerModelBuilder.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/TagCombinerModelBuilder.scala
@@ -16,61 +16,68 @@ import org.springframework.web.servlet.ModelAndView
 @Component class TagCombinerModelBuilder @Autowired()(contentRetrievalService: ContentRetrievalService, rssUrlBuilder: RssUrlBuilder, urlBuilder: UrlBuilder, relatedTagsService: RelatedTagsService, commonAttributesModelBuilder: CommonAttributesModelBuilder) extends ModelBuilder {
 
   def isValid(request: HttpServletRequest): Boolean = {
-    val tags: List[Tag] = request.getAttribute("tags").asInstanceOf[List[Tag]]
+    val tags = request.getAttribute("tags").asInstanceOf[List[Tag]]
     val isTagCombinerPage: Boolean = tags != null && tags.size == 2
     return isTagCombinerPage
   }
 
-  def populateContentModel(request: HttpServletRequest): ModelAndView = {
-    if (isValid(request)) {
-      val tags: List[Tag] = request.getAttribute("tags").asInstanceOf[List[Tag]]
-      val page: Int = commonAttributesModelBuilder.getPage(request)
-      return populateTagCombinerModelAndView(tags, page)
+  def populateContentModel(request: HttpServletRequest): Option[ModelAndView] = {
+
+    def populateTagCombinerModelAndView(tags: List[Tag], page: Int): Option[ModelAndView] = {
+      val startIndex = commonAttributesModelBuilder.getStartIndex(page)
+      val totalNewsitemCount = contentRetrievalService.getTaggedNewsitemsCount(tags)
+
+      if (startIndex > totalNewsitemCount) {
+        None
+
+      } else {
+        val firstTag = tags.get(0)
+        val secondTag = tags.get(1)
+
+        val mv = new ModelAndView
+        mv.addObject("tag", firstTag)
+        mv.addObject("tags", tags)
+        mv.addObject("heading", firstTag.getDisplayName + " and " + secondTag.getDisplayName)
+        mv.addObject("description", "Items tagged with " + firstTag.getDisplayName + " and " + secondTag.getDisplayName + ".")
+        mv.addObject("link", urlBuilder.getTagCombinerUrl(firstTag, secondTag))
+        if (totalNewsitemCount > 0) {
+          commonAttributesModelBuilder.populatePagination(mv, startIndex, totalNewsitemCount)
+          val taggedNewsitems: List[FrontendResource] = contentRetrievalService.getTaggedNewsitems(tags, startIndex, CommonAttributesModelBuilder.MAX_NEWSITEMS)
+          mv.addObject("main_content", taggedNewsitems)
+          commonAttributesModelBuilder.setRss(mv, rssUrlBuilder.getRssTitleForTagCombiner(tags.get(0), tags.get(1)), rssUrlBuilder.getRssUrlForTagCombiner(tags.get(0), tags.get(1)))
+          Some(mv)
+        }
+      }
     }
-    return null
+
+    if (isValid(request)) {
+      val tags = request.getAttribute("tags").asInstanceOf[List[Tag]]
+      val page = commonAttributesModelBuilder.getPage(request)
+      populateTagCombinerModelAndView(tags, page)
+    } else {
+      None
+    }
   }
 
   def populateExtraModelContent(request: HttpServletRequest, mv: ModelAndView) {
-    val tags: List[Tag] = request.getAttribute("tags").asInstanceOf[List[Tag]]
-    val tag: Tag = tags.get(0)
-    mv.addObject("related_tags", relatedTagsService.getRelatedLinksForTag(tag, 8))
-    mv.addObject("latest_news", contentRetrievalService.getLatestWebsites(5))
-    val taggedWebsites: List[FrontendResource] = contentRetrievalService.getTaggedWebsites(new HashSet[Tag](tags), CommonAttributesModelBuilder.MAX_WEBSITES)
-    mv.addObject("websites", taggedWebsites)
+    val tags = request.getAttribute("tags").asInstanceOf[List[Tag]]
+    if (tags.nonEmpty) {
+      val tag = tags.get(0)
+      mv.addObject("related_tags", relatedTagsService.getRelatedLinksForTag(tag, 8))
+      mv.addObject("latest_news", contentRetrievalService.getLatestWebsites(5))
+      val taggedWebsites = contentRetrievalService.getTaggedWebsites(new HashSet[Tag](tags), CommonAttributesModelBuilder.MAX_WEBSITES)
+      mv.addObject("websites", taggedWebsites)
+    }
   }
 
   def getViewName(mv: ModelAndView): String = {
-    val taggedNewsitemsCount: Long = mv.getModel.get("main_content_total").asInstanceOf[Long]
-    val taggedWebsites: List[Resource] = mv.getModel.get("websites").asInstanceOf[List[Resource]]
+    val taggedNewsitemsCount = mv.getModel.get("main_content_total").asInstanceOf[Long]
+    val taggedWebsites = mv.getModel.get("websites").asInstanceOf[List[Resource]]
     val isOneContentType: Boolean = taggedNewsitemsCount == 0 || taggedWebsites.size == 0
     if (isOneContentType) {
       return "tagCombinedOneContentType"
     }
     return "tag"
-  }
-
-  private def populateTagCombinerModelAndView(tags: List[Tag], page: Int): ModelAndView = {
-    val startIndex: Int = commonAttributesModelBuilder.getStartIndex(page)
-    val totalNewsitemCount: Long = contentRetrievalService.getTaggedNewsitemsCount(tags)
-    if (startIndex > totalNewsitemCount) {
-      return null
-    }
-    val firstTag: Tag = tags.get(0)
-    val secondTag: Tag = tags.get(1)
-    val mv: ModelAndView = new ModelAndView
-    mv.addObject("tag", firstTag)
-    mv.addObject("tags", tags)
-    mv.addObject("heading", firstTag.getDisplayName + " and " + secondTag.getDisplayName)
-    mv.addObject("description", "Items tagged with " + firstTag.getDisplayName + " and " + secondTag.getDisplayName + ".")
-    mv.addObject("link", urlBuilder.getTagCombinerUrl(firstTag, secondTag))
-    if (totalNewsitemCount > 0) {
-      commonAttributesModelBuilder.populatePagination(mv, startIndex, totalNewsitemCount)
-      val taggedNewsitems: List[FrontendResource] = contentRetrievalService.getTaggedNewsitems(tags, startIndex, CommonAttributesModelBuilder.MAX_NEWSITEMS)
-      mv.addObject("main_content", taggedNewsitems)
-      commonAttributesModelBuilder.setRss(mv, rssUrlBuilder.getRssTitleForTagCombiner(tags.get(0), tags.get(1)), rssUrlBuilder.getRssUrlForTagCombiner(tags.get(0), tags.get(1)))
-      return mv
-    }
-    return null
   }
 
 }
