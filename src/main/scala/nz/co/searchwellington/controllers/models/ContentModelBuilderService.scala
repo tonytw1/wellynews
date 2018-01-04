8c0cc6fbfbee02a0cd42859c9e6022284305cb7a
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-01-04 07:57:22 +0000

    Vaguely more Scala like.

--- src/main/scala/nz/co/searchwellington/controllers/models/ContentModelBuilderService.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/ContentModelBuilderService.scala
@@ -15,53 +15,51 @@ import javax.servlet.http.HttpServletRequest
   private val JSON_CALLBACK_PARAMETER = "callback"
 
   def populateContentModel(request: HttpServletRequest): Option[ModelAndView] = {
-    var i = 0
 
-    while ( {
-      i < modelBuilders.length
-    }) {
-      val modelBuilder = modelBuilders(i)
-      if (modelBuilder.isValid(request)) {
-        logger.debug("Using " + modelBuilder.getClass.getName + " to serve path: " + request.getPathInfo)
-        val mv = modelBuilder.populateContentModel(request)
+    def populateJsonCallback(request: HttpServletRequest, mv: ModelAndView) = if (request.getParameter(JSON_CALLBACK_PARAMETER) != null) {
+      // TODO use CORS
+      val callback = request.getParameter(JSON_CALLBACK_PARAMETER)
+      if (jsonCallbackNameValidator.isValidCallbackName(callback)) {
+        logger.debug("Adding callback to model:" + callback)
+        mv.addObject(JSON_CALLBACK_PARAMETER, callback)
+      }
+    }
+
+    val modelBuilderToUse = modelBuilders.filter(mb => mb.isValid(request)).headOption // TODO collect first?
+
+    val x = modelBuilderToUse.fold {
+      logger.warn("No matching model builder found for path: " + request.getPathInfo)
+      None // TODO Blocks the above value
+
+    } { mb =>
+      logger.info("Using " + modelBuilder.getClass.getName + " to serve path: " + request.getPathInfo)
+      modelBuilder.populateContentModel(request).map { mv =>
         val path = request.getPathInfo
+
         if (path.endsWith("/rss")) {
           logger.debug("Selecting rss view for path: " + path)
           mv.setView(viewFactory.getRssView(mv.getModel.get("heading").asInstanceOf[String], mv.getModel.get("link").asInstanceOf[String], mv.getModel.get("description").asInstanceOf[String]))
           mv.addObject("data", mv.getModel.get("main_content"))
-          return mv
-        }
-        if (path.endsWith("/json")) {
-          logger.debug("Selecting json view for path: " + path)
-          val jsonView = viewFactory.getJsonView
-          jsonView.setDataField("main_content")
-          mv.setView(jsonView)
-          populateJsonCallback(request, mv)
-          return mv
-        }
-        if (mv != null) {
+
+        } else if (path.endsWith("/json")) {
+            logger.debug("Selecting json view for path: " + path)
+            val jsonView = viewFactory.getJsonView
+            jsonView.setDataField("main_content")
+            mv.setView(jsonView)
+            populateJsonCallback(request, mv)
+
+        } else {
           modelBuilder.populateExtraModelContent(request, mv)
           mv.setViewName(modelBuilder.getViewName(mv))
           commonModelObjectsService.populateCommonLocal(mv)
-          Some(mv)
         }
-        None
+
+        mv
       }
-      {
-        i += 1; i - 1
-      }
+
     }
 
-    logger.warn("No matching model builders found for path: " + request.getPathInfo)
-    None  // TODO Blocks the above value
-  }
-
-  private def populateJsonCallback(request: HttpServletRequest, mv: ModelAndView) = if (request.getParameter(JSON_CALLBACK_PARAMETER) != null) {
-    val callback = request.getParameter(JSON_CALLBACK_PARAMETER)
-    if (jsonCallbackNameValidator.isValidCallbackName(callback)) {
-      logger.debug("Adding callback to model:" + callback)
-      mv.addObject(JSON_CALLBACK_PARAMETER, callback)
-    }
+    x // TODO
   }
 
 }
