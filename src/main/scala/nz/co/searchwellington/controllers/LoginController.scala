4cd2f5d1b4d3ca5fdea6d4f8fd317bfd2e0e8d60
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-01-15 12:55:06 +0000

    Moves to Scala.

---
+++ src/main/scala/nz/co/searchwellington/controllers/LoginController.scala
@@ -0,0 +1,25 @@
+package nz.co.searchwellington.controllers
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.view.RedirectView
+
+@Controller class LoginController @Autowired()(urlStack: UrlStack) {
+
+  @RequestMapping(Array("/logout")) def logout(request: HttpServletRequest, response: HttpServletResponse): ModelAndView = {
+
+    def setRedirect(mv: ModelAndView, request: HttpServletRequest) {
+      mv.setView(new RedirectView(urlStack.getExitUrlFromStack(request)))
+    }
+
+    val mv = new ModelAndView
+    request.getSession.setAttribute("user", null)
+    setRedirect(mv, request)
+    mv
+  }
+
+}
