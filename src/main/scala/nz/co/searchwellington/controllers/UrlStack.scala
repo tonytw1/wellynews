243726a2e7841647a7bc2acac940f94f324a5fd7
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-01-15 12:59:10 +0000

    Moves to Scala.

---
+++ src/main/scala/nz/co/searchwellington/controllers/UrlStack.scala
@@ -0,0 +1,37 @@
+package nz.co.searchwellington.controllers
+
+import javax.servlet.http.HttpServletRequest
+
+import nz.co.searchwellington.urls.UrlBuilder
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component class UrlStack @Autowired()(urlBuilder: UrlBuilder) {
+
+  private val log = Logger.getLogger(classOf[UrlStack])
+
+  def getExitUrlFromStack(request: HttpServletRequest): String = {
+    val stackUrl = request.getSession.getAttribute("url").asInstanceOf[String]
+    if (stackUrl != null) {
+      log.debug("Stack url is: " + stackUrl)
+      urlBuilder.getHomeUrl + stackUrl
+    } else {
+      urlBuilder.getHomeUrl
+    }
+  }
+
+  def setUrlStack(request: HttpServletRequest) {
+    var url = request.getPathInfo
+    if (request.getQueryString != null) {
+      url = url + "?" + request.getQueryString
+    }
+    setUrlStack(request, url)
+  }
+
+  def setUrlStack(request: HttpServletRequest, url: String) {
+    request.getSession.setAttribute("url", url)
+    log.debug("Put url onto the stack: " + url)
+  }
+
+}
