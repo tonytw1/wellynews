3756981bb3a4dbc8d03e8f3f67d3f7689d2e0f14
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-01-30 09:54:58 +0000

    Clean ups.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/NewsitemPageModelBuilderTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/NewsitemPageModelBuilderTest.scala
@@ -12,25 +12,24 @@ import org.junit.{Before, Test}
 import org.mockito.Mockito.when
 import org.mockito.{Mock, MockitoAnnotations}
 import org.springframework.mock.web.MockHttpServletRequest
-import org.springframework.web.servlet.ModelAndView
 import uk.co.eelpieconsulting.common.geo.model.Place
 
 class NewsitemPageModelBuilderTest {
 
-  private val NEWSITEM_ID: Int = 123
+  private val NEWSITEM_ID = 123
   private val VALID_NEWSITEM_PAGE_PATH = "/wellington-city-council/2010/feb/01/something-about-rates"
 
-  @Mock private[models] var contentRetrievalService: ContentRetrievalService = null
-  @Mock private[models] var taggingReturnsOfficerService: TaggingReturnsOfficerService = null
-  @Mock private[models] var tagWidgetFactory: TagsWidgetFactory = null
-  @Mock private[models] var handTaggingDAO: HandTaggingDAO = null
-  @Mock private[models] var loggedInUserFilter: LoggedInUserFilter = null
-  @Mock private[models] var geotaggedNewsitem: FrontendNewsitem = null
-  @Mock private[models] var frontendNewsitem: FrontendNewsitem = null
-  @Mock private[models] var place: Place = null
-  @Mock private[models] var resourceDAO: HibernateResourceDAO = null
-  @Mock private[models] var newsitem: Newsitem = null
-  @Mock private var geotaggingVote: GeotaggingVote = null
+  @Mock var contentRetrievalService: ContentRetrievalService = null
+  @Mock var taggingReturnsOfficerService: TaggingReturnsOfficerService = null
+  @Mock var tagWidgetFactory: TagsWidgetFactory = null
+  @Mock var handTaggingDAO: HandTaggingDAO = null
+  @Mock var loggedInUserFilter: LoggedInUserFilter = null
+  @Mock var geotaggedNewsitem: FrontendNewsitem = null
+  @Mock var frontendNewsitem: FrontendNewsitem = null
+  @Mock var place: Place = null
+  @Mock var resourceDAO: HibernateResourceDAO = null
+  @Mock var newsitem: Newsitem = null
+  @Mock var geotaggingVote: GeotaggingVote = null
 
   private var request: MockHttpServletRequest = null
   private var builder: NewsitemPageModelBuilder = null
