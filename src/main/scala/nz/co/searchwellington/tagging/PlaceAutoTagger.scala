ea1b2ff06fcdfc6eb6f7c2ddf47c38859478223f
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-02-05 07:23:26 +0000

    Clean up.

--- src/main/scala/nz/co/searchwellington/tagging/PlaceAutoTagger.scala
+++ src/main/scala/nz/co/searchwellington/tagging/PlaceAutoTagger.scala
@@ -5,28 +5,27 @@ import nz.co.searchwellington.repositories.TagDAO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-import scala.collection.JavaConverters._
-
 @Component class PlaceAutoTagger @Autowired() (tagDAO: TagDAO) {
 
   private final val PLACES_TAG_NAME = "places"
 
   def suggestTags(resource: Resource): Set[Tag] = {
+
+    def getPlaces: Set[Tag] = {
+      tagDAO.loadTagByName(PLACES_TAG_NAME).map { placesTag =>
+        tagDAO.loadTagsByParent(placesTag.id)
+      }.getOrElse {
+        Seq()
+      }.toSet
+    }
+
+    def checkForMatchingTag(resource: Resource, tag: Tag): Boolean = {
+      val headlineMatchesTag = resource.getName.toLowerCase.contains(tag.getDisplayName.toLowerCase)
+      val bodyMatchesTag = resource.getDescription.toLowerCase.contains(tag.getDisplayName.toLowerCase)
+      headlineMatchesTag || bodyMatchesTag
+    }
+
     getPlaces.filter(p => checkForMatchingTag(resource, p))
-  }
-
-  def getPlaces: Set[Tag] = {
-    tagDAO.loadTagByName(PLACES_TAG_NAME).map { placesTag =>
-      tagDAO.loadTagsByParent(placesTag.id)
-    }.getOrElse {
-      Seq()
-    }.toSet
-  }
-
-  private def checkForMatchingTag(resource: Resource, tag: Tag): Boolean = {
-    val headlineMatchesTag: Boolean = resource.getName.toLowerCase.contains(tag.getDisplayName.toLowerCase)
-    val bodyMatchesTag: Boolean = resource.getDescription.toLowerCase.contains(tag.getDisplayName.toLowerCase)
-    headlineMatchesTag || bodyMatchesTag
   }
 
 }
