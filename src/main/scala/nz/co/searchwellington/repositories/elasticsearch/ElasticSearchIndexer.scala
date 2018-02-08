2e40969c96795e119cbfc7f2d44473a1a2fa91b1
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-02-08 08:40:05 +0000

    Can query? DSL hell.

--- src/main/scala/nz/co/searchwellington/repositories/elasticsearch/ElasticSearchIndexer.scala
+++ src/main/scala/nz/co/searchwellington/repositories/elasticsearch/ElasticSearchIndexer.scala
@@ -1,13 +1,17 @@
 package nz.co.searchwellington.repositories.elasticsearch
 
+import com.sksamuel.elastic4s.ElasticDsl.search
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.ElasticDsl._
-import com.sksamuel.elastic4s.http.HttpClient
+import com.sksamuel.elastic4s.http.search.SearchResponse
+import com.sksamuel.elastic4s.http.{HttpClient, RequestFailure, RequestSuccess}
+import com.sksamuel.elastic4s.searches._
 import nz.co.searchwellington.model.Resource
+import org.elasticsearch.action.search.SearchResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.{Duration, MILLISECONDS}
 
@@ -35,6 +39,26 @@ class ElasticSearchIndexer @Autowired()() {
 
     val result = Await.result(client.execute (bulk(indexDefinations)), Duration(10000, MILLISECONDS))
     println(result)
+  }
+
+
+  def test = {
+
+    var request: SearchDefinition = {
+      search in Index / Resources
+    }
+
+    val result: Future[Either[RequestFailure, RequestSuccess[SearchResponse]]] = client.execute (request)
+
+
+    result.map { r =>
+      val a = r
+      a.map { x =>
+
+        val y: RequestSuccess[Any] = x
+      }
+    }
+
   }
 
 }
