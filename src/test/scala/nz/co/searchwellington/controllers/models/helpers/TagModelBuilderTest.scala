82f14090f91b035f1e8beb8ddf57ec91b52cc916
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-02-16 17:56:37 +0000

    Test fixes; asJava is broken by mock Seqs.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/TagModelBuilderTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/TagModelBuilderTest.scala
@@ -1,7 +1,5 @@
 package nz.co.searchwellington.controllers.models.helpers
 
-import java.util.Arrays
-
 import nz.co.searchwellington.controllers.{RelatedTagsService, RssUrlBuilder}
 import nz.co.searchwellington.feeds.{FeedItemLocalCopyDecorator, RssfeedNewsitemService}
 import nz.co.searchwellington.model.Tag
@@ -11,7 +9,8 @@ import nz.co.searchwellington.urls.UrlBuilder
 import nz.co.searchwellington.views.GeocodeToPlaceMapper
 import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.{Before, Test}
-import org.mockito.{Mock, Mockito, MockitoAnnotations}
+import org.mockito.Mockito.when
+import org.mockito.{Mock, MockitoAnnotations}
 import org.springframework.mock.web.MockHttpServletRequest
 
 object TagModelBuilderTest {
@@ -27,7 +26,10 @@ class TagModelBuilderTest {
   @Mock var feedItemLocalCopyDecorator: FeedItemLocalCopyDecorator = null
   @Mock var geocodeToPlaceMapper: GeocodeToPlaceMapper = null
   @Mock var commonAttributesModelBuilder: CommonAttributesModelBuilder = null
-  @Mock var tagNewsitems: Seq[FrontendResource] = null
+
+  @Mock val newsitem1: FrontendResource = null
+  @Mock val newsitem2: FrontendResource = null
+
   @Mock var tag: Tag = null
 
   var request: MockHttpServletRequest = null
@@ -35,9 +37,10 @@ class TagModelBuilderTest {
 
   @Before def setup {
     MockitoAnnotations.initMocks(this)
+
     modelBuilder = new TagModelBuilder(rssUrlBuilder, urlBuilder, relatedTagsService, rssfeedNewsitemService, contentRetrievalService, feedItemLocalCopyDecorator, geocodeToPlaceMapper, commonAttributesModelBuilder)
     request = new MockHttpServletRequest
-    Mockito.when(tag.getDisplayName).thenReturn(TagModelBuilderTest.TAG_DISPLAY_NAME)
+    when(tag.getDisplayName).thenReturn(TagModelBuilderTest.TAG_DISPLAY_NAME)
   }
 
   @Test
@@ -64,7 +67,8 @@ class TagModelBuilderTest {
   @throws(classOf[Exception])
   def tagPageHeadingShouldBeTheTagDisplayName {
     request.setAttribute("tags", Seq(tag))
-    Mockito.when(contentRetrievalService.getTaggedNewsitems(tag, 0, 30)).thenReturn(tagNewsitems)
+    val tagNewsitems = Seq(newsitem1, newsitem2) // TODO populate with content; mocking breaks asJava
+    when(contentRetrievalService.getTaggedNewsitems(tag, 0, 30)).thenReturn(tagNewsitems)
 
     val mv = modelBuilder.populateContentModel(request).get
 
@@ -75,10 +79,13 @@ class TagModelBuilderTest {
   @throws(classOf[Exception])
   def mainContentShouldBeTagNewsitems {
     request.setAttribute("tags", Seq(tag))
-    Mockito.when(contentRetrievalService.getTaggedNewsitems(tag, 0, 30)).thenReturn(tagNewsitems)
+    val tagNewsitems = Seq(newsitem1, newsitem2) // TODO populate with content; mocking breaks asJava
+    when(contentRetrievalService.getTaggedNewsitems(tag, 0, 30)).thenReturn(tagNewsitems)
 
     val mv = modelBuilder.populateContentModel(request).get
 
-    assertEquals(tagNewsitems, mv.getModel.get("main_content"))
+    import scala.collection.JavaConverters._
+    assertEquals(tagNewsitems.asJava, mv.getModel.get("main_content"))
   }
+
 }
