2de0dcdd27f0c22d22f4797aabdf0947999a6b3b
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-02-20 21:25:51 +0000

    asJava.

--- src/main/scala/nz/co/searchwellington/controllers/CommonModelObjectsService.scala
+++ src/main/scala/nz/co/searchwellington/controllers/CommonModelObjectsService.scala
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.ModelAndView
 @Component class CommonModelObjectsService @Autowired() (contentRetrievalService: ContentRetrievalService) {
 
   def populateCommonLocal(mv: ModelAndView) {
-    mv.addObject("top_level_tags", contentRetrievalService.getTopLevelTags)
-    mv.addObject("featuredTags", contentRetrievalService.getFeaturedTags)
+    import scala.collection.JavaConverters._
+    mv.addObject("top_level_tags", contentRetrievalService.getTopLevelTags.asJava)
+    mv.addObject("featuredTags", contentRetrievalService.getFeaturedTags.asJava)
   }
 
 }
