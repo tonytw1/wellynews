2e427b01a27f5cfee7e283dde75bc30e67f9eb4b
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-02-22 18:22:15 +0000

    Move to Scala.

---
+++ src/main/scala/nz/co/searchwellington/model/UrlWordsGenerator.scala
@@ -0,0 +1,30 @@
+package nz.co.searchwellington.model
+
+import nz.co.searchwellington.model.frontend.FrontendNewsitem
+import org.joda.time.DateTimeZone
+import org.springframework.stereotype.Component
+import uk.co.eelpieconsulting.common.dates.DateFormatter
+
+@Component class UrlWordsGenerator {
+
+  def makeUrlWordsFromName(name: String): String = {
+    if (name != null) {
+      val urlWords = new String(name)
+      urlWords.replaceAll("\\(.*?\\)", "").trim.replaceAll(" ", "-").replaceAll("\\s", "").replaceAll("[^\\w-]", "").replaceAll("-+", "-").toLowerCase
+    }
+    else null
+  }
+
+  def makeUrlForNewsitem(newsitem: FrontendNewsitem): String = {
+    val uri = new StringBuilder
+    if (newsitem.getPublisherName != null) uri.append("/" + makeUrlWordsFromName(newsitem.getPublisherName))
+    val dateFormatter = new DateFormatter(DateTimeZone.UTC)
+    if (newsitem.getDate != null) {
+      uri.append("/" + dateFormatter.yearMonthDayUrlStub(newsitem.getDate))
+      uri.append("/" + makeUrlWordsFromName(newsitem.getName))
+      return uri.toString
+    }
+    null
+  }
+
+}
