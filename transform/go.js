37ee9e842c9f608090731726c2b563b813e02c99
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-02-25 13:20:15 +0000

    Remap tag geocode from join table to inline.

--- transform/go.js
+++ transform/go.js
@@ -56,3 +56,17 @@ db.resource.find({}).forEach(
         }
     }
 );
+
+db.tag.find({}).forEach(
+    function(doc) {
+        var tagId = doc.id;
+        if (doc.geocode_id) {
+                var geocode = db.geocode.findOne({id: doc.geocode_id});
+                print(tagId);
+                print(geocode);
+                doc.geocode = geocode;
+                db.tag.save(doc);
+
+        }
+    }
+);
