31199fbd89b28deffb764f3c49639826561cd10b
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-22 20:54:24 +0000

    Place holder test for missing coverage.

--- src/test/scala/nz/co/searchwellington/tagging/TaggingReturnsOfficerServiceTest.scala
+++ src/test/scala/nz/co/searchwellington/tagging/TaggingReturnsOfficerServiceTest.scala
@@ -18,11 +18,12 @@ class TaggingReturnsOfficerServiceTest {
   private var aroValleyTag: Tag = null
   private var aroValleyNewsitem: Newsitem = null
 
-  @Mock private var handTaggingDAO: HandTaggingDAO  = null
+  @Mock private var handTaggingDAO: HandTaggingDAO = null
 
   private var taggingReturnsOfficerService: TaggingReturnsOfficerService = null
 
-  @Before def setUp {
+  @Before
+  def setUp {
     MockitoAnnotations.initMocks(this)
     placesTag = new TagBuilder().name("places").displayName("Places").build
     aroValleyTag = new TagBuilder().name("arovalley").displayName("Aro Valley").parent(placesTag).build
@@ -31,7 +32,8 @@ class TaggingReturnsOfficerServiceTest {
     taggingReturnsOfficerService = new TaggingReturnsOfficerService(handTaggingDAO, null) // TODO
   }
 
-  @Test def compliedTagsShouldContainAtLeastOneCopyOfEachManuallyAppliedTag {
+  @Test
+  def compliedTagsShouldContainAtLeastOneCopyOfEachManuallyAppliedTag {
     aroValleyNewsitem = Newsitem(id = "1", title = Some("Test newsitem"), description = Some(".. Student flats in the Aro Valley... Test"))
     val handTags = Seq(new HandTagging(user = null, tag = aroValleyTag))
     when(handTaggingDAO.getHandTaggingsForResource(aroValleyNewsitem)).thenReturn(handTags)
@@ -41,7 +43,8 @@ class TaggingReturnsOfficerServiceTest {
     assertTrue(taggings.get(0).tag.equals(aroValleyTag)); // TODO not a great assert
   }
 
-  @Test def indexTagsShouldContainAtLeastOneCopyOfEachManuallyAppliedTag {
+  @Test
+  def indexTagsShouldContainAtLeastOneCopyOfEachManuallyAppliedTag {
     aroValleyNewsitem = Newsitem(id = "1", title = Some("Test newsitem"), description = Some(".. Student flats in the Aro Valley... Test"))
     val handTags = Seq(new HandTagging(user = null, tag = aroValleyTag))
     when(handTaggingDAO.getHandTaggingsForResource(aroValleyNewsitem)).thenReturn(handTags)
@@ -49,6 +52,11 @@ class TaggingReturnsOfficerServiceTest {
     var indexTags: util.Set[Tag] = taggingReturnsOfficerService.getIndexTagsForResource(aroValleyNewsitem)
 
     assertTrue(indexTags.contains(aroValleyTag))
+  }
+
+  @Test
+  def shouldIncludePublishersTagsInNewsitemsIndexTags = {
+    fail()
   }
 
 }
