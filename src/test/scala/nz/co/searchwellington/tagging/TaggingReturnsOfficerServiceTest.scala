63847af16a3b763e147a02a30acc29cdb761abd7
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-23 10:25:38 +0000

    Setup first publisher index tags test.

--- src/test/scala/nz/co/searchwellington/tagging/TaggingReturnsOfficerServiceTest.scala
+++ src/test/scala/nz/co/searchwellington/tagging/TaggingReturnsOfficerServiceTest.scala
@@ -1,9 +1,7 @@
 package nz.co.searchwellington.tagging
 
-import java.util
-
 import nz.co.searchwellington.model.taggingvotes.HandTagging
-import nz.co.searchwellington.model.{Newsitem, Tag, User}
+import nz.co.searchwellington.model.{Newsitem, Tag, User, Website}
 import nz.co.searchwellington.repositories.HandTaggingDAO
 import org.junit.Assert._
 import org.junit.{Before, Test}
@@ -16,8 +14,16 @@ class TaggingReturnsOfficerServiceTest {
 
   private val placesTag = Tag(name = "places", display_name = "Places")
   private val aroValleyTag = Tag(name = "arovalley", display_name = "Aro Valley", parent = placesTag._id)
-  private val aroValleyNewsitem = Newsitem(title = Some("Test newsitem"), description = Some(".. Student flats in the Aro Valley... Test"))
+  private val educationTag = Tag(name = "education", display_name = "Education")
+
   private val taggingUser = User(name = Some("auser"))
+
+  private val victoriaUniversity = Website(title = Some("Victoria University"))
+
+  private val aroValleyNewsitem = Newsitem(title = Some("Test newsitem"),
+    description = Some(".. Student flats in the Aro Valley... Test"),
+    publisher = Some(victoriaUniversity._id)
+  )
 
   @Mock private var handTaggingDAO: HandTaggingDAO = null
 
@@ -44,14 +50,19 @@ class TaggingReturnsOfficerServiceTest {
     val handTags = Seq(new HandTagging(user = taggingUser, tag = aroValleyTag))
     when(handTaggingDAO.getHandTaggingsForResource(aroValleyNewsitem)).thenReturn(handTags)
 
-    var indexTags: util.Set[Tag] = taggingReturnsOfficerService.getIndexTagsForResource(aroValleyNewsitem)
+    var indexTags = taggingReturnsOfficerService.getIndexTagsForResource(aroValleyNewsitem)
 
     assertTrue(indexTags.contains(aroValleyTag))
   }
 
   @Test
   def shouldIncludePublishersTagsInNewsitemsIndexTags = {
-    fail()
+    when(handTaggingDAO.getHandTaggingsForResource(aroValleyNewsitem)).thenReturn(Seq(new HandTagging(user = taggingUser, tag = aroValleyTag)))
+    when(handTaggingDAO.getHandTaggingsForResource(victoriaUniversity)).thenReturn(Seq(new HandTagging(user = taggingUser, tag = educationTag)))
+
+    var indexTags = taggingReturnsOfficerService.getIndexTagsForResource(aroValleyNewsitem)
+
+    assertTrue(indexTags.contains(educationTag))
   }
 
 }
