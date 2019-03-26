311d4719cb0a44fdb79b545916610ea2dcbc4a65
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-26 09:54:56 +0000

    Clean up; removing Mock annotations gets rid of the sequencing requirement when setting up mocks.

--- src/test/scala/nz/co/searchwellington/controllers/ContentUpdateServiceTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/ContentUpdateServiceTest.scala
@@ -8,29 +8,28 @@ import nz.co.searchwellington.queues.LinkCheckerQueue
 import nz.co.searchwellington.repositories.FrontendContentUpdater
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import org.junit.{Before, Test}
-import org.mockito.Mockito.{verify, when}
-import org.mockito.{Mock, MockitoAnnotations}
+import org.mockito.Mockito.{mock, verify, when}
 import reactivemongo.bson.BSONObjectID
 
 import scala.concurrent.Future
 
 class ContentUpdateServiceTest {
-  @Mock private val mongoReposity: MongoRepository = null
-  @Mock private val linkCheckerQueue: LinkCheckerQueue = null
+
+  private val mongoReposity = mock(classOf[MongoRepository])
+  private val linkCheckerQueue = mock(classOf[LinkCheckerQueue])
+  private val frontendContentUpdater = mock(classOf[FrontendContentUpdater])
+
   private val resourceObjectId: BSONObjectID = BSONObjectID.generate()
   private val resourceId: String = UUID.randomUUID().toString
   private val exitingResource= Newsitem(id = resourceId, page = Some("http://test/abc"))
   private val updatedResource= Newsitem(id = resourceId , page = Some("http://test/123"))
   private val newResource = Website(id = "", page = Some("http://test/abc"))
 
-  @Mock private val frontendContentUpdater: FrontendContentUpdater = null
-  private var service: ContentUpdateService = null
+  private var service =  new ContentUpdateService(mongoReposity, linkCheckerQueue, frontendContentUpdater)
 
   @Before
   def setUp {
-    MockitoAnnotations.initMocks(this)
     when(mongoReposity.getResourceByObjectId(resourceObjectId)).thenReturn(Future.successful(Some(updatedResource)))
-    service = new ContentUpdateService(mongoReposity, linkCheckerQueue, frontendContentUpdater)
   }
 
   @Test
