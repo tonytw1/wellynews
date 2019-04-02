50f93b328b32a0f0428e6668595e7ed83d13c82c
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-02 22:04:39 +0100

    Unused.

--- src/main/scala/nz/co/searchwellington/repositories/HibernateBackedUserDAO.scala
+++ src/main/scala/nz/co/searchwellington/repositories/HibernateBackedUserDAO.scala
@@ -1,20 +1,11 @@
 package nz.co.searchwellington.repositories
 
-import java.util.UUID
-
 import nz.co.searchwellington.model.User
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-import scala.concurrent.Await
-import scala.concurrent.duration.{Duration, SECONDS}
-
 @Component class HibernateBackedUserDAO @Autowired() (mongoRepository: MongoRepository) {
-
-  def saveUser(user: User) {
-    //sessionFactory.getCurrentSession.saveOrUpdate(user)
-  }
 
   def deleteUser(user: User) {
     // sessionFactory.getCurrentSession.delete(user)
