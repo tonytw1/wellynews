dd439fbd51033e4b9395531bfa45371c441b76f9
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-02 21:51:06 +0100

    Clean up; constructor Autowired.

--- src/main/java/nz/co/searchwellington/utils/StandardHttpFetcher.java
+++ src/main/java/nz/co/searchwellington/utils/StandardHttpFetcher.java
@@ -1,8 +1,6 @@
 package nz.co.searchwellington.utils;
 
-import java.io.IOException;
-import java.io.InputStream;
-
+import com.google.common.base.Strings;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
@@ -10,90 +8,82 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.params.HttpClientParams;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.google.common.base.Strings;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Component
-public class StandardHttpFetcher implements HttpFetcher {	// TODO migrate to common http
+public class StandardHttpFetcher implements HttpFetcher {    // TODO migrate to common http
 
-	private static Logger log = Logger.getLogger(StandardHttpFetcher.class);
-	
+    private static Logger log = Logger.getLogger(StandardHttpFetcher.class);
+
     private static final int HTTP_TIMEOUT = 60000;
-    
-    @Value("#{config['http.useragent']}")
-    private String userAgent;
-    
-    @Value("#{config['http.proxy.hostname']}")
-    private String httpProxyHostname;
-    
-    @Value("#{config['http.proxy.port']}")
-    private int httpProxyPort;
-    
+
+    private final String userAgent;
+    private final String httpProxyHostname;
+    private final int httpProxyPort;
+
+    @Autowired
+    public StandardHttpFetcher(
+            @Value("#{config['http.useragent']}") String userAgent,
+            @Value("#{config['http.proxy.hostname']}") String httpProxyHostname,
+            @Value("#{config['http.proxy.port']}") int httpProxyPort) {
+        this.userAgent = userAgent;
+        this.httpProxyHostname = httpProxyHostname;
+        this.httpProxyPort = httpProxyPort;
+    }
+
     public HttpFetchResult httpFetch(String url) {
-    	log.info("Attempting fetch of url: " + url);
-		HttpClient client = setupClient();        
-		try {
-		    HttpMethod method = new GetMethod(url);		    
-		    method.addRequestHeader("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
-		    method.addRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			client.executeMethod(method);
-			
+        log.info("Attempting fetch of url: " + url);
+        HttpClient client = setupClient();
+        try {
+            HttpMethod method = new GetMethod(url);
+            method.addRequestHeader("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+            method.addRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            client.executeMethod(method);
+
             log.info("http status was: " + method.getStatusCode());
-			if (method.getStatusCode() == HttpStatus.SC_OK) {
-				InputStream stream = method.getResponseBodyAsStream();
-				return new HttpFetchResult(method.getStatusCode(), stream);
-			}
-			return new HttpFetchResult(method.getStatusCode(), null);
-			
-		} catch (HttpException e) {
-		    log.warn("An exception was thrown will trying to http fetch " + url + "; see debug log level");
+            if (method.getStatusCode() == HttpStatus.SC_OK) {
+                InputStream stream = method.getResponseBodyAsStream();
+                return new HttpFetchResult(method.getStatusCode(), stream);
+            }
+            return new HttpFetchResult(method.getStatusCode(), null);
+
+        } catch (HttpException e) {
+            log.warn("An exception was thrown will trying to http fetch " + url + "; see debug log level");
             log.info(e);
-            
-		} catch (IOException e) {
+
+        } catch (IOException e) {
             log.warn("An exception was thrown will trying to http fetch; see debug log level");
             log.debug(e);
-            
-		} catch (IllegalStateException e) {
-            log.warn("An exception was thrown will trying to http fetch; see debug log level");
-            log.debug(e);		        
         }
-		
-		log.info("Setting -1 status code for: " + url);
-		return new HttpFetchResult(-1, null);
-	}
-    
-	public String getUserAgent() {
-		return userAgent;
-	}
-	
-	public void setUserAgent(String userAgent) {
-		this.userAgent = userAgent;
-	}
-	
-	public void setHttpProxyHostname(String httpProxyHostname) {
-		this.httpProxyHostname = httpProxyHostname;
-	}
-	
-	public void setHttpProxyPort(int httpProxyPort) {
-		this.httpProxyPort = httpProxyPort;
-	}
-	
-	private HttpClient setupClient() {
-		final HttpClient client = new HttpClient();
-		if (!Strings.isNullOrEmpty(userAgent)) {
-			client.getParams().setParameter(HttpClientParams.USER_AGENT, userAgent);
-		}
-		
-		if (!Strings.isNullOrEmpty(httpProxyHostname)) {
-			client.getHostConfiguration().setProxy(httpProxyHostname, httpProxyPort);
-		}
-		
-		client.getParams().setParameter("http.socket.timeout", new Integer(HTTP_TIMEOUT));
-		client.getParams().setParameter("http.connection.timeout", new Integer(HTTP_TIMEOUT));
-		client.getParams().setParameter("http.protocol.content-charset", "UTF-8");
-		return client;
-	}
-	
+
+        log.info("Setting -1 status code for: " + url);
+        return new HttpFetchResult(-1, null);
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+
+    private HttpClient setupClient() {
+        final HttpClient client = new HttpClient();
+        if (!Strings.isNullOrEmpty(userAgent)) {
+            client.getParams().setParameter(HttpClientParams.USER_AGENT, userAgent);
+        }
+
+        if (!Strings.isNullOrEmpty(httpProxyHostname)) {
+            client.getHostConfiguration().setProxy(httpProxyHostname, httpProxyPort);
+        }
+
+        client.getParams().setParameter("http.socket.timeout", new Integer(HTTP_TIMEOUT));
+        client.getParams().setParameter("http.connection.timeout", new Integer(HTTP_TIMEOUT));
+        client.getParams().setParameter("http.protocol.content-charset", "UTF-8");
+        return client;
+    }
+
 }
