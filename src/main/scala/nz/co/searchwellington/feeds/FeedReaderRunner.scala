791cf33924e8344bcf22de76e71dde46c4acb783
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-02 19:00:57 +0100

    Correct Mongo feed reader user call.

--- src/main/scala/nz/co/searchwellington/feeds/FeedReaderRunner.scala
+++ src/main/scala/nz/co/searchwellington/feeds/FeedReaderRunner.scala
@@ -1,36 +1,41 @@
 package nz.co.searchwellington.feeds
 
+import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.model.{Feed, User}
-import nz.co.searchwellington.repositories.{HibernateBackedUserDAO, HibernateResourceDAO}
+import nz.co.searchwellington.repositories.HibernateResourceDAO
+import nz.co.searchwellington.repositories.mongo.MongoRepository
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
-@Component class FeedReaderRunner @Autowired()(feedReader: FeedReader, userDAO: HibernateBackedUserDAO, resourceDAO: HibernateResourceDAO) {
+import scala.concurrent.Await
+
+@Component class FeedReaderRunner @Autowired()(feedReader: FeedReader, userDAO: MongoRepository, resourceDAO: HibernateResourceDAO)
+  extends ReasonableWaits {
 
   private val log = Logger.getLogger(classOf[FeedReaderRunner])
   private val FEED_READER_PROFILE_NAME = "feedreader"
 
   @Scheduled(cron = "0 */10 * * * *")
   def readFeeds {
+
+    def readAllFeeds(feeds: Seq[Feed]): Unit = {
+      getFeedReaderUser.map { feedReaderUser =>
+        log.info("Reading " + feeds.size + " feeds as user " + feedReaderUser.name)
+        feeds.foreach { feed =>
+          feedReader.processFeed(feed, feedReaderUser)
+        }
+      }
+    }
+
     log.info("Running feed reader.")
     readAllFeeds(resourceDAO.getAllFeeds)
     log.info("Finished reading feeds.")
   }
 
-  def readAllFeeds(feeds: Seq[Feed]): Unit = {
-    getFeedReaderUser.map { feedReaderUser =>
-      log.info("Reading " + feeds.size + " feeds as user " + feedReaderUser.name)
-      feeds.foreach { feed =>
-        feedReader.processFeed(feed, feedReaderUser)
-      }
-    }
-  }
-
   private def getFeedReaderUser: Option[User] = {
-    val feedReaderUser = userDAO.getUserByProfileName(FEED_READER_PROFILE_NAME)
+    val feedReaderUser = Await.result(userDAO.getUserByProfilename(FEED_READER_PROFILE_NAME), TenSeconds)
     if (feedReaderUser.isEmpty) {
       log.warn("Feed reader could not run as no user was found with profile name: " + FEED_READER_PROFILE_NAME)
     }
