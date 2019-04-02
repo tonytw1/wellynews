2ea31b95e6f681e8e3f0fe389a48601db4e532a7
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-02 19:03:40 +0100

    Correct Mongo autotag user call.

--- src/main/scala/nz/co/searchwellington/tagging/AutoTaggingService.scala
+++ src/main/scala/nz/co/searchwellington/tagging/AutoTaggingService.scala
@@ -1,19 +1,24 @@
 package nz.co.searchwellington.tagging
 
+import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.model.Resource
-import nz.co.searchwellington.repositories.{HandTaggingDAO, HibernateBackedUserDAO}
+import nz.co.searchwellington.repositories.HandTaggingDAO
+import nz.co.searchwellington.repositories.mongo.MongoRepository
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-@Component class AutoTaggingService @Autowired() (placeAutoTagger: PlaceAutoTagger, tagHintAutoTagger: TagHintAutoTagger, handTaggingDAO: HandTaggingDAO, userDAO: HibernateBackedUserDAO) {
+import scala.concurrent.Await
+
+@Component class AutoTaggingService @Autowired()(placeAutoTagger: PlaceAutoTagger, tagHintAutoTagger: TagHintAutoTagger, handTaggingDAO: HandTaggingDAO, mongoRepository: MongoRepository)
+  extends ReasonableWaits {
 
   private var log = Logger.getLogger(classOf[AutoTaggingService])
   private val AUTOTAGGER_PROFILE_NAME = "autotagger"
 
   def autotag(resource: Resource) {
 
-    userDAO.getUserByProfileName(AUTOTAGGER_PROFILE_NAME).fold {
+    Await.result(mongoRepository.getUserByProfilename(AUTOTAGGER_PROFILE_NAME), TenSeconds).fold {
       log.warn("Could not find auto tagger user: " + AUTOTAGGER_PROFILE_NAME)
 
     } { autotagUser =>
