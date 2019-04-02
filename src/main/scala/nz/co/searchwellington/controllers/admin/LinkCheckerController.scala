dc321b685b96c2e474f3b77c150853e7518d3247
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-02 21:28:20 +0100

    Correct _id format for manual link check.

--- src/main/scala/nz/co/searchwellington/controllers/admin/LinkCheckerController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/admin/LinkCheckerController.scala
@@ -1,8 +1,6 @@
 package nz.co.searchwellington.controllers.admin
 
-import java.io.IOException
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import nz.co.searchwellington.controllers.UrlStack
 import nz.co.searchwellington.filters.AdminRequestFilter
 import nz.co.searchwellington.model.Resource
@@ -19,13 +17,12 @@ import org.springframework.web.servlet.view.RedirectView
   private val log = Logger.getLogger(classOf[LinkCheckerController])
 
   @RequestMapping(Array("/admin/linkchecker/add"))
-  @throws[IOException]
   def addToQueue(request: HttpServletRequest, response: HttpServletResponse): ModelAndView = {
     requestFilter.loadAttributesOntoRequest(request)
     if (request.getAttribute("resource") != null) {
       val resource = request.getAttribute("resource").asInstanceOf[Resource]
-      log.info("Adding resource to queue: " + resource.id + "(" + resource.id + ")")
-      queue.add(resource.id)
+      log.info("Adding resource to queue: " + resource.id + "(" + resource._id.stringify + ")")
+      queue.add(resource._id.stringify)
     } else {
       log.warn("No resource found on request; not adding to queue")
     }
