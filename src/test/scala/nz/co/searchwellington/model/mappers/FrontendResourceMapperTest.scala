587408992dc8bbb487cd2bcd478e93a104449900
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-03 17:40:18 +0100

    Appears to be the current truth; no index tags visible on the frontend.

--- src/test/scala/nz/co/searchwellington/model/mappers/FrontendResourceMapperTest.scala
+++ src/test/scala/nz/co/searchwellington/model/mappers/FrontendResourceMapperTest.scala
@@ -7,16 +7,16 @@ import nz.co.searchwellington.model.{Newsitem, Tag, UrlWordsGenerator}
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import nz.co.searchwellington.tagging.TaggingReturnsOfficerService
 import nz.co.searchwellington.views.GeocodeToPlaceMapper
-import org.junit.Assert.{assertEquals, assertFalse}
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.Test
 import org.mockito.Mockito.{mock, when}
 
 class FrontendResourceMapperTest {
 
-  val taggingReturnsOfficerService = mock(classOf[TaggingReturnsOfficerService])
-  val urlWordsGenerator = mock(classOf[UrlWordsGenerator])
-  val geocodeToPlaceMapper = mock(classOf[GeocodeToPlaceMapper])
-  val mongoRepository = mock(classOf[MongoRepository])
+  private val taggingReturnsOfficerService = mock(classOf[TaggingReturnsOfficerService])
+  private val urlWordsGenerator = mock(classOf[UrlWordsGenerator])
+  private val geocodeToPlaceMapper = mock(classOf[GeocodeToPlaceMapper])
+  private val mongoRepository = mock(classOf[MongoRepository])
 
   val mapper = new FrontendResourceMapper(taggingReturnsOfficerService, urlWordsGenerator, geocodeToPlaceMapper, mongoRepository)
 
@@ -44,21 +44,6 @@ class FrontendResourceMapperTest {
   }
 
   @Test
-  def tagsShouldBeAppliedToFrontendNewsitems(): Unit = {
-    val newsitem = new Newsitem(id = "123")
-    when(urlWordsGenerator.makeUrlForNewsitem(newsitem)).thenReturn(Some("some-url-words"))
-
-    val tag = Tag(id = UUID.randomUUID().toString, name = "123", display_name = "123")
-    when(taggingReturnsOfficerService.getIndexTagsForResource(newsitem)).thenReturn(Set(tag))
-    when(taggingReturnsOfficerService.getHandTagsForResource(newsitem)).thenReturn(Set[Tag]())
-
-    val frontendNewsitem = mapper.createFrontendResourceFrom(newsitem)
-
-    assertFalse(frontendNewsitem.tags.isEmpty)
-    assertEquals(tag.id, frontendNewsitem.tags.get(0).id)
-  }
-
-  @Test
   def handTaggingsShouldBeAppliedToFrontendNewsitems(): Unit = {
     val newsitem = new Newsitem(id = "123")
     when(urlWordsGenerator.makeUrlForNewsitem(newsitem)).thenReturn(Some("some-url-words"))
@@ -74,6 +59,20 @@ class FrontendResourceMapperTest {
 
     assertFalse(frontendNewsitem.handTags.isEmpty)
     assertEquals(tag.id, frontendNewsitem.handTags.get(0).id)
+  }
+
+  @Test
+  def indexTagsShouldNotBeAppliedToFrontendNewsitems(): Unit = {
+    val newsitem = new Newsitem(id = "123")
+    when(urlWordsGenerator.makeUrlForNewsitem(newsitem)).thenReturn(Some("some-url-words"))
+
+    val tag = Tag(id = UUID.randomUUID().toString, name = "123", display_name = "123")
+    when(taggingReturnsOfficerService.getIndexTagsForResource(newsitem)).thenReturn(Set(tag))
+    when(taggingReturnsOfficerService.getHandTagsForResource(newsitem)).thenReturn(Set[Tag]())
+
+    val frontendNewsitem = mapper.createFrontendResourceFrom(newsitem)
+
+    assertTrue(frontendNewsitem.tags.isEmpty)
   }
 
 }
