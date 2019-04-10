f2c2309abb94c204078a2ac836be35538bc7a6cb
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-10 19:39:28 +0100

    Validate for existing feed url words clash.

--- src/main/scala/nz/co/searchwellington/controllers/NewFeedController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/NewFeedController.scala
@@ -11,7 +11,7 @@ import nz.co.searchwellington.urls.UrlBuilder
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.validation.BindingResult
+import org.springframework.validation.{BindingResult, ObjectError}
 import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping, RequestMethod}
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.view.RedirectView
@@ -37,43 +37,57 @@ class NewFeedController @Autowired()(contentUpdateService: ContentUpdateService,
   @RequestMapping(value = Array("/new-feed"), method = Array(RequestMethod.POST))
   def submit(@Valid @ModelAttribute("newFeed") newFeed: NewFeed, result: BindingResult): ModelAndView = {
 
-    if (result.hasErrors) {
+    if (!result.hasErrors) {
+      log.info("Got valid new feed submission: " + newFeed)
+
+      val urlWordsFromTitle = urlWordsGenerator.makeUrlWordsFromName(newFeed.getTitle)
+
+      val existingFeedWithSameUrlWords = Await.result(mongoRepository.getFeedByUrlwords(urlWordsFromTitle), TenSeconds)
+      if (existingFeedWithSameUrlWords.nonEmpty) {
+        result.addError(new ObjectError("urlWords", "Found existing feed with same URL words"))
+      }
+
+      if (!result.hasErrors) {
+        val publisherName = if (newFeed.getPublisher.trim.nonEmpty) {
+          Some(newFeed.getPublisher.trim)
+        } else {
+          None
+        }
+        val publisher = publisherName.flatMap { publisherName =>
+          Await.result(mongoRepository.getWebsiteByName(publisherName), TenSeconds)
+        }
+
+        val owner = Option(loggedInUserFilter.getLoggedInUser)
+
+        val feed = Feed(title = Some(newFeed.getTitle),
+          page = Some(newFeed.getUrl),
+          url_words = Some(urlWordsFromTitle),
+          publisher = publisher.map(_._id),
+          acceptance = newFeed.getAcceptancePolicy,
+          owner = owner.map(_._id)
+        )
+
+        contentUpdateService.create(feed)
+        log.info("Created feed: " + feed)
+
+        feed.page.map { feedUrl =>
+          whakaokoService.createFeedSubscription(feedUrl)
+        }
+
+        new ModelAndView(new RedirectView(urlBuilder.getFeedUrl(feed)))
+
+      } else {
+        log.warn("New feed submission has errors: " + result)
+        val mv = new ModelAndView("newFeed")
+        mv.addObject("newFeed", newFeed)
+        mv
+      }
+
+    } else {
       log.warn("New feed submission has errors: " + result)
       val mv = new ModelAndView("newFeed")
       mv.addObject("newFeed", newFeed)
-      return mv
-
-    } else {
-      log.info("Got valid new feed submission: " + newFeed)
-
-
-      val publisherName = if (newFeed.getPublisher.trim.nonEmpty) {
-        Some(newFeed.getPublisher.trim)
-      } else {
-        None
-      }
-      val publisher = publisherName.flatMap { publisherName =>
-        Await.result(mongoRepository.getWebsiteByName(publisherName), TenSeconds)
-      }
-
-      val owner = Option(loggedInUserFilter.getLoggedInUser)
-
-      val feed = Feed(title = Some(newFeed.getTitle),
-        page = Some(newFeed.getUrl),
-        url_words = Some(urlWordsGenerator.makeUrlWordsFromName(newFeed.getTitle)),
-        publisher = publisher.map(_._id),
-        acceptance =  newFeed.getAcceptancePolicy,
-        owner = owner.map(_._id)
-      )
-
-      contentUpdateService.create(feed)
-      log.info("Created feed: " + feed)
-
-      feed.page.map { feedUrl =>
-        whakaokoService.createFeedSubscription(feedUrl)
-      }
-
-      new ModelAndView(new RedirectView(urlBuilder.getFeedUrl(feed)))
+      mv
     }
   }
 
