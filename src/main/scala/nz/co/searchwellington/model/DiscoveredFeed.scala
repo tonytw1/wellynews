6a67587120cf9f40562661993d7d2c2ec7063f02
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-04-21 18:37:52 +0100

    Java view getters.

--- src/main/scala/nz/co/searchwellington/model/DiscoveredFeed.scala
+++ src/main/scala/nz/co/searchwellington/model/DiscoveredFeed.scala
@@ -1,7 +1,14 @@
 package nz.co.searchwellington.model
 
 import reactivemongo.bson.BSONObjectID
+import scala.collection.JavaConverters._
 
 import scala.collection.mutable
 
-case class DiscoveredFeed(_id: BSONObjectID = BSONObjectID.generate, url: String, references: mutable.Set[String] = mutable.Set.empty)
+case class DiscoveredFeed(_id: BSONObjectID = BSONObjectID.generate, url: String, references: mutable.Set[String] = mutable.Set.empty) {
+
+  def getUrl: String = url
+
+  def getReferences: java.util.Set[String] = references.asJava
+
+}
