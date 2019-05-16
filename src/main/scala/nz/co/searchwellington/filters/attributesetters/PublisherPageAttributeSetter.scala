181251c49ee57d3226d4cf57275ce14f298846c3
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-05-16 09:22:09 +0100

    Clean up; logging.

--- src/main/scala/nz/co/searchwellington/filters/attributesetters/PublisherPageAttributeSetter.scala
+++ src/main/scala/nz/co/searchwellington/filters/attributesetters/PublisherPageAttributeSetter.scala
@@ -4,7 +4,6 @@ import java.util.regex.{Matcher, Pattern}
 
 import javax.servlet.http.HttpServletRequest
 import nz.co.searchwellington.ReasonableWaits
-import nz.co.searchwellington.repositories.HibernateResourceDAO
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,21 +11,19 @@ import org.springframework.stereotype.Component
 
 import scala.concurrent.Await
 
-@Component class PublisherPageAttributeSetter @Autowired()(var mongoRepository: MongoRepository)
+@Component class PublisherPageAttributeSetter @Autowired()(mongoRepository: MongoRepository)
   extends AttributeSetter with ReasonableWaits {
 
   private val log = Logger.getLogger(classOf[PublisherPageAttributeSetter])
   private val publisherPagePathPattern = Pattern.compile("^/(.*?)(/(comment|geotagged))?(/(edit|save|rss|json))?$")
 
   def setAttributes(request: HttpServletRequest): Boolean = {
-    log.debug("Looking for single publisher path")
     val contentMatcher: Matcher = publisherPagePathPattern.matcher(request.getPathInfo)
     if (contentMatcher.matches) {
       val `match` = contentMatcher.group(1)
       log.debug("'" + `match` + "' matches content")
       log.debug("Looking for publisher '" + `match` + "'")
       Await.result(mongoRepository.getWebsiteByUrlwords(`match`), TenSeconds).map { publisher =>
-        log.info("Setting publisher: " + publisher.title)
         request.setAttribute("publisher", publisher)
         request.setAttribute("resource", publisher)
         true
