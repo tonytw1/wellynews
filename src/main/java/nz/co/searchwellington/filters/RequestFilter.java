497d914a0edb9f565bd12a52232bf808b8451c07
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-05-31 08:59:09 +0100

    White space.

--- src/main/java/nz/co/searchwellington/filters/RequestFilter.java
+++ src/main/java/nz/co/searchwellington/filters/RequestFilter.java
@@ -21,67 +21,67 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 @Component("requestFilter")
-@Scope(value="request", proxyMode=ScopedProxyMode.TARGET_CLASS)
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class RequestFilter {
-	
-	private static Logger log = Logger.getLogger(RequestFilter.class);
-	 
-	private RequestAttributeFilter[] filters;
-	private List<AttributeSetter> attributeSetters;
 
-	public RequestFilter() {         
-    }
-    
-	@Autowired
-    public RequestFilter(CombinerPageAttributeSetter combinerPageAttributeSetter, 
-    		PublisherPageAttributeSetter publisherPageAttributeSetter,
-    		FeedAttributeSetter feedAttributeSetter,
-    		TagPageAttributeSetter tagPageAttibuteSetter,
-    		RequestAttributeFilter[] filters) {
-        
-		this.attributeSetters = Lists.newArrayList(tagPageAttibuteSetter, publisherPageAttributeSetter, feedAttributeSetter, combinerPageAttributeSetter);        
-        this.filters = filters;
-    }
-    
-	public void loadAttributesOntoRequest(HttpServletRequest request) {		
-    	for (RequestAttributeFilter filter : filters) {
-			filter.filter(request);
-		}
-    	
-    	if (isReservedPath(request.getPathInfo())) {
-    		return;
-    	}
-    	
-    	for (AttributeSetter attributeSetter : attributeSetters) {
-    		if (attributeSetter.setAttributes(request)) {    		
-    			return;
-    		}
-    	}
-    	
-		log.debug("Looking for single publisher and tag urls");       
-    }
-	
-	public RequestAttributeFilter[] getFilters() {
-		return filters;
-	}
+  private static Logger log = Logger.getLogger(RequestFilter.class);
 
-	private boolean isReservedPath(String path) {
-    	Set<String> reservedUrlWords = Sets.newHashSet();		 // TODO this wants to be in the spring config
-    	reservedUrlWords.add("/about");
-    	reservedUrlWords.add("/api");
-    	reservedUrlWords.add("/autotag");
-       	reservedUrlWords.add("/index");
-    	reservedUrlWords.add("/feeds");
-    	reservedUrlWords.add("/comment");
-    	reservedUrlWords.add("/geotagged");
-    	reservedUrlWords.add("/tags");
-    	
-    	for(String prefix : reservedUrlWords) {
-    		if (path.startsWith(prefix)) {
-    			return true;
-    		}
-    	}    	
-    	return false;
-	}
-	
+  private RequestAttributeFilter[] filters;
+  private List<AttributeSetter> attributeSetters;
+
+  public RequestFilter() {
+  }
+
+  @Autowired
+  public RequestFilter(CombinerPageAttributeSetter combinerPageAttributeSetter,
+                       PublisherPageAttributeSetter publisherPageAttributeSetter,
+                       FeedAttributeSetter feedAttributeSetter,
+                       TagPageAttributeSetter tagPageAttibuteSetter,
+                       RequestAttributeFilter[] filters) {
+
+    this.attributeSetters = Lists.newArrayList(tagPageAttibuteSetter, publisherPageAttributeSetter, feedAttributeSetter, combinerPageAttributeSetter);
+    this.filters = filters;
+  }
+
+  public void loadAttributesOntoRequest(HttpServletRequest request) {
+    for (RequestAttributeFilter filter : filters) {
+      filter.filter(request);
+    }
+
+    if (isReservedPath(request.getPathInfo())) {
+      return;
+    }
+
+    for (AttributeSetter attributeSetter : attributeSetters) {
+      if (attributeSetter.setAttributes(request)) {
+        return;
+      }
+    }
+
+    log.debug("Looking for single publisher and tag urls");
+  }
+
+  public RequestAttributeFilter[] getFilters() {
+    return filters;
+  }
+
+  private boolean isReservedPath(String path) {
+    Set<String> reservedUrlWords = Sets.newHashSet();     // TODO this wants to be in the spring config
+    reservedUrlWords.add("/about");
+    reservedUrlWords.add("/api");
+    reservedUrlWords.add("/autotag");
+    reservedUrlWords.add("/index");
+    reservedUrlWords.add("/feeds");
+    reservedUrlWords.add("/comment");
+    reservedUrlWords.add("/geotagged");
+    reservedUrlWords.add("/tags");
+
+    for (String prefix : reservedUrlWords) {
+      if (path.startsWith(prefix)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
