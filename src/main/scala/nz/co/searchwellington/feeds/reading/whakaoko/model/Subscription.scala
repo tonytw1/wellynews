7e43f462f591ad604ae36380269103b4f98e2cfd
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-06-10 22:03:23 +0100

    Optional.

--- src/main/scala/nz/co/searchwellington/feeds/reading/whakaoko/model/Subscription.scala
+++ src/main/scala/nz/co/searchwellington/feeds/reading/whakaoko/model/Subscription.scala
@@ -3,7 +3,7 @@ package nz.co.searchwellington.feeds.reading.whakaoko.model
 import org.joda.time.DateTime
 
 case class Subscription(id: String,
-                        name: String,
+                        name: Option[String],
                         channelId: String,
                         url: String,
                         lastRead: Option[DateTime],
