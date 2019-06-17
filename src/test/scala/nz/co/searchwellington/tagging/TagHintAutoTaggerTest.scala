6d811a8ba83256d066391e3d8df9a5994daf14c8
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-06-17 08:26:57 +0100

    Clean up; mock annotations.

--- src/test/scala/nz/co/searchwellington/tagging/TagHintAutoTaggerTest.scala
+++ src/test/scala/nz/co/searchwellington/tagging/TagHintAutoTaggerTest.scala
@@ -5,22 +5,16 @@ import java.util.UUID
 import nz.co.searchwellington.model.{Newsitem, TagBuilder}
 import nz.co.searchwellington.repositories.TagDAO
 import org.junit.Assert.{assertFalse, assertTrue}
-import org.junit.{Before, Test}
-import org.mockito.Mockito.when
-import org.mockito.{Mock, MockitoAnnotations}
+import org.junit.Test
+import org.mockito.Mockito.{mock, when}
 
 import scala.concurrent.Future
 
 class TagHintAutoTaggerTest {
 
-  @Mock private var tagDAO: TagDAO = null
+  private val tagDAO = mock(classOf[TagDAO])
 
-  private var tagHintAutoTagger: TagHintAutoTagger = null
-
-  @Before def setup {
-    MockitoAnnotations.initMocks(this)
-    tagHintAutoTagger = new TagHintAutoTagger(tagDAO)
-  }
+  private val tagHintAutoTagger = new TagHintAutoTagger(tagDAO)
 
   @Test def shouldMatchTitlesWhichContainAutotaggingHint {
     val tag = new TagBuilder().autotagHints("fox,animal").build
