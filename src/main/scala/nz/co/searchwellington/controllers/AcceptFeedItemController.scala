87a7cd3d80b4bd1c5fd5943f26db77b3e7e81508
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-06-19 22:30:32 +0100

    Accept end point.

--- src/main/scala/nz/co/searchwellington/controllers/AcceptFeedItemController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/AcceptFeedItemController.scala
@@ -2,7 +2,8 @@ package nz.co.searchwellington.controllers
 
 import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.feeds.reading.WhakaokoService
-import nz.co.searchwellington.model.{Feed, UrlWordsGenerator}
+import nz.co.searchwellington.feeds.{FeedReaderUpdateService, RssfeedNewsitemService}
+import nz.co.searchwellington.model.UrlWordsGenerator
 import nz.co.searchwellington.modification.ContentUpdateService
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import nz.co.searchwellington.urls.UrlBuilder
@@ -13,38 +14,49 @@ import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod}
 import org.springframework.web.servlet.ModelAndView
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 
 @Controller
 class AcceptFeedItemController @Autowired()(contentUpdateService: ContentUpdateService,
                                             mongoRepository: MongoRepository,
                                             urlWordsGenerator: UrlWordsGenerator, urlBuilder: UrlBuilder,
                                             whakaokoService: WhakaokoService,
+                                            rssfeedNewsitemService: RssfeedNewsitemService,
+                                            feedReaderUpdateService: FeedReaderUpdateService,
                                             loggedInUserFilter: LoggedInUserFilter) extends ReasonableWaits with AcceptancePolicyOptions {
 
   private val log = Logger.getLogger(classOf[AcceptFeedItemController])
 
-  /*
   @RequestMapping(value = Array("/accept-feed-item"), method = Array(RequestMethod.GET))
   def accept(feed: String, url: String): ModelAndView = {
 
+    val loggedInUser = loggedInUserFilter.getLoggedInUser // TODO map
 
-    val eventualMaybeResource = mongoRepository.getResourceById(feed)
+    val x: Future[ModelAndView] = mongoRepository.getFeedByUrlwords(feed).map { fo =>
+      fo.map { feed =>
 
-    eventualMaybeResource.map { fo =>
-      fo.map { f =>
-        f match {
-          case feed: Feed => {
-            None
+        rssfeedNewsitemService.getFeedItemsAndDetailsFor(feed).map { fis =>
+          fis.fold({ l =>
+            log.warn("Could not read feed items: " + l)
+          },
+            { r =>
+              val feedItems = r._1
 
-
-
-          }
-          case _ =>
-            None
+              feedItems.find(fi => fi.url == url).map { feedItemToAccept =>
+                feedReaderUpdateService.acceptNewsitem(loggedInUser, feedItemToAccept, feed)
+              }
+            })
         }
 
+        new ModelAndView()
+
+      }.getOrElse {
+        new ModelAndView() // TODO file not found
       }
 
-   */
-  
+    }
+
+    Await.result(x, TenSeconds)
+  }
+
 }
