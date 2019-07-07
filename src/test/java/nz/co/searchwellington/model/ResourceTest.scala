593b2352e65a2944c45d249916d50a5c270ddfca
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-07-07 10:06:58 +0100

    Tagging test.

--- src/test/java/nz/co/searchwellington/model/ResourceTest.scala
+++ src/test/java/nz/co/searchwellington/model/ResourceTest.scala
@@ -6,12 +6,21 @@ import org.junit.Assert.assertEquals
 class ResourceTest {
 
   @Test
-  def canEditCommonFieldsLikeTitle: Unit = {
+  def canEditCommonFieldsLikeTitle = {
     val website = Website(title = Some("Title"))
 
     val edited = website.copy(title = Some("New title"))
 
     assertEquals("New title", edited.title.get)
+  }
+
+  @Test
+  def canTagResources = {
+    val tag = Tag()
+    val user = User()
+    val website = Website(resource_tags = Seq(Tagging(tag_id = tag._id, user_id = user._id)))
+
+    assertEquals(tag._id, website.resource_tags.head.tag_id)
   }
 
 }
