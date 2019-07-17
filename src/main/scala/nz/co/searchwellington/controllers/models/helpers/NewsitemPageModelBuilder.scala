140ed874b0c22af3d098c74515b05986859d4471
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-07-17 08:27:29 +0100

    Delete resourceDAO get by id.

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/NewsitemPageModelBuilder.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/NewsitemPageModelBuilder.scala
@@ -1,10 +1,11 @@
 package nz.co.searchwellington.controllers.models.helpers
 
 import javax.servlet.http.HttpServletRequest
-
+import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.controllers.LoggedInUserFilter
 import nz.co.searchwellington.controllers.models.ModelBuilder
-import nz.co.searchwellington.repositories.{ContentRetrievalService, HandTaggingDAO, HibernateResourceDAO}
+import nz.co.searchwellington.repositories.mongo.MongoRepository
+import nz.co.searchwellington.repositories.{ContentRetrievalService, HandTaggingDAO}
 import nz.co.searchwellington.tagging.TaggingReturnsOfficerService
 import nz.co.searchwellington.widgets.TagsWidgetFactory
 import org.apache.log4j.Logger
@@ -13,13 +14,14 @@ import org.springframework.stereotype.Component
 import org.springframework.web.servlet.ModelAndView
 
 import scala.collection.JavaConverters._
+import scala.concurrent.Await
 
-@Component class NewsitemPageModelBuilder @Autowired() (contentRetrievalService: ContentRetrievalService,
+@Component class NewsitemPageModelBuilder @Autowired()(contentRetrievalService: ContentRetrievalService,
                                                        taggingReturnsOfficerService: TaggingReturnsOfficerService,
                                                        tagWidgetFactory: TagsWidgetFactory,
                                                        tagVoteDAO: HandTaggingDAO,
                                                        loggedInUserFilter: LoggedInUserFilter,
-                                                       resourceDAO: HibernateResourceDAO) extends ModelBuilder {
+                                                       mongoRepository: MongoRepository) extends ModelBuilder with ReasonableWaits {
 
   private val log = Logger.getLogger(classOf[NewsitemPageModelBuilder])
 
@@ -40,7 +42,7 @@ import scala.collection.JavaConverters._
         mv.addObject("geocoded", List(frontendResource).asJava)
       }
 
-      resourceDAO.loadResourceById(frontendResource.getId).map { resource => // TODO abit strange that we have to load this database object just to pass it as an argument to someone else
+      Await.result(mongoRepository.getResourceById(frontendResource.getId), TenSeconds).map { resource => // TODO abit strange that we have to load this database object just to pass it as an argument to someone else
         mv.addObject("votes", taggingReturnsOfficerService.compileTaggingVotes(resource).asJava)
         mv.addObject("geotag_votes", taggingReturnsOfficerService.getGeotagVotesForResource(resource).asJava)
         mv.addObject("tag_select", tagWidgetFactory.createMultipleTagSelect(tagVoteDAO.getHandpickedTagsForThisResourceByUser(loggedInUserFilter.getLoggedInUser, resource)))
