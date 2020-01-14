b2cd54b2ba46024725b688c304efa567fabe5312
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-01-14 21:04:50 +0000

    White space.

--- src/main/java/nz/co/searchwellington/model/Image.java
+++ src/main/java/nz/co/searchwellington/model/Image.java
@@ -3,49 +3,44 @@ package nz.co.searchwellington.model;
 import java.io.Serializable;
 
 public class Image implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private int id;
-	private String url;
-	private String description;
 
-		
-	public Image() {
-	}
+    private static final long serialVersionUID = 1L;
 
-	public Image(String url, String description) {
-		this.url = url;
-		this.description = description;
-	}
-	
-	public int getId() {
-		return id;
-	}
+    private int id;
+    private String url;
+    private String description;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public Image() {
+    }
 
+    public Image(String url, String description) {
+        this.url = url;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	
-	
-	
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
