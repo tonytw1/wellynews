6d5c7113875e65ced9d1f932338b73a1543c14a7
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-02-02 21:22:09 +0100

    Marking TODO.

--- src/main/scala/nz/co/searchwellington/controllers/models/ContentModelBuilderService.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/ContentModelBuilderService.scala
@@ -30,7 +30,7 @@ import uk.co.eelpieconsulting.common.views.ViewFactory
         } else if (path.endsWith("/json")) {
           logger.debug("Selecting json view for path: " + path)
           val jsonView = viewFactory.getJsonView
-          jsonView.setDataField("main_content")
+          jsonView.setDataField("main_content") // TODO push to a parameter of getJsonView
           mv.setView(jsonView)
           mv
 
