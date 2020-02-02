aba31605493647e7a1e57d0733e25e54790b0aee
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-02-02 19:54:33 +0100

    Mark TODOs.

--- src/main/scala/nz/co/searchwellington/controllers/ajax/BaseAjaxController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/ajax/BaseAjaxController.scala
@@ -4,6 +4,7 @@ import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.springframework.web.servlet.ModelAndView
 import uk.co.eelpieconsulting.common.views.ViewFactory
 
+// TODO should be able to dog food these end points by consuming the json put of the main publishers and tags models
 abstract class BaseAjaxController {
 
   private val TERM = "term"
