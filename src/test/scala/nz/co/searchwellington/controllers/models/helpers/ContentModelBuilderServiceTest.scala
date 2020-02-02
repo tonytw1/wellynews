4e1d0b573f1d53e3338b555a08baf19d5adc4a34
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-02-02 13:21:04 +0100

    Clean up; remove Mockito annotations.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/ContentModelBuilderServiceTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/ContentModelBuilderServiceTest.scala
@@ -1,16 +1,11 @@
 package nz.co.searchwellington.controllers.models.helpers
 
-import java.util.List
-
 import nz.co.searchwellington.controllers.CommonModelObjectsService
 import nz.co.searchwellington.controllers.models.{ContentModelBuilderService, ModelBuilder}
-import nz.co.searchwellington.model.Tag
 import org.junit.Assert.assertEquals
 import org.junit.{Before, Test}
 import org.mockito.Matchers.anyString
-import org.mockito.Mockito.when
-import org.mockito.MockitoAnnotations
-import org.mockito.Mock
+import org.mockito.Mockito.{mock, when}
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.web.servlet.ModelAndView
 import uk.co.eelpieconsulting.common.views.ViewFactory
@@ -19,47 +14,58 @@ import uk.co.eelpieconsulting.common.views.rss.RssView
 
 class ContentModelBuilderServiceTest {
 
-  @Mock private[helpers] val viewFactory: ViewFactory = null
-  @Mock private[helpers] val commonModelObjectsService: CommonModelObjectsService = null
-  private[helpers] var request: MockHttpServletRequest = null
-  @Mock private[helpers] val jsonView: JsonView = null
-  @Mock private[helpers] val rssView: RssView = null
-  @Mock private[helpers] val featuredTags: List[Tag] = null
-  @Mock private[helpers] val topLevelTags: List[Tag] = null
-  private[helpers] var validModelAndView: ModelAndView = null
-  @Mock private[helpers] val invalidModelBuilder: ModelBuilder = null
-  @Mock private[helpers] val validModelBuilder: ModelBuilder = null
-  private[helpers] var contentModelBuilderService: ContentModelBuilderService = null
+  private val viewFactory = mock(classOf[ViewFactory])
+  private val commonModelObjectsService = mock(classOf[CommonModelObjectsService])
 
+  private var request: MockHttpServletRequest = null
+
+  private[helpers] var validModelAndView: ModelAndView = mock(classOf[ModelAndView])
+
+  private val invalidModelBuilder = mock(classOf[ModelBuilder])
+  private val validModelBuilder =  mock(classOf[ModelBuilder])
 
   @Before def setup {
-    MockitoAnnotations.initMocks(this)
     request = new MockHttpServletRequest
     validModelAndView = new ModelAndView
     request.setPathInfo("/something")
-    when(invalidModelBuilder.isValid(request)).thenReturn(false)
-    when(validModelBuilder.isValid(request)).thenReturn(true)
-    when(validModelBuilder.populateContentModel(request)).thenReturn(Some(validModelAndView))
-    contentModelBuilderService = new ContentModelBuilderService(
-      viewFactory,
-      commonModelObjectsService,
-      Seq(invalidModelBuilder, validModelBuilder)
-    )
   }
 
   @Test
   def shouldDelegateModelBuildingToTheFirstBuildWhoSaysTheyAreValid {
+    when(invalidModelBuilder.isValid(request)).thenReturn(false)
+    when(validModelBuilder.isValid(request)).thenReturn(true)
+    when(validModelBuilder.populateContentModel(request)).thenReturn(Some(validModelAndView))
+
+    val contentModelBuilderService = new ContentModelBuilderService(viewFactory,
+      commonModelObjectsService,
+      Seq(invalidModelBuilder, validModelBuilder)
+    )
+
     assertEquals(Some(validModelAndView), contentModelBuilderService.populateContentModel(request))
   }
 
   @Test
   def shouldReturnNullIfNoModelBuilderWasFoundForRequest {
-    contentModelBuilderService = new ContentModelBuilderService(viewFactory, commonModelObjectsService, Seq(invalidModelBuilder))
+    when(invalidModelBuilder.isValid(request)).thenReturn(false)
+
+    val contentModelBuilderService = new ContentModelBuilderService(viewFactory, commonModelObjectsService, Seq(invalidModelBuilder))
+
     assertEquals(None, contentModelBuilderService.populateContentModel(request))
   }
 
   @Test
   def rssSuffixedRequestsShouldBeGivenTheRssView {
+    when(invalidModelBuilder.isValid(request)).thenReturn(false)
+    when(validModelBuilder.isValid(request)).thenReturn(true)
+    when(validModelBuilder.populateContentModel(request)).thenReturn(Some(validModelAndView))
+
+    val contentModelBuilderService = new ContentModelBuilderService(
+      viewFactory,
+      commonModelObjectsService,
+      Seq(invalidModelBuilder, validModelBuilder)
+    )
+
+    val rssView = mock(classOf[RssView])
     when(viewFactory.getRssView(anyString, anyString, anyString)).thenReturn(rssView)
     request.setPathInfo("/something/rss")
     assertEquals(rssView, contentModelBuilderService.populateContentModel(request).get.getView)
@@ -67,6 +73,17 @@ class ContentModelBuilderServiceTest {
 
   @Test
   def jsonSuffixedRequestsShouldBeGivenTheRssView {
+    when(invalidModelBuilder.isValid(request)).thenReturn(false)
+    when(validModelBuilder.isValid(request)).thenReturn(true)
+    when(validModelBuilder.populateContentModel(request)).thenReturn(Some(validModelAndView))
+
+    val contentModelBuilderService = new ContentModelBuilderService(
+      viewFactory,
+      commonModelObjectsService,
+      Seq(invalidModelBuilder, validModelBuilder)
+    )
+
+    val jsonView = mock(classOf[JsonView])
     when(viewFactory.getJsonView).thenReturn(jsonView)
     request.setPathInfo("/something/json")
     assertEquals(jsonView, contentModelBuilderService.populateContentModel(request).get.getView)
