443550375d0909b8e7549f9ae9d7131066330638
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-02-13 21:55:40 +0000

    Unused.

--- src/main/scala/nz/co/searchwellington/repositories/FrontendContentUpdater.scala
+++ src/main/scala/nz/co/searchwellington/repositories/FrontendContentUpdater.scala
@@ -1,15 +1,14 @@
 package nz.co.searchwellington.repositories
 
 import nz.co.searchwellington.model.Resource
-import nz.co.searchwellington.repositories.elasticsearch.{ElasticSearchIndexRebuildService, ElasticSearchIndexUpdateService}
+import nz.co.searchwellington.repositories.elasticsearch.ElasticSearchIndexRebuildService
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 import scala.concurrent.{ExecutionContext, Future}
 
-@Component class FrontendContentUpdater @Autowired()(elasticSearchIndexUpdateService: ElasticSearchIndexUpdateService,
-                                                     elasticSearchIndexRebuildService: ElasticSearchIndexRebuildService) {
+@Component class FrontendContentUpdater @Autowired()(elasticSearchIndexRebuildService: ElasticSearchIndexRebuildService) {
 
   private val log = Logger.getLogger(classOf[FrontendContentUpdater])
 
