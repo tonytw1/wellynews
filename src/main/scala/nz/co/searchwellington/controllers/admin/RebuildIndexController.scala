3b033a080543698bcbf07028ead6ef7fb95a5a4d
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-02-16 18:38:18 +0000

    Reindex all end point.

---
+++ src/main/scala/nz/co/searchwellington/controllers/admin/RebuildIndexController.scala
@@ -0,0 +1,28 @@
+package nz.co.searchwellington.controllers.admin
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import nz.co.searchwellington.controllers.AcceptFeedItemController
+import nz.co.searchwellington.repositories.elasticsearch.ElasticSearchIndexRebuildService
+import nz.co.searchwellington.repositories.mongo.MongoRepository
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class RebuildIndexController @Autowired()(mongoRepository: MongoRepository, elasticSearchIndexRebuildService: ElasticSearchIndexRebuildService) {
+  private val log = Logger.getLogger(classOf[AcceptFeedItemController])
+
+
+  @RequestMapping(Array("/admin/rebuild-index")) def prompt(request: HttpServletRequest, response: HttpServletResponse): ModelAndView = {
+    mongoRepository.getAllResourceIds().flatMap { resourceIds =>
+      elasticSearchIndexRebuildService.reindexResources(resourceIds)
+    }.map { i =>
+      log.info("Reindexed " + i + " resources.")
+    }
+
+    new ModelAndView("TODO")
+  }
+
+}
