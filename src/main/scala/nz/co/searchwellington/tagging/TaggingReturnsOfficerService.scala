096a3b1571c7860ed731b7b4b1aa889c51f5ee6b
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-02-27 08:35:09 +0000

    Imports.

--- src/main/scala/nz/co/searchwellington/tagging/TaggingReturnsOfficerService.scala
+++ src/main/scala/nz/co/searchwellington/tagging/TaggingReturnsOfficerService.scala
@@ -10,7 +10,6 @@ import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-import scala.collection.mutable
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
