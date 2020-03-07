cc373cc41cb8c65177fa54428da006504082456e
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-03-07 20:31:35 +0000

    Clean ups.

--- src/test/scala/nz/co/searchwellington/views/GoogleMapsDisplayCleanerTest.scala
+++ src/test/scala/nz/co/searchwellington/views/GoogleMapsDisplayCleanerTest.scala
@@ -1,31 +1,21 @@
 package nz.co.searchwellington.views
 
 import nz.co.searchwellington.model.{Geocode, Newsitem}
-import org.junit.Assert.{assertFalse, assertTrue, assertEquals}
-import org.junit.{Before, Test}
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
+import org.junit.Test
 
 class GoogleMapsDisplayCleanerTest {
-  private val here = new Geocode(Some("here"), Some(1.1), Some(1.1))
-  private val there = new Geocode(Some("there"),  Some(2.2), Some(2.2))
-  private val alsoHere = new Geocode(Some("here"),  Some(1.1), Some(1.1))
-  private var geocoded: Seq[Newsitem] = null
-  private var firstNewsitem: Newsitem = null
-  private var secondNewsitem: Newsitem= null
-  private var thirdNewsitem: Newsitem = null
-  private var cleaner = new GoogleMapsDisplayCleaner()
+  private val here = Geocode(Some("here"), Some(1.1), Some(1.1))
+  private val there = Geocode(Some("there"), Some(2.2), Some(2.2))
+  private val alsoHere = Geocode(Some("here"), Some(1.1), Some(1.1))
+  private val firstNewsitem = Newsitem(title = Some("First"), geocode = Some(here))
+  private val secondNewsitem = Newsitem(title = Some("Second"), geocode = Some(there))
+  private val thirdNewsitem = Newsitem(title = Some("Third"), geocode = Some(alsoHere))
+  private val geocoded = Seq(firstNewsitem, secondNewsitem, thirdNewsitem)
 
-  @Before
-  @throws[Exception]
-  def setup(): Unit = {
-    firstNewsitem = Newsitem(title = Some("First"), geocode = Some(here))
-    secondNewsitem = Newsitem(title = Some("Second"),geocode = Some(there))
-    thirdNewsitem = Newsitem(title = Some("Third"), geocode = Some(alsoHere))
-    geocoded = Seq(firstNewsitem, secondNewsitem, thirdNewsitem)
-    cleaner = new GoogleMapsDisplayCleaner
-  }
+  private val cleaner = new GoogleMapsDisplayCleaner()
 
   @Test
-  @throws[Exception]
   def shouldDedupeListByGeocodeSoThatLowerItemsDoNotOverlayEarlierOnes(): Unit = {
     val deduped = cleaner.dedupe(geocoded)
 
@@ -34,7 +24,6 @@ class GoogleMapsDisplayCleanerTest {
   }
 
   @Test
-  @throws[Exception]
   def shouldPutSelectedItemInFirst(): Unit = {
     val selected = Newsitem(geocode = Some(there))
 
