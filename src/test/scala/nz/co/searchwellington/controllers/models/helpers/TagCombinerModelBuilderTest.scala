074679e725c723f5814792c29ee1650dc460b677
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-03-11 20:50:56 +0000

    Basic test for tag combiner.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/TagCombinerModelBuilderTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/TagCombinerModelBuilderTest.scala
@@ -2,43 +2,31 @@ package nz.co.searchwellington.controllers.models.helpers
 
 import java.util.UUID
 
-import nz.co.searchwellington.controllers.{LoggedInUserFilter, RelatedTagsService, RssUrlBuilder}
-import nz.co.searchwellington.feeds.{FeedItemLocalCopyDecorator, RssfeedNewsitemService}
+import nz.co.searchwellington.ReasonableWaits
+import nz.co.searchwellington.controllers.{RelatedTagsService, RssUrlBuilder}
 import nz.co.searchwellington.model.Tag
-import nz.co.searchwellington.model.frontend.FrontendResource
-import nz.co.searchwellington.model.mappers.FrontendResourceMapper
+import nz.co.searchwellington.model.frontend.{FrontendNewsitem, FrontendResource}
 import nz.co.searchwellington.repositories.{ContentRetrievalService, TagDAO}
 import nz.co.searchwellington.urls.UrlBuilder
-import org.junit.Assert.{assertFalse, assertTrue}
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.{Before, Test}
 import org.mockito.Mockito.{mock, when}
 import org.springframework.mock.web.MockHttpServletRequest
 import reactivemongo.bson.BSONObjectID
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 
-class TagCombinerModelBuilderTest {
+class TagCombinerModelBuilderTest extends ReasonableWaits {
 
   private val contentRetrievalService = mock(classOf[ContentRetrievalService])
   private val rssUrlBuilder = mock(classOf[RssUrlBuilder])
   private val urlBuilder = mock(classOf[UrlBuilder])
   private val relatedTagsService = mock(classOf[RelatedTagsService])
-  private val rssfeedNewsitemService = mock(classOf[RssfeedNewsitemService])
-  private val feedItemLocalCopyDecorator = mock(classOf[FeedItemLocalCopyDecorator])
   private val commonAttributesModelBuilder = mock(classOf[CommonAttributesModelBuilder])
   private val tagDAO = mock(classOf[TagDAO])
-  private val frontendResourceMapper = mock(classOf[FrontendResourceMapper])
-  private val loggedInUserFilter = mock(classOf[LoggedInUserFilter])
 
-  private val newsitem1 = mock(classOf[FrontendResource])
-  private val newsitem2 = mock(classOf[FrontendResource])
-
-  private val TAG_DISPLAY_NAME = "Penguins"
-
-  private val parentId = BSONObjectID.generate
-  private val tag = Tag(_id = parentId, id = UUID.randomUUID().toString, display_name = TAG_DISPLAY_NAME)
-
-  private val loggedInUser = None
+  private val tag = Tag(_id = BSONObjectID.generate, id = UUID.randomUUID().toString, display_name = "Penguins")
+  private val anotherTag = Tag(_id =  BSONObjectID.generate, id = UUID.randomUUID().toString, display_name = "Airport")
 
   val request = new MockHttpServletRequest()
 
@@ -46,25 +34,35 @@ class TagCombinerModelBuilderTest {
 
   @Before
   def setup {
-    when(tagDAO.loadTagsByParent(parentId)).thenReturn(Future.successful(List.empty))
   }
 
   @Test
   @throws(classOf[Exception])
-  def isNotValidIfNotTagsAreOnTheRequest {
+  def isNotValidIfNotTagsAreOnTheRequest(): Unit = {
     assertFalse(modelBuilder.isValid(request))
   }
 
   @Test
-  def isNotValidIsOneTagIsOnTheRequest {
+  def isNotValidIsOneTagIsOnTheRequest(): Unit = {
     request.setAttribute("tags", Seq(tag))
     assertFalse(modelBuilder.isValid(request))
   }
 
   @Test
-  def isValidIfTwoTagsIsOnTheRequest {
+  def isValidIfTwoTagsIsOnTheRequest(): Unit = {
     request.setAttribute("tags", Seq(tag, tag))
     assertTrue(modelBuilder.isValid(request))
+  }
+
+  @Test
+  def mainContentIsTagCombinerNewsitem(): Unit = {
+    request.setAttribute("tags", Seq(tag, anotherTag))
+    val tagCombinerNewsitems = (Seq[FrontendResource](FrontendNewsitem(id = "123"), FrontendNewsitem(id = "456")), 2L)
+    when(contentRetrievalService.getTaggedNewsitems(Set(tag, anotherTag), 0, 30, None)).thenReturn(Future.successful(tagCombinerNewsitems))
+
+    val mv = Await.result(modelBuilder.populateContentModel(request), TenSeconds).get
+
+    assertEquals(tagCombinerNewsitems._1, mv.getModel.get("main_content"))
   }
 
 }
