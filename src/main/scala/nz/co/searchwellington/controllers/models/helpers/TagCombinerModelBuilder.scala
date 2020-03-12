fccb07e8b3f69e061ddb5162164cc68d3c7c5a42
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-03-12 22:43:52 +0000

    Futures.

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/TagCombinerModelBuilder.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/TagCombinerModelBuilder.scala
@@ -9,20 +9,25 @@ import nz.co.searchwellington.controllers.{RelatedTagsService, RssUrlBuilder}
 import nz.co.searchwellington.model.{Resource, Tag, User}
 import nz.co.searchwellington.repositories.ContentRetrievalService
 import nz.co.searchwellington.urls.UrlBuilder
+import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.ModelAndView
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 
 @Component class TagCombinerModelBuilder @Autowired()(contentRetrievalService: ContentRetrievalService, rssUrlBuilder: RssUrlBuilder, urlBuilder: UrlBuilder,
                                                       relatedTagsService: RelatedTagsService, commonAttributesModelBuilder: CommonAttributesModelBuilder)
   extends ModelBuilder with CommonSizes with Pagination with ReasonableWaits {
 
+  private val logger = Logger.getLogger(classOf[TagCombinerModelBuilder])
+
   def isValid(request: HttpServletRequest): Boolean = {
     val tags = request.getAttribute("tags").asInstanceOf[Seq[Tag]]
+    logger.info("Is valid tags: " + tags)
     val isTagCombinerPage = tags != null && tags.size == 2
+    logger.info("Is valid: " + isTagCombinerPage)
     isTagCombinerPage
   }
 
@@ -71,21 +76,27 @@ import scala.concurrent.{Await, Future}
   }
 
   def populateExtraModelContent(request: HttpServletRequest, mv: ModelAndView, loggedInUser: User): Future[ModelAndView] = {
-    for {
-      latestWebsites <- contentRetrievalService.getLatestWebsites(5, loggedInUser = Option(loggedInUser))
-    } yield {
-      val tags = request.getAttribute("tags").asInstanceOf[Seq[Tag]]
-      if (tags.nonEmpty) {
-        val tag = tags.head
+    val tags = request.getAttribute("tags").asInstanceOf[Seq[Tag]]
+    if (tags.nonEmpty) {
+      val tag = tags.head
+      val eventualTaggedWebsites = contentRetrievalService.getTaggedWebsites(tag, MAX_WEBSITES, loggedInUser = Option(loggedInUser))
+      val eventualLatestWebsites = contentRetrievalService.getLatestWebsites(5, loggedInUser = Option(loggedInUser))
+      for {
+        taggedWebsites <- eventualTaggedWebsites
+        latestWebsites <- eventualLatestWebsites
+      } yield {
+
         mv.addObject("related_tags", relatedTagsService.getRelatedTagsForTag(tag, 8, Option(loggedInUser)))
         import scala.collection.JavaConverters._
         mv.addObject("latest_news", latestWebsites.asJava)
-        val taggedWebsites = contentRetrievalService.getTaggedWebsites(tag, MAX_WEBSITES, loggedInUser = Option(loggedInUser))
-        mv.addObject("websites", taggedWebsites)
+        mv.addObject("websites", eventualTaggedWebsites)
       }
       mv
+    } else {
+      Future.successful(mv)
     }
   }
+
 
   def getViewName(mv: ModelAndView): String = {
     val taggedNewsitemsCount = mv.getModel.get("main_content_total").asInstanceOf[Long]
