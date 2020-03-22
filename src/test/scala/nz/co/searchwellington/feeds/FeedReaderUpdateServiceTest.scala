4a84abe6e91ac8b6bca3c389ad6450e439b594fe
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-03-22 11:53:50 +0000

    Improve test; add some taggings.

--- src/test/scala/nz/co/searchwellington/feeds/FeedReaderUpdateServiceTest.scala
+++ src/test/scala/nz/co/searchwellington/feeds/FeedReaderUpdateServiceTest.scala
@@ -2,7 +2,8 @@ package nz.co.searchwellington.feeds
 
 import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.feeds.reading.whakaoko.model.FeedItem
-import nz.co.searchwellington.model.{Feed, Newsitem, User}
+import nz.co.searchwellington.model.taggingvotes.HandTagging
+import nz.co.searchwellington.model.{Feed, Newsitem, Tag, Tagging, User}
 import nz.co.searchwellington.modification.ContentUpdateService
 import nz.co.searchwellington.queues.LinkCheckerQueue
 import nz.co.searchwellington.tagging.AutoTaggingService
@@ -10,6 +11,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito.{mock, when}
 
+import scala.collection.immutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
 
@@ -25,13 +27,28 @@ class FeedReaderUpdateServiceTest extends ReasonableWaits {
   @Test
   def acceptedFeedItemsShouldBeAutotaggedAndSaved(): Unit = {
     val feedReaderUser = User()
+    val autoTagUser = User()
+
     val feednewsitem = FeedItem(id = "123", url = "http://localhost/123", subscriptionId = "123")
-    val feed: Feed = Feed()
+    val feed = Feed()
+
+    val atag = Tag(id = "atag")
+    val anotherTag = Tag(id = "anothertag")
 
     val acceptedNewsitem = Newsitem()
+    val autoTaggings = Seq(
+      HandTagging(tag = atag, user = autoTagUser),
+      HandTagging(tag = anotherTag, user = autoTagUser)
+    )
+
     when(feedItemAcceptor.acceptFeedItem(feedReaderUser, (feednewsitem, feed))).thenReturn(acceptedNewsitem)
-    when(autoTaggingService.autotag(acceptedNewsitem.copy(held = false))).thenReturn(Future.successful(List.empty))
-    when(contentUpdateService.create(acceptedNewsitem.copy(held = false))).thenReturn(Future.successful(acceptedNewsitem))
+    when(autoTaggingService.autotag(acceptedNewsitem.copy(held = false))).thenReturn(Future.successful(autoTaggings))
+    when(contentUpdateService.create(acceptedNewsitem.copy(held = false,
+      resource_tags = Seq(
+        Tagging(tag_id = atag._id, user_id = autoTagUser._id),
+        Tagging(tag_id = anotherTag._id, user_id = autoTagUser._id),
+      )
+    ))).thenReturn(Future.successful(acceptedNewsitem))
 
     val created = Await.result(feedReaderUpdateService.acceptFeeditem(feedReaderUser, feednewsitem, feed), TenSeconds)
 
