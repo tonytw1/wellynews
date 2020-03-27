3f9695d3006bf99f7ec5c5403f1766a2d9c8ebe0
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-03-27 00:10:47 +0000

    WIP content function for publisher month.

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/PublisherMonthModelBuilder.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/PublisherMonthModelBuilder.scala
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest
 import nz.co.searchwellington.controllers.models.ModelBuilder
 import nz.co.searchwellington.model.{User, Website}
 import nz.co.searchwellington.repositories.ContentRetrievalService
+import org.joda.time.Interval
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.servlet.ModelAndView
 
@@ -13,21 +14,38 @@ class PublisherMonthModelBuilder @Autowired()(val contentRetrievalService: Conte
 
   override def isValid(request: HttpServletRequest): Boolean = {
     Option(request.getAttribute("publisher").asInstanceOf[Website]).flatMap { publisher =>
-      publisher.url_words.flatMap { publisherUrlWords =>
-        val path = request.getContextPath
-        if (path.startsWith("/" + publisherUrlWords)) {
-          val last = path.split("/").last
-          parseYearMonth(last)
-        } else {
-          None
-        }
-      }
+      parseMonth(publisher, request.getContextPath)
     }.nonEmpty
   }
 
-  override def populateContentModel(request: HttpServletRequest, loggedInUser: User): Future[Option[ModelAndView]] = ???
+  override def populateContentModel(request: HttpServletRequest, loggedInUser: User): Future[Option[ModelAndView]] = {
+    Option(request.getAttribute("publisher").asInstanceOf[Website]).map { publisher =>
+
+      parseMonth(publisher, request.getContextPath).map { month =>
+        Future.successful(None)
+
+      }.getOrElse {
+        Future.successful(None)
+      }
+
+    }.getOrElse {
+      Future.successful(None)
+    }
+  }
 
   override def populateExtraModelContent(request: HttpServletRequest, mv: ModelAndView, loggedInUser: User): Future[ModelAndView] = ???
 
   override def getViewName(mv: ModelAndView): String = ???
+
+  private def parseMonth(publisher: Website, path: String): Option[Interval] = {
+    publisher.url_words.flatMap { publisherUrlWords =>
+      if (path.startsWith("/" + publisherUrlWords)) {
+        val last = path.split("/").last
+        parseYearMonth(last)
+      } else {
+        None
+      }
+    }
+  }
+
 }
