5486d3ff5fe4dea07ae86e2db427721d47642700
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-04-26 20:52:25 +0100

    Pagination links happy case test.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/PaginationTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/PaginationTest.scala
@@ -1,14 +1,31 @@
 package nz.co.searchwellington.controllers.models.helpers
 
 import nz.co.searchwellington.model.PaginationLink
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.springframework.web.servlet.ModelAndView
 
 class PaginationTest extends Pagination {
 
   @Test
-  def shouldOmitPageLinksIfOnlyOnePageOfMainCotent() {
+  def shouldProvidePaginationLinksForMainContent() {
+    val mv = new ModelAndView()
+
+    populatePagination(mv, 0, 100, 30, dummyLinkBuilder)
+
+    import scala.collection.JavaConverters._
+    val pageLinks = mv.getModel.get("page_links").asInstanceOf[java.util.List[PaginationLink]].asScala
+
+    assertFalse(pageLinks.isEmpty)
+    assertEquals(4, pageLinks.size)
+    assertEquals(1, pageLinks.head.page)
+    assertEquals("/something?page=1", pageLinks.head.url)
+  }
+
+  @Test
+  def shouldOmitPageLinksIfOnlyOnePageOfMainContent() {
     val mv = new ModelAndView()
 
     populatePagination(mv, 0, 10, 30, dummyLinkBuilder)
