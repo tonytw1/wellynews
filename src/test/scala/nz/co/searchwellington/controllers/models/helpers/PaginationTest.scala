a75f530b68761d8822d764ab65933fad6ed6df95
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-04-26 20:42:54 +0100

    WIP Failing test for don't provide pagination links if there is only 1 page.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/PaginationTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/PaginationTest.scala
@@ -1,5 +1,7 @@
 package nz.co.searchwellington.controllers.models.helpers
 
+import nz.co.searchwellington.model.PaginationLink
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.springframework.web.servlet.ModelAndView
 
@@ -11,12 +13,15 @@ class PaginationTest extends Pagination {
 
     populatePagination(mv, 0, 10, 30, dummyLinkBuilder)
 
-    val pageLinks = mv.getModel.get("page_links")
-    fail("Should be empty")
+    import scala.collection.JavaConverters._
+    val pageLinks = mv.getModel.get("page_links").asInstanceOf[java.util.List[PaginationLink]].asScala
+
+    println(pageLinks)
+    assertTrue(pageLinks.isEmpty)
   }
 
   def dummyLinkBuilder(page: Int): String = {
-    "/?page=" + page
+    "/something?page=" + page
   }
 
 }
