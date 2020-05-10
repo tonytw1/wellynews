fcdf2aee60d813f5300fb221923a173d59042d3c
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-05-10 12:37:37 +0100

    Formatting.

--- src/main/scala/nz/co/searchwellington/model/taggingvotes/TaggingVote.scala
+++ src/main/scala/nz/co/searchwellington/model/taggingvotes/TaggingVote.scala
@@ -6,4 +6,8 @@ import nz.co.searchwellington.model.taggingvotes.voters.TaggingVoter
 trait TaggingVote {
   def tag: Tag
   def voter: TaggingVoter
+
+  def getTag: Tag = tag
+  def getVoter: TaggingVoter = voter
+
 }
