6b91aa6eead520af5eeafda594e5dcb04ddb9057
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-05-10 12:22:19 +0100

    Formatting.

--- src/main/scala/nz/co/searchwellington/model/taggingvotes/GeotaggingVote.scala
+++ src/main/scala/nz/co/searchwellington/model/taggingvotes/GeotaggingVote.scala
@@ -4,6 +4,10 @@ import nz.co.searchwellington.model.Geocode
 import nz.co.searchwellington.model.taggingvotes.voters.TaggingVoter
 
 class GeotaggingVote(val geocode: Geocode, val voter: TaggingVoter, val weight: Int) {
+
+  def getGeocode: Geocode = geocode
+  def getVoter: TaggingVoter = voter
+
   override def toString = s"GeotaggingVote($geocode, $voter, $weight)"
 }
 
