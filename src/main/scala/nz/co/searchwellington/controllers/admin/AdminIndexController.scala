04310a10ba8a61b20b6e20a04de726e16616537c
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-06-17 13:39:25 +0100

    Admin only.

--- src/main/scala/nz/co/searchwellington/controllers/admin/AdminIndexController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/admin/AdminIndexController.scala
@@ -1,40 +1,23 @@
 package nz.co.searchwellington.controllers.admin
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
-import nz.co.searchwellington.controllers.LoggedInUserFilter
-import nz.co.searchwellington.feeds.FeedReader
-import nz.co.searchwellington.filters.AdminRequestFilter
-import nz.co.searchwellington.model.{Feed, FeedAcceptancePolicy}
-import nz.co.searchwellington.permissions.EditPermissionService
-import nz.co.searchwellington.urls.UrlBuilder
+import nz.co.searchwellington.controllers.{LoggedInUserFilter, RequiringLoggedInUser}
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
-import org.springframework.web.servlet.view.RedirectView
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
-@Controller class AdminIndexController @Autowired()(requestFilter: AdminRequestFilter, feedReader: FeedReader, urlBuilder: UrlBuilder,
-                                                    editPermissionService: EditPermissionService, loggedInUserFilter: LoggedInUserFilter) {
+@Controller class AdminIndexController @Autowired()(val loggedInUserFilter: LoggedInUserFilter) extends RequiringLoggedInUser {
 
   private val log = Logger.getLogger(classOf[AdminIndexController])
 
   @RequestMapping(Array("/admin"))
   def acceptAllFrom(request: HttpServletRequest, response: HttpServletResponse): ModelAndView = {
-    loggedInUserFilter.getLoggedInUser.map { loggedInUser =>
-      if (loggedInUser.isAdmin) {
-        val mv = new ModelAndView("adminindex")
-        mv.addObject("heading", "Admin index")
-        return mv
-
-      } else {
-        null // TODO 403
-      }
-    }.getOrElse {
-      null // TODO 403
+    def show(): ModelAndView = {
+      new ModelAndView("adminindex").addObject("heading", "Admin index")
     }
+    requiringAdminUser(show)
   }
 
 }
