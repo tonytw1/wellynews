ebeaf14fff2b9b8b7f61d8a50fb3f875e3231ea2
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-07-07 11:57:03 +0100

    Logging

--- src/main/java/nz/co/searchwellington/filters/RequestObjectLoadingFilter.java
+++ src/main/java/nz/co/searchwellington/filters/RequestObjectLoadingFilter.java
@@ -1,6 +1,7 @@
 package nz.co.searchwellington.filters;
 
 import nz.co.searchwellington.controllers.LoggedInUserFilter;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,8 @@ import java.io.IOException;
 
 @Component("requestObjectLoadingFilter")
 public class RequestObjectLoadingFilter implements Filter {
+
+	private final static Logger log = Logger.getLogger(RequestObjectLoadingFilter.class);
 
 	private RequestFilter requestFilter;
 	private LoggedInUserFilter loggedInUserFilter;
@@ -24,6 +27,7 @@ public class RequestObjectLoadingFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.info("Filtering");
 		requestFilter.loadAttributesOntoRequest((HttpServletRequest) request);
 		loggedInUserFilter.loadLoggedInUser((HttpServletRequest) request);
 		chain.doFilter (request, response);
