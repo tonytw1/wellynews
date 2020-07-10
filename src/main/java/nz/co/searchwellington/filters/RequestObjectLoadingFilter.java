bf861371c5e4da7a18241bb94a5646cc560feb7c
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-07-10 10:37:41 +0100

    CORS headers applied from filter chain.

--- src/main/java/nz/co/searchwellington/filters/RequestObjectLoadingFilter.java
+++ src/main/java/nz/co/searchwellington/filters/RequestObjectLoadingFilter.java
@@ -1,44 +1,47 @@
 package nz.co.searchwellington.filters;
 
 import nz.co.searchwellington.controllers.LoggedInUserFilter;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component("requestObjectLoadingFilter")
 public class RequestObjectLoadingFilter implements Filter {
+    
+    private RequestFilter requestFilter;
+    private LoggedInUserFilter loggedInUserFilter;
 
-	private final static Logger log = Logger.getLogger(RequestObjectLoadingFilter.class);
+    public RequestObjectLoadingFilter() {
+    }
 
-	private RequestFilter requestFilter;
-	private LoggedInUserFilter loggedInUserFilter;
-	
-	public RequestObjectLoadingFilter() {
-	}
-	
-	@Autowired
-	public RequestObjectLoadingFilter(RequestFilter requestFilter, LoggedInUserFilter loggedInUserFilter) {	
-		this.requestFilter = requestFilter;
-		this.loggedInUserFilter = loggedInUserFilter;
-	}
+    @Autowired
+    public RequestObjectLoadingFilter(RequestFilter requestFilter, LoggedInUserFilter loggedInUserFilter) {
+        this.requestFilter = requestFilter;
+        this.loggedInUserFilter = loggedInUserFilter;
+    }
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		log.info("Filtering");
-		requestFilter.loadAttributesOntoRequest((HttpServletRequest) request);
-		loggedInUserFilter.loadLoggedInUser((HttpServletRequest) request);
-		chain.doFilter (request, response);
-	}
-	
-	@Override
-	public void destroy() {
-	}
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        requestFilter.loadAttributesOntoRequest((HttpServletRequest) request);
+        loggedInUserFilter.loadLoggedInUser((HttpServletRequest) request);
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {		
-	}
-	
+        // TODO move to seperate filter
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
 }
