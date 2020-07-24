f07fa5548010efb9ad7b4ace180d8659ceefedff
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-07-24 16:58:24 +0100

    This test works fine without mocks; nice.

--- src/test/scala/nz/co/searchwellington/feeds/FeedItemAcceptorTest.scala
+++ src/test/scala/nz/co/searchwellington/feeds/FeedItemAcceptorTest.scala
@@ -1,30 +1,26 @@
 package nz.co.searchwellington.feeds
 
 import nz.co.searchwellington.feeds.whakaoko.model.FeedItem
-import nz.co.searchwellington.model.{Feed, Newsitem, User}
+import nz.co.searchwellington.model.{Feed, User}
 import org.joda.time.DateTime
 import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.Test
-import org.mockito.Mockito.{mock, when}
 import reactivemongo.bson.BSONObjectID
-import uk.co.eelpieconsulting.common.geo.model.Place
 
 class FeedItemAcceptorTest {
-  private val place = mock(classOf[Place])
 
   private val feed = Feed(publisher = Some(BSONObjectID.generate))
-  private val feedReadingUser = User(name = Some("Feed reading feedReadingUser"))
-  private val feeditemToNewsItemSerice = mock(classOf[FeeditemToNewsitemService])
+  private val feedReadingUser = User(name = Some("Feed reading user"))
+  private val feeditemToNewsItemService = new FeeditemToNewsitemService(new PlaceToGeocodeMapper)
 
-  private val service = new FeedItemAcceptor(feeditemToNewsItemSerice)
+  private val feedItemAcceptor = new FeedItemAcceptor(feeditemToNewsItemService)
 
   @Test
   def shouldSetAcceptedTimeWhenAccepting(): Unit = {
-    val feedItem = mock(classOf[FeedItem])
-    when(feeditemToNewsItemSerice.makeNewsitemFromFeedItem(feedItem, feed)).thenReturn(Newsitem())
+    val feedItem = FeedItem(id = "", title = Some("A headline"), url ="",  subscriptionId = "", body = None)
     val before = DateTime.now
 
-    val acceptedNewsitem = service.acceptFeedItem(feedReadingUser, (feedItem, feed))
+    val acceptedNewsitem = feedItemAcceptor.acceptFeedItem(feedReadingUser, (feedItem, feed))
 
     assertTrue(acceptedNewsitem.accepted.nonEmpty)
     assertFalse(acceptedNewsitem.accepted.get.before(before.toDate))
@@ -32,10 +28,9 @@ class FeedItemAcceptorTest {
 
   @Test
   def shouldSetAcceptedByUserAndOwnerWhenAccepting(): Unit = {
-    val feedItem = mock(classOf[FeedItem])
-    when(feeditemToNewsItemSerice.makeNewsitemFromFeedItem(feedItem, feed)).thenReturn(Newsitem())
+    val feedItem = FeedItem(id = "", title = Some("A headline"), url ="",  subscriptionId = "", body = None)
 
-    val acceptedNewsitem = service.acceptFeedItem(feedReadingUser, (feedItem, feed))
+    val acceptedNewsitem = feedItemAcceptor.acceptFeedItem(feedReadingUser, (feedItem, feed))
 
     assertEquals(Some(feedReadingUser._id), acceptedNewsitem.acceptedBy)
     assertEquals(Some(feedReadingUser._id), acceptedNewsitem.owner)
@@ -43,10 +38,9 @@ class FeedItemAcceptorTest {
 
   @Test
   def shouldFlattenLoudHeadlinesWhenAccepting(): Unit = {
-    val feedItemWithLoudCapsHeadline = FeedItem(id = "", title = Some("Headline"), url ="",  subscriptionId = "")
-    when(feeditemToNewsItemSerice.makeNewsitemFromFeedItem(feedItemWithLoudCapsHeadline, feed)).thenReturn(Newsitem(title = Some("HEADLINE")))  // TODO consider using real service here
+    val feedItemWithLoudCapsHeadline = FeedItem(id = "", title = Some("HEADLINE"), url ="",  subscriptionId = "", body = None)
 
-    val accepted = service.acceptFeedItem(feedReadingUser, (feedItemWithLoudCapsHeadline, feed))
+    val accepted = feedItemAcceptor.acceptFeedItem(feedReadingUser, (feedItemWithLoudCapsHeadline, feed))
 
     assertEquals("Headline", accepted.title.get)
   }
