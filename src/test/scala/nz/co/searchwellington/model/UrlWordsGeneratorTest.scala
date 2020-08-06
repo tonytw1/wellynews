a07367be2a52322ef719600405832065de956562
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-06 12:35:26 +0100

    Restore test; note publisher newsitem regression.

--- src/test/scala/nz/co/searchwellington/model/UrlWordsGeneratorTest.scala
+++ src/test/scala/nz/co/searchwellington/model/UrlWordsGeneratorTest.scala
@@ -1,44 +1,30 @@
 package nz.co.searchwellington.model
 
-import java.util.UUID
-
+import org.joda.time.{DateTime, DateTimeZone}
 import org.junit.Assert.assertEquals
-import nz.co.searchwellington.model.frontend.FrontendNewsitem
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone
-import org.junit.Before
 import org.junit.Test
 
 class UrlWordsGeneratorTest {
-  private var urlWordsGenerator: UrlWordsGenerator = null
-  private var newsitem: FrontendNewsitem = null
 
-  @Before def setup {
-    val pubdate = new DateTime(2010, 4, 2, 0, 0, 0, 0, DateTimeZone.UTC).toDate
-    newsitem = FrontendNewsitem(id = UUID.randomUUID().toString, name = "Something happening", date = pubdate)
-    this.urlWordsGenerator = new UrlWordsGenerator
-  }
+  private val april2010 = new DateTime(2010, 4, 2, 12, 0).withZone(DateTimeZone.UTC).toDate
 
-  /*
+  private val urlWordsGenerator = new UrlWordsGenerator
+
   @Test
-  @throws[Exception]
   def shouldProduceCorrectUrlBasedOnPublisherDateAndHeadline {
-    val publisher = new Website(title = Some("Island Bay school"))
-    newsitem.setPublisherName("Island Bay school")
-    assertEquals("/island-bay-school/2010/apr/2/some-thing-happening", urlWordsGenerator.makeUrlForNewsitem(newsitem))
-  }
-  */
+    val publisher = Website(title = Some("Island Bay school"))
+    val newsitemWithPublisher = Newsitem(title = Some("Something happened at the school"), date = Some(april2010), publisher = Some(publisher._id))
 
-  /*
+    assertEquals(Some("/island-bay-school/2010/apr/2/something-happened"), urlWordsGenerator.makeUrlForNewsitem(newsitemWithPublisher))
+  }
+
   @Test
-  @throws[Exception]
   def urlWordshouldBeDateAndHeadlineIfPublisherIsNotSet {
-    assertEquals("/2010/apr/2/some-thing-happening", urlWordsGenerator.makeUrlForNewsitem(newsitem))
+    val newsitemWithNoPublisher = Newsitem(title = Some("Something-happening"), date = Some(april2010))
+    assertEquals(Some("/2010/apr/2/something-happening"), urlWordsGenerator.makeUrlForNewsitem(newsitemWithNoPublisher))
   }
-  */
 
   @Test
-  @throws[Exception]
   def shouldBeNullSafe {
     assertEquals(null, urlWordsGenerator.makeUrlWordsFromName(null))
   }
