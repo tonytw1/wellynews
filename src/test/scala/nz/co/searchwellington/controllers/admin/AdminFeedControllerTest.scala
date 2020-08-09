260bfd86ff4ce8394556aefbf8726b85cef58ddd
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-09 11:31:32 +0100

    Test clean ups.

--- src/test/scala/nz/co/searchwellington/controllers/admin/AdminFeedControllerTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/admin/AdminFeedControllerTest.scala
@@ -5,46 +5,38 @@ import java.util.UUID
 import nz.co.searchwellington.controllers.LoggedInUserFilter
 import nz.co.searchwellington.feeds.FeedReader
 import nz.co.searchwellington.filters.AdminRequestFilter
-import nz.co.searchwellington.model.{Feed, FeedAcceptancePolicy, User}
+import nz.co.searchwellington.model._
 import nz.co.searchwellington.permissions.EditPermissionService
 import nz.co.searchwellington.urls.UrlBuilder
-import org.junit.{Before, Test}
-import org.mockito.{Mock, Mockito, MockitoAnnotations}
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.{mock, when}
 import org.springframework.mock.web.{MockHttpServletRequest, MockHttpServletResponse}
-import reactivemongo.bson.BSONObjectID
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class AdminFeedControllerTest {
-
   private val FEED_ID = UUID.randomUUID().toString
-  private val FEED_OBJECT_ID = BSONObjectID.generate()
-
-  @Mock val requestFilter: AdminRequestFilter = null
-  @Mock val feedReader: FeedReader = null
-  @Mock val urlBuilder: UrlBuilder = null
-  @Mock val permissionService: EditPermissionService = null
-  @Mock val loggedInUserFilter: LoggedInUserFilter = null
-  private var request: MockHttpServletRequest = null
-  private var response: MockHttpServletResponse = null
   private val adminUser = User(id ="273", admin = true)
   private val feed = Feed(id = FEED_ID, title = Some("A feed"))
 
-  @Before def setup(): Unit = {
-    MockitoAnnotations.initMocks(this)
-    request = new MockHttpServletRequest
-    response = new MockHttpServletResponse
-  }
+  private val requestFilter = mock(classOf[AdminRequestFilter])
+  private val feedReader = mock(classOf[FeedReader])
+  private val urlBuilder = new UrlBuilder(new SiteInformation("", "", "", "", ""), new UrlWordsGenerator)
+  private val permissionService: EditPermissionService = mock(classOf[EditPermissionService])
+  private val loggedInUserFilter: LoggedInUserFilter = mock(classOf[LoggedInUserFilter])
+
+  val controller = new AdminFeedController(requestFilter, feedReader, urlBuilder, permissionService, loggedInUserFilter)
 
   @Test
   def manualFeedReaderRunsShouldBeAttributedToTheUserWhoKicksThemOffAndShouldAcceptAllEvenIfNoDateIsGivenOfNotCurrent(): Unit = {
-    Mockito.when(loggedInUserFilter.getLoggedInUser).thenReturn(Some(adminUser))
-    Mockito.when(permissionService.canAcceptAllFrom(feed)).thenReturn(true)
+    when(loggedInUserFilter.getLoggedInUser).thenReturn(Some(adminUser))
+    when(permissionService.canAcceptAllFrom(feed)).thenReturn(true)
 
-    val controller = new AdminFeedController(requestFilter, feedReader, urlBuilder, permissionService, loggedInUserFilter)
+    val request = new MockHttpServletRequest()
     request.setAttribute("feedAttribute", feed)
 
-    controller.acceptAllFrom(request, response)
+    controller.acceptAllFrom(request, new MockHttpServletResponse)
 
     Mockito.verify(feedReader).processFeed(feed, adminUser, FeedAcceptancePolicy.ACCEPT_EVEN_WITHOUT_DATES)
   }
