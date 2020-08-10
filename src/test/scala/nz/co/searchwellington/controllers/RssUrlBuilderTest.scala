b04923042152b44d0eb6a4bff8cd5b86d8150ecc
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-10 16:42:33 +0100

    Test clean up.

--- src/test/scala/nz/co/searchwellington/controllers/RssUrlBuilderTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/RssUrlBuilderTest.scala
@@ -2,29 +2,27 @@ package nz.co.searchwellington.controllers
 
 import java.util.UUID
 
+import nz.co.searchwellington.model.{SiteInformation, Tag}
 import org.junit.Assert.assertEquals
-import nz.co.searchwellington.model.SiteInformation
-import nz.co.searchwellington.model.Tag
-import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
 
 class RssUrlBuilderTest {
-  @Mock val siteInformation: SiteInformation = null
-  val tag = Tag(id = UUID.randomUUID().toString, description = Some("This is a tag about something..."))
 
-  @Before def setup(): Unit = {
-    MockitoAnnotations.initMocks(this)
-    Mockito.when(siteInformation.getAreaname).thenReturn("Somewhere")
-  }
+  private val siteInformation = new SiteInformation(areaname = "Somewhere", url = "https://wellynews.local")
+  private val rssUrlBuilder = new RssUrlBuilder(siteInformation)
 
   @Test
-  @throws[Exception]
   def rssDescriptionForTagShouldBeSetFromTagDescriptionTextIfAvailable(): Unit = {
-    val rssUrlBuilder = new RssUrlBuilder(siteInformation)
+    val tag = Tag(id = UUID.randomUUID().toString, description = Some("This is a tag about something..."))
 
     assertEquals("This is a tag about something...", rssUrlBuilder.getRssDescriptionForTag(tag))
   }
+
+  @Test
+  def tagRssUrlIsBasedOnTagName(): Unit = {
+    val tag = Tag(id = UUID.randomUUID().toString, description = Some("This is a tag about something..."), name = "atag")
+
+    assertEquals("https://wellynews.local/atag/rss", rssUrlBuilder.getRssUrlForTag(tag))
+  }
+
 }
