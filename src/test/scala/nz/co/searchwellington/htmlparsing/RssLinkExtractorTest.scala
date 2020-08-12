34f922953f2db018bee844c522476236847b9fc1
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-12 17:19:13 +0100

    Playing with more Scala like exception handler in RSS link extraction.

--- src/test/scala/nz/co/searchwellington/htmlparsing/RssLinkExtractorTest.scala
+++ src/test/scala/nz/co/searchwellington/htmlparsing/RssLinkExtractorTest.scala
@@ -1,5 +1,6 @@
 package nz.co.searchwellington.htmlparsing
 
+import org.apache.commons.io.IOUtils
 import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.Test
 
@@ -8,13 +9,23 @@ class RssLinkExtractorTest {
   private val rssLinkExtractor = new RssLinkExtractor
 
   @Test
+  def canExtractRssAutodiscoverLinksFromHtmlPage = {
+    val pageWithRssLink = IOUtils.toString(this.getClass.getClassLoader.getResourceAsStream("page-with-rss-autodiscoverable-feed.html"))
+
+    val extractedLinks = rssLinkExtractor.extractLinks(pageWithRssLink)
+
+    assertEquals(1, extractedLinks.size)
+    assertEquals("/news/rss", extractedLinks.head)
+  }
+
+  @Test
   def discoveredLinksShouldBeUnescaped = {
     val htmlWithEncodedAutoDiscoverUrls = "<html>" + "<head>" + "<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Encoded link\" href=\"http://wellington.scoop.co.nz/?feed=rss2&amp;p=34601\">" + "</head>" + "</html>"
 
     val extractedLinks = rssLinkExtractor.extractLinks(htmlWithEncodedAutoDiscoverUrls)
 
     assertEquals(1, extractedLinks.size)
-    assertEquals("http://wellington.scoop.co.nz/?feed=rss2&p=34601", extractedLinks.iterator.next)
+    assertEquals("http://wellington.scoop.co.nz/?feed=rss2&p=34601", extractedLinks.head)
   }
 
   @Test
@@ -25,4 +36,5 @@ class RssLinkExtractorTest {
 
     assertTrue(extractedLinks.isEmpty)
   }
+
 }
