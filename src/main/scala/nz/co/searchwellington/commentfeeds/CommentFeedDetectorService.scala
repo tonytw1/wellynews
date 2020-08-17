fb7608dafe43d6f381d4efae71820e70ed4cbded
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-08-17 16:38:20 +0100

    Migrate to Scala now that we've figured out autowiring by interface.

---
+++ src/main/scala/nz/co/searchwellington/commentfeeds/CommentFeedDetectorService.scala
@@ -0,0 +1,21 @@
+package nz.co.searchwellington.commentfeeds
+
+import nz.co.searchwellington.commentfeeds.detectors.CommentFeedDetector
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component class CommentFeedDetectorService @Autowired()(detectors: Array[CommentFeedDetector]) {
+
+  private val log = Logger.getLogger(classOf[CommentFeedDetectorService])
+
+  log.info("Autowired " + detectors.length + " comment detectors: " + detectors.toSeq.map(_.getClass.getSimpleName).mkString(", "))
+
+  def isCommentFeedUrl(url: String): Boolean = {
+    detectors.find(d => d.isValid(url)).exists { d =>
+      log.debug(d.getClass.getName + " detected comment feed url: " + url)
+      true
+    }
+  }
+
+}
