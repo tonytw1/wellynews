218bc43bad53849fb72c5b16b325cd346d0306e9
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-02-18 22:26:47 +0000

    Test.

---
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/ProfileModelBuilderTest.scala
@@ -0,0 +1,24 @@
+package nz.co.searchwellington.controllers.models.helpers
+
+import nz.co.searchwellington.ReasonableWaits
+import nz.co.searchwellington.repositories.ContentRetrievalService
+import nz.co.searchwellington.repositories.mongo.MongoRepository
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.springframework.mock.web.MockHttpServletRequest
+
+class ProfileModelBuilderTest extends ReasonableWaits {
+  private val contentRetrievalService = mock(classOf[ContentRetrievalService])
+  private val mongoRepository = mock(classOf[MongoRepository])
+
+  val modelBuilder = new ProfileModelBuilder( contentRetrievalService, mongoRepository)
+
+  @Test
+  def usersProfilePathIsValid {
+    val request = new MockHttpServletRequest()
+    request.setRequestURI("/profiles/a-user")
+    assertTrue(modelBuilder.isValid(request))
+  }
+
+}
