b58970cd5ff2ff612a84d10ca2f89eb7557996d2
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-03-12 19:07:31 +0000

    Link checker processor to extract Twitter card images.
    Scala / Mongo 22 fields limit on Feed is been problematic here.

---
+++ src/test/scala/nz/co/searchwellington/linkchecking/TwitterPhotoDetectorTest.scala
@@ -0,0 +1,28 @@
+package nz.co.searchwellington.linkchecking
+
+import nz.co.searchwellington.model.Newsitem
+import org.apache.commons.io.IOUtils
+import org.joda.time.DateTime
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+import scala.concurrent.ExecutionContext
+
+class TwitterPhotoDetectorTest {
+
+  private val twitterPhotoUrl = "https://www.rimutaka-incline-railway.org.nz/sites/default/files/2020-12/20201017-a1328-IMG_6380.JPG"
+  private val pageWithTwitterPhoto = IOUtils.toString(this.getClass.getClassLoader.getResourceAsStream("rimutaka-incline-railway-news.html"))
+
+  private val twitterPhotoDetector = new TwitterPhotoDetector()
+
+  @Test
+  def canDetectTwitterPhotoUrlFromTwitterMetadata(): Unit = {
+    implicit val ec = ExecutionContext.Implicits.global
+
+    val newsitem = Newsitem()
+    twitterPhotoDetector.process(newsitem, pageWithTwitterPhoto, DateTime.now)
+
+    //assertEquals(Some(twitterPhotoUrl), newsitem.image)
+  }
+
+}
