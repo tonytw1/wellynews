a67daa2f1c1d1ebc5ff00bd1d79dfa19e53d270e
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-07-15 23:03:52 +0100

    Geo model classes with equals fix mock test.

--- src/test/java/nz/co/searchwellington/filters/LocationParameterFilterTest.java
+++ src/test/java/nz/co/searchwellington/filters/LocationParameterFilterTest.java
@@ -1,25 +1,16 @@
 package nz.co.searchwellington.filters;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
 import nz.co.searchwellington.geocoding.osm.CachingNominatimGeocodingService;
 import nz.co.searchwellington.geocoding.osm.OsmIdParser;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
-
 import uk.co.eelpieconsulting.common.geo.model.LatLong;
 import uk.co.eelpieconsulting.common.geo.model.OsmId;
 import uk.co.eelpieconsulting.common.geo.model.OsmType;
 import uk.co.eelpieconsulting.common.geo.model.Place;
 
-import com.google.common.collect.Lists;
+import static org.junit.Assert.assertEquals;
 
 public class LocationParameterFilterTest {
 	
@@ -27,17 +18,15 @@ public class LocationParameterFilterTest {
 
 	private CachingNominatimGeocodingService geocodeService = Mockito.mock(CachingNominatimGeocodingService.class);
 
-	private MockHttpServletRequest request;
 	private Place petoneStation = new Place(VALID_LOCATION, new LatLong(1.1, 2.2), new OsmId(123, OsmType.NODE));
 
 	private LocationParameterFilter filter = new LocationParameterFilter(geocodeService, new OsmIdParser());
 
 	@Test
 	public void canResolveOsmIdAsLocation() throws Exception {
+		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setParameter("osm", "123/NODE");
-		System.out.println(petoneStation.getOsmId());
 		Mockito.when(geocodeService.resolveOsmId(petoneStation.getOsmId())).thenReturn(petoneStation);
-
 		filter.filter(request);
 
 		final Place locationAttribute = (Place) request.getAttribute(LocationParameterFilter.LOCATION);
@@ -46,6 +35,7 @@ public class LocationParameterFilterTest {
 
 	@Test
 	public void canResolveLocationSearchRadius() throws Exception {
+		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setParameter("radius", "3");
 		filter.filter(request);		
 		assertEquals(3.0, request.getAttribute(LocationParameterFilter.RADIUS));
@@ -53,6 +43,7 @@ public class LocationParameterFilterTest {
 	
 	@Test
 	public void canResolveAbsoluteLatLongPointAsALocation() throws Exception {
+		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setParameter("latitude", "51.2");
 		request.setParameter("longitude", "-0.1");
 		
