60aac7e98fd673d2f8a9e541fc25f047a2caf1ba
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-02 11:14:39 +0000

    Restore create anon submitting user from old resource edit controller and apply to new websites when no one is logged in.
    Lots of side effect here; the correct place todo this also needs some thought.

--- src/main/scala/nz/co/searchwellington/controllers/NewWebsiteController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/NewWebsiteController.scala
@@ -1,6 +1,5 @@
 package nz.co.searchwellington.controllers
 
-import javax.validation.Valid
 import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.forms.NewWebsite
 import nz.co.searchwellington.model.{UrlWordsGenerator, User, Website}
@@ -8,22 +7,25 @@ import nz.co.searchwellington.modification.ContentUpdateService
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import nz.co.searchwellington.urls.UrlBuilder
 import org.apache.log4j.Logger
+import org.joda.time.DateTime
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.validation.{BindingResult, ObjectError}
 import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping, RequestMethod}
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.view.RedirectView
-import org.joda.time.DateTime
 
-import scala.concurrent.{Await, Future}
+import javax.servlet.http.HttpServletRequest
+import javax.validation.Valid
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 
 @Controller
 class NewWebsiteController @Autowired()(contentUpdateService: ContentUpdateService,
                                         mongoRepository: MongoRepository,
                                         urlWordsGenerator: UrlWordsGenerator, urlBuilder: UrlBuilder,
-                                        loggedInUserFilter: LoggedInUserFilter) extends ReasonableWaits {
+                                        loggedInUserFilter: LoggedInUserFilter,
+                                        anonUserService: AnonUserService) extends ReasonableWaits {
 
   private val log = Logger.getLogger(classOf[NewWebsiteController])
 
@@ -33,28 +35,30 @@ class NewWebsiteController @Autowired()(contentUpdateService: ContentUpdateServi
   }
 
   @RequestMapping(value = Array("/new-website"), method = Array(RequestMethod.POST))
-  def submit(@Valid @ModelAttribute("newWebsite") newWebsite: NewWebsite, result: BindingResult): ModelAndView = {
+  def submit(@Valid @ModelAttribute("newWebsite") newWebsite: NewWebsite, result: BindingResult, request: HttpServletRequest): ModelAndView = {
     if (result.hasErrors) {
       log.warn("New website submission has errors: " + result)
       renderNewWebsiteForm(newWebsite)
 
     } else {
       log.info("Got valid new website submission: " + newWebsite)
-      val owner = loggedInUserFilter.getLoggedInUser
-
       val w = Website(title = Some(newWebsite.getTitle),
         page = newWebsite.getUrl,
-        owner = owner.map(_._id),
         date = Some(DateTime.now.toDate),
-        held = submissionShouldBeHeld(owner)
       )
       val website = w.copy(url_words = urlWordsGenerator.makeUrlWordsFor(w))
 
       val eventualModelAndView = mongoRepository.getWebsiteByUrlwords(website.url_words.get).flatMap { maybeExistingWebsite =>
         maybeExistingWebsite.fold {
-          contentUpdateService.create(website).map { _ =>
-            log.info("Created website: " + website)
-            new ModelAndView(new RedirectView(urlBuilder.getPublisherUrl(website)))
+
+          val submittingUser = loggedInUserFilter.getLoggedInUser.getOrElse {
+            createAndSetAnonUser(request)
+          }
+          val withSubmittingUser = website.copy(owner = Some(submittingUser._id), held = submissionShouldBeHeld(Some(submittingUser)))
+
+          contentUpdateService.create(withSubmittingUser).map { _ =>
+            log.info("Created website: " + withSubmittingUser)
+            new ModelAndView(new RedirectView(urlBuilder.getPublisherUrl(withSubmittingUser)))
           }
         } { existing =>
           log.warn("Found existing website site same url words: " + existing.title)
@@ -75,6 +79,14 @@ class NewWebsiteController @Autowired()(contentUpdateService: ContentUpdateServi
     new ModelAndView("newWebsite").
       addObject("heading", "Adding a website").
       addObject("newWebsite", newWebsite)
+  }
+
+  private def createAndSetAnonUser(request: HttpServletRequest): User = {
+    log.info("Creating new anon user for resource submission")
+    val loggedInUser: User = anonUserService.createAnonUser
+    loggedInUserFilter.setLoggedInUser(request, loggedInUser)
+    loggedInUserFilter.loadLoggedInUser(request)
+    loggedInUser
   }
 
 }
