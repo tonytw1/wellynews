0d269b19f65988e506eaaa7c20c74caf116d0bb4
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-03 15:58:34 +0000

    Non admin users can see there held and broken content.

--- src/main/scala/nz/co/searchwellington/repositories/elasticsearch/ModeratedQueries.scala
+++ src/main/scala/nz/co/searchwellington/repositories/elasticsearch/ModeratedQueries.scala
@@ -1,6 +1,6 @@
 package nz.co.searchwellington.repositories.elasticsearch
 
-import com.sksamuel.elastic4s.ElasticDsl.{must, termQuery}
+import com.sksamuel.elastic4s.ElasticDsl.{must, should, termQuery}
 import com.sksamuel.elastic4s.requests.searches.queries.Query
 import nz.co.searchwellington.controllers.ShowBrokenDecisionService
 import nz.co.searchwellington.model.User
@@ -12,25 +12,20 @@ trait ModeratedQueries extends ElasticFields {
   def withModeration(query: Query, loggedInUser: Option[User]): Query = {
 
     if (!showBrokenDecisionService.shouldShowBroken(loggedInUser)) {
-      /*
-        if (!shouldShowBroken) {
-          val contentIsOk: TermQueryBuilder = QueryBuilders.termQuery(HTTP_STATUS, "200")
-          val contentIsApproved: TermQueryBuilder = QueryBuilders.termQuery(HELD, false)
-          val contentIsPublic = QueryBuilders.boolQuery.must(contentIsOk).must(contentIsApproved)
-          val userCanViewContent = QueryBuilders.boolQuery.minimumNumberShouldMatch(1).should(contentIsPublic)
-          if (loggedInUserFilter.getLoggedInUser != null) {
-            userCanViewContent.should(QueryBuilders.termQuery(OWNER, loggedInUserFilter.getLoggedInUser.getProfilename))
-          }
-          query.must(userCanViewContent)
-        }
-      */
       val contentIsOk = termQuery(HttpStatus, 200)
       val contentIsApproved = termQuery(Held, false)
       val contentIsPublic = must(contentIsOk, contentIsApproved)
-      // val isContentOwner = TODO reimplement
-      must(query, contentIsPublic)
+
+      val moderationClause = loggedInUser.map { user: User =>
+        val isContentOwner = termQuery(Owner, user._id)
+        should(contentIsPublic, isContentOwner).minimumShouldMatch(1)
+      }.getOrElse {
+        contentIsPublic
+      }
+      must(query, moderationClause)
 
     } else {
+      // No moderation required
       query
     }
   }
