c579e809d7ac7306a5fe6f1fc37239a12619a7e0
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-04 14:12:18 +0000

    Clean up; no need to resolve the Whakaoko subscription now; just call throught with the id.

--- src/main/scala/nz/co/searchwellington/feeds/whakaoko/WhakaokoFeedReader.scala
+++ src/main/scala/nz/co/searchwellington/feeds/whakaoko/WhakaokoFeedReader.scala
@@ -18,31 +18,23 @@ import scala.concurrent.{ExecutionContext, Future}
   def fetchFeedItems(feed: Feed)(implicit ec: ExecutionContext): Future[Either[String, (Seq[FeedItem], Long)]] = {
     log.debug("Fetching feed items for feed with url: " + feed.page)
 
-    val eventualMaybeWhakaokoSubscription: Future[Option[Subscription]] = feed.whakaokoSubscription.map { subscripitonId =>
-      whakaokoService.getSubscription(subscripitonId)
-    }.getOrElse{
-      Future.successful(None)
-    }
-
-    eventualMaybeWhakaokoSubscription.flatMap { maybeWhakaokoSubscription =>
-      maybeWhakaokoSubscription.map { subscription =>
-        log.debug("Feed mapped to whakaoko subscription: " + subscription.id)
-        whakaokoService.getSubscriptionFeedItems(subscription.id).map { result =>
-          result.fold(
-            { l =>
-              Left(l)
-            },
-            { subscriptionFeedItems =>
-              Right(subscriptionFeedItems)
-            }
-          )
-        }
-
-      }.getOrElse {
-        log.warn("No whakaoko subscription found for feed: " + feed)
-        Future.successful(Left("No whakaoko subscription found for feed"))
+    feed.whakaokoSubscription.map { subscriptionId =>
+      log.debug("Feed mapped to whakaoko subscription: " + subscriptionId)
+      whakaokoService.getSubscriptionFeedItems(subscriptionId).map { result =>
+        result.fold(
+          { l =>
+            Left(l)
+          },
+          { subscriptionFeedItems =>
+            Right(subscriptionFeedItems)
+          }
+        )
       }
+    }.getOrElse {
+      log.warn("No whakaoko subscription found for feed: " + feed)
+      Future.successful(Left("No whakaoko subscription found for feed"))
     }
+
   }
 
 }
