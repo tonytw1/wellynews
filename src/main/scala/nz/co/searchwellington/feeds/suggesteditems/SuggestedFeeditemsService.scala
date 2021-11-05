88c0419bd4f365f6eb37e89a17fcda962f603662
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-05 11:27:53 +0000

    More for comprehension dressage.

--- src/main/scala/nz/co/searchwellington/feeds/suggesteditems/SuggestedFeeditemsService.scala
+++ src/main/scala/nz/co/searchwellington/feeds/suggesteditems/SuggestedFeeditemsService.scala
@@ -26,9 +26,7 @@ import scala.concurrent.{ExecutionContext, Future}
   private val MaximumChannelPagesToScan = 5
 
   /*
-    Return a list of feed newsitems which an editor may wish to accept after reviewing them.
-    This list should exclude feeds items which are going to be automatically accepted anyway.
-    It should exclude items from ignored feeds.
+    Return a list of suggested feed newsitems which an editor may wish to accept after reviewing them.
     Items which have suppressed URLs should be excluded.
    */
   def getSuggestionFeednewsitems(maxItems: Int, loggedInUser: Option[User])(implicit ec: ExecutionContext): Future[Seq[FrontendResource]] = {
@@ -68,17 +66,14 @@ import scala.concurrent.{ExecutionContext, Future}
       }
     }
 
-    val eventualFrontendResources = for {
-        feeds <- mongoRepository.getAllFeeds()
-        suggestedFeeds = feeds.filter(feed => feed.acceptance == FeedAcceptancePolicy.SUGGEST)
-        suggestedFeedItems <- paginateChannelFeedItems(feeds = suggestedFeeds)
-        eventualFrontendResources <- Future.sequence(suggestedFeedItems.map(r => frontendResourceMapper.mapFrontendResource(r, r.geocode)))
-      } yield {
-        eventualFrontendResources
-      }
-
-    eventualFrontendResources.flatMap { suggestedNewsitems =>
-      feedItemActionDecorator.withFeedItemSpecificActions(suggestedNewsitems, loggedInUser)
+    for {
+      feeds <- mongoRepository.getAllFeeds()
+      suggestedFeeds = feeds.filter(feed => feed.acceptance == FeedAcceptancePolicy.SUGGEST)
+      suggestedFeedItems <- paginateChannelFeedItems(feeds = suggestedFeeds)
+      frontendResources <- Future.sequence(suggestedFeedItems.map(r => frontendResourceMapper.mapFrontendResource(r, r.geocode)))
+      withActions <- feedItemActionDecorator.withFeedItemSpecificActions(frontendResources, loggedInUser)
+    } yield {
+      withActions
     }
   }
 
