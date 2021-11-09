699b9adc2f1811687348de438f5a48a6cb99a057
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-09 21:38:39 +0000

    If a feed url matches the url of an existing newsitem with /feed appended then it is probably that newsitem's comment feed

---
+++ src/main/scala/nz/co/searchwellington/commentfeeds/detectors/ExistingNewsitemCommentFeedDetector.scala
@@ -0,0 +1,37 @@
+package nz.co.searchwellington.commentfeeds.detectors
+
+import nz.co.searchwellington.ReasonableWaits
+import nz.co.searchwellington.model.Newsitem
+import nz.co.searchwellington.repositories.mongo.MongoRepository
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+
+@Component
+class ExistingNewsitemCommentFeedDetector @Autowired()(mongoRepository: MongoRepository)
+  extends CommentFeedDetector with ReasonableWaits {
+
+  private val log = Logger.getLogger(classOf[ExistingNewsitemCommentFeedDetector])
+
+  override def isValid(url: String): Boolean = {
+    // If a feed url matches the url of an existing newsitem with /feed appended
+    // then it is probably that newsitem's comment feed
+    if (url.endsWith("/feed")) {
+      val newsitemUrl = url.dropRight("/feed".length)
+      val maybeResource = Await.result(mongoRepository.getResourceByUrl(newsitemUrl), TenSeconds)
+      maybeResource match {
+        case n: Some[Newsitem] => {
+          log.info(s"Feed url $url appears to be a comment feed for newsitem: " + n)
+          true
+        }
+        case None => false
+        case _ => false
+      }
+    } else {
+      true
+    }
+  }
+}
