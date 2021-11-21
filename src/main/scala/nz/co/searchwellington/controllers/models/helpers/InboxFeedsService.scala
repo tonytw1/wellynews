384bf241921646c59c267d8a5a148588756e4f3a
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-21 18:21:41 +0000

    Push inbox feed list to class for testing.

---
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/InboxFeedsService.scala
@@ -0,0 +1,58 @@
+package nz.co.searchwellington.controllers.models.helpers
+
+import nz.co.searchwellington.ReasonableWaits
+import nz.co.searchwellington.feeds.whakaoko.WhakaokoService
+import nz.co.searchwellington.model.{Feed, FeedAcceptancePolicy}
+import nz.co.searchwellington.model.frontend.FrontendFeed
+import nz.co.searchwellington.model.mappers.FrontendResourceMapper
+import nz.co.searchwellington.repositories.mongo.MongoRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+@Component
+class InboxFeedsService @Autowired()(mongoRepository: MongoRepository, whakaokoService: WhakaokoService, frontendResourceMapper: FrontendResourceMapper)
+  extends ReasonableWaits {
+
+  def getInboxFeeds()(implicit ec: ExecutionContext): Future[Seq[FrontendFeed]] = {
+    val eventualWhakaokoSubscriptions = whakaokoService.getSubscriptions
+    for {
+      allFeeds: Seq[Feed] <- mongoRepository.getAllFeeds
+      suggestOnlyFeeds = allFeeds.filter(_.getAcceptancePolicy == FeedAcceptancePolicy.SUGGEST)
+      whakaokoSubscriptions <- eventualWhakaokoSubscriptions
+      inboxFeeds <- {
+        val subscriptionsById = whakaokoSubscriptions.map { subscription =>
+          (subscription.id, subscription)
+        }.toMap
+
+        val feedsWithSubscriptions = suggestOnlyFeeds.flatMap { feed =>
+          val mayBeSubscription = feed.whakaokoSubscription.map { subscriptionId =>
+            subscriptionsById(subscriptionId)
+          }
+          mayBeSubscription.map { subscription =>
+            Some(feed, subscription)
+          }
+        }.flatten
+
+        // Override the last change field with better information from the whakaoko subscription; TODO this field is really last accepted date or something
+        val frontendFeedsWithAmmendedLatestItemDates = feedsWithSubscriptions.map { feed =>
+          frontendResourceMapper.createFrontendResourceFrom(feed._1).map {
+            case frontendFeed: FrontendFeed =>
+              Some(frontendFeed.copy(lastChanged = feed._2.latestItemDate.map(_.toDate)))
+            case _ =>
+              None
+          }
+        }
+
+        Future.sequence(frontendFeedsWithAmmendedLatestItemDates).map { maybeFrontendFeeds =>
+          maybeFrontendFeeds.flatten.filter(_.latestItemDate != null).sortBy(_.latestItemDate).reverse
+        }
+      }
+    } yield {
+      inboxFeeds
+
+    }
+  }
+
+}
--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/SuggestionsModelBuilder.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/SuggestionsModelBuilder.scala
@@ -6,18 +6,19 @@ import nz.co.searchwellington.feeds.suggesteditems.SuggestedFeeditemsService
 import nz.co.searchwellington.feeds.whakaoko.WhakaokoService
 import nz.co.searchwellington.filters.RequestPath
 import nz.co.searchwellington.model.User
-import nz.co.searchwellington.model.frontend.FrontendFeed
 import nz.co.searchwellington.model.mappers.FrontendResourceMapper
 import nz.co.searchwellington.repositories.ContentRetrievalService
 import nz.co.searchwellington.repositories.mongo.MongoRepository
+import nz.co.searchwellington.signin.SigninController
 import nz.co.searchwellington.urls.UrlBuilder
+import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.ModelAndView
 
 import javax.servlet.http.HttpServletRequest
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 import scala.jdk.CollectionConverters._
 
 @Component class SuggestionsModelBuilder @Autowired()(suggestedFeeditemsService: SuggestedFeeditemsService,
@@ -31,6 +32,7 @@ import scala.jdk.CollectionConverters._
   with ReasonableWaits {
 
   private val MAX_SUGGESTIONS = 50
+  private val log = Logger.getLogger(classOf[SigninController])
 
   def isValid(request: HttpServletRequest): Boolean = {
     RequestPath.getPathFrom(request).matches("^/feeds/inbox(/(rss|json))?$")
@@ -52,42 +54,11 @@ import scala.jdk.CollectionConverters._
 
   def populateExtraModelContent(request: HttpServletRequest, mv: ModelAndView, loggedInUser: Option[User]): Future[ModelAndView] = {
     for {
-      suggestOnlyFeeds <- mongoRepository.getAllFeeds
-      whakaokoSubscriptions <- whakaokoService.getSubscriptions
-
+      inboxFeeds <- new InboxFeedsService(mongoRepository, whakaokoService, frontendResourceMapper).getInboxFeeds()
     } yield {
-      val subscriptionsById = whakaokoSubscriptions.map { subscription =>
-        (subscription.id, subscription)
-      }.toMap
-
-      val feedsWithSubscriptions = suggestOnlyFeeds.flatMap { feed =>
-          val mayBeSubscription = feed.whakaokoSubscription.map { subscriptionId =>
-            subscriptionsById(subscriptionId)
-          }
-          mayBeSubscription.map { subscription =>
-            Some(feed, subscription)
-          }
-      }.flatten
-
-      // Override the last change field with better information from the whakaoko subscription; TODO this field is really last accepted date or something
-      val x: Seq[Future[Option[FrontendFeed]]] = feedsWithSubscriptions.map { feed =>
-        frontendResourceMapper.createFrontendResourceFrom(feed._1).map {
-          case frontendFeed: FrontendFeed =>
-            Some(frontendFeed.copy(lastChanged = feed._2.latestItemDate.map(_.toDate)))
-          case _ =>
-            None
-        }
-      }
-
-      val y: Future[Seq[FrontendFeed]] = Future.sequence(x).map { f: Seq[Option[FrontendFeed]] =>
-        val flatten: Seq[FrontendFeed] = f.flatten
-        flatten.filter(_.latestItemDate != null).sortBy(_.latestItemDate).reverse
-      }
-
-      val sortedFeeds = Await.result(y, TenSeconds)
       mv.addObject("righthand_heading", "Suggest only feeds")
       mv.addObject("righthand_description", "Newsitems from these feeds are not automatically accepted.")
-      mv.addObject("righthand_content", sortedFeeds)
+      mv.addObject("righthand_content", inboxFeeds.asJava)
     }
   }
 
