9cb0028d5fa1ae26bc34d51cca1ac23b6c7fb1bc
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-25 13:08:21 +0000

    Clean up; exists.

--- src/main/scala/nz/co/searchwellington/spam/SpamFilter.scala
+++ src/main/scala/nz/co/searchwellington/spam/SpamFilter.scala
@@ -3,10 +3,11 @@ package nz.co.searchwellington.spam
 import org.springframework.stereotype.Component
 import nz.co.searchwellington.model.Resource
 
-@Component class SpamFilter {
+@Component
+class SpamFilter {
   def isSpam(editResource: Resource): Boolean = {
     val urlIsSpam = editResource.page.contains("rfid")
-    val descriptionsSpam = editResource.description.map(d => d.contains("rfid")).getOrElse(false)
+    val descriptionsSpam = editResource.description.exists(d => d.contains("rfid"))
     urlIsSpam || descriptionsSpam
   }
 }
