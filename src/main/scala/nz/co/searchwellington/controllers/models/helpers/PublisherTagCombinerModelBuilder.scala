55e0738fd03c627e5c7859d2afd091e7dff35a0e
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-12-07 22:42:32 +0000

    Brackets.

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/PublisherTagCombinerModelBuilder.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/PublisherTagCombinerModelBuilder.scala
@@ -45,7 +45,7 @@ import scala.jdk.CollectionConverters._
         addObject("heading", publisher.title.getOrElse("") + " and " + tag.getDisplayName).
         addObject("description", "Items tagged with " + publisher.getTitle + " and " + tag.getDisplayName + ".").
         addObject("link", urlBuilder.fullyQualified(urlBuilder.getPublisherCombinerUrl(publisher, tag))).
-        addObject(("rss_url"), rssUrlBuilder.getRssUrlForPublisherTagCombiner(publisher, tag)).
+        addObject("rss_url", rssUrlBuilder.getRssUrlForPublisherTagCombiner(publisher, tag)).
         addObject(MAIN_CONTENT, publisherTagNewsitems.asJava)
 
       if (publisherTagNewsitems.nonEmpty) {
