3dbc297c1fdd0ba07f04430355db1ce5575eeacd
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-12-27 20:41:57 +0000

    Trying to restore Spring bind support.

--- src/main/java/nz/co/searchwellington/views/VelocityView.java
+++ src/main/java/nz/co/searchwellington/views/VelocityView.java
@@ -3,6 +3,7 @@ package nz.co.searchwellington.views;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.spring.VelocityEngineUtils;
 import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.support.RequestContext;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VelocityView extends VelocityEngineUtils implements View {
+
+    // TODO is this still exists in a Spring abstract class we should work towards extending from it
+    private static final String SPRING_MACRO_REQUEST_CONTEXT_ATTRIBUTE = "springMacroRequestContext";
 
     private final String viewname;
     private final Map<String, Object> attributes;
@@ -26,6 +30,11 @@ public class VelocityView extends VelocityEngineUtils implements View {
         Map<String, Object> combined = new HashMap<>();
         combined.putAll(map);
         combined.putAll(attributes);
+
+        // Expose RequestContext instance for Spring macros.
+        combined.put(SPRING_MACRO_REQUEST_CONTEXT_ATTRIBUTE,
+                new RequestContext(httpServletRequest, httpServletResponse, null, combined));
+
         httpServletResponse.setContentType("text/html;charset=UTF-8");
         mergeTemplate(velocityEngine, viewname, "UTF-8", combined, httpServletResponse.getWriter());
     }
