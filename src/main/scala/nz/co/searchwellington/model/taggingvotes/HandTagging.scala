00ad9825ec118de9becc9cc8f928c68ae6420977
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-01-28 08:08:17 +0000

    Render reason correctly.

--- src/main/scala/nz/co/searchwellington/model/taggingvotes/HandTagging.scala
+++ src/main/scala/nz/co/searchwellington/model/taggingvotes/HandTagging.scala
@@ -3,6 +3,8 @@ package nz.co.searchwellington.model.taggingvotes
 import nz.co.searchwellington.model.{Tag, User}
 
 case class HandTagging(tag: Tag, user: User, reason: Option[String] = None) extends TaggingVote {
-  override def explanation: String = "Hand tagged by " + user.getDisplayName +
-    reason.map( r => " (" + r + ")")
+  override def explanation: String = {
+    "Hand tagged by " + user.getDisplayName +
+      reason.map(r => " (" + r + ")").getOrElse("")
+  }
 }
