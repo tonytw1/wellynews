50438eb795397b9b5f2bc78e90eb18bace5de35c
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-04-04 23:02:44 +0100

    Nudged into failing state.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveMonthsTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveMonthsTest.scala
@@ -11,22 +11,22 @@ class ArchiveMonthsTest extends ArchiveMonths {
   def canFindPreviousAndNextMonthLinks(): Unit = {
     val june = new DateTime(2020, 6, 1, 0, 0, DateTimeZone.UTC)
     val july = june.plusMonths(1)
-    val august = july.plusMonths(1)
+    val september = july.plusMonths(2)
 
     val monthOfJune = new Interval(june, june.plusMonths(1))
     val monthOfJuly = new Interval(july, july.plusMonths(1))
-    val monthOfAugust = new Interval(august, august.plusMonths(1))
+    val monthOfSeptember = new Interval(september, september.plusMonths(1))
 
     val archiveLinks = Seq(
         ArchiveLink(monthOfJune, 1L),
         ArchiveLink(monthOfJuly, 2L),
-        ArchiveLink(monthOfAugust, 3L),
+        ArchiveLink(monthOfSeptember, 3L),
       )
 
     val mv = populateNextAndPreviousLinks(monthOfJuly, archiveLinks)
 
     assertEquals( ArchiveLink(monthOfJune, 1L), mv.get("previous_month"))
-    assertEquals( ArchiveLink(monthOfAugust, 3L), mv.get("next_month"))
+    assertEquals( ArchiveLink(monthOfSeptember, 3L), mv.get("next_month"))
   }
 
 }
