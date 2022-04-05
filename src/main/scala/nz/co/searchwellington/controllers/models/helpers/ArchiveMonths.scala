03c549282b253e661c736b4cfccc07fc53f7ffa8
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-04-05 08:31:31 +0100

    Accomodates gaps.

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveMonths.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveMonths.scala
@@ -9,21 +9,15 @@ trait ArchiveMonths {
   def populateNextAndPreviousLinks(currentMonth: Interval, archiveLinks: Seq[ArchiveLink]): ModelMap = {
     // Given the ordered list of all available archive link months and the current month,
     // populate the previous and next links (if available)
-    val previousMonth = currentMonth.withStart(currentMonth.getStart.minusMonths(1)).withEnd(currentMonth.getStart)
-    val nextMonth = currentMonth.withStart(currentMonth.getStart.plusMonths(1)).withEnd(currentMonth.getStart.plusMonths(2))
-
-    // TODO probably does not work if there are gaps in the sequence.
+    val previous = archiveLinks.filter(_.interval.isBefore(currentMonth)).lastOption
+    val next = archiveLinks.find(_.interval.isAfter(currentMonth))
     val mv = new ModelMap()
-    archiveLinks.find { link =>
-      link.interval == previousMonth
-    }.map { l =>
-      mv.addAttribute("previous_month", l)
-    }
-    archiveLinks.find { link =>
-      link.interval == nextMonth
-    }.map { l =>
-      mv.addAttribute("next_month", l)
-    }
+    previous.foreach ( p =>
+      mv.addAttribute("previous_month", p)
+    )
+    next.foreach ( n =>
+      mv.addAttribute("next_month", n)
+    )
     mv
   }
 
