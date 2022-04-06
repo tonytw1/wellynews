e0f0bd164bed6458981926463fc46161f2e0ecf0
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-04-06 08:58:35 +0100

    Failing test for correct previous and next.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveMonthsTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveMonthsTest.scala
@@ -11,22 +11,28 @@ class ArchiveMonthsTest extends ArchiveMonths {
   def canFindPreviousAndNextMonthLinks(): Unit = {
     val june = new DateTime(2020, 6, 1, 0, 0, DateTimeZone.UTC)
     val july = june.plusMonths(1)
-    val september = july.plusMonths(2)
+    val august = june.plusMonths(2)
+    val november = july.plusMonths(4)
+    val december = july.plusMonths(5)
 
     val monthOfJune = new Interval(june, june.plusMonths(1))
     val monthOfJuly = new Interval(july, july.plusMonths(1))
-    val monthOfSeptember = new Interval(september, september.plusMonths(1))
+    val monthOfAugust = new Interval(july, august.plusMonths(1))
+    val monthOfNovember = new Interval(november, november.plusMonths(1))
+    val monthOfDecember = new Interval(december, december.plusMonths(1))
 
     val archiveLinks = Seq(
-        ArchiveLink(monthOfJune, 1L),
-        ArchiveLink(monthOfJuly, 2L),
-        ArchiveLink(monthOfSeptember, 3L),
-      )
+      ArchiveLink(monthOfJune, 1L),
+      ArchiveLink(monthOfJuly, 2L),
+      ArchiveLink(monthOfAugust, 1L),
+      ArchiveLink(monthOfNovember, 1L),
+      ArchiveLink(monthOfDecember, 3L),
+    )
 
-    val mv = populateNextAndPreviousLinks(monthOfJuly, archiveLinks)
+    val mv = populateNextAndPreviousLinks(monthOfAugust, archiveLinks)
 
-    assertEquals( ArchiveLink(monthOfJune, 1L), mv.get("previous_month"))
-    assertEquals( ArchiveLink(monthOfSeptember, 3L), mv.get("next_month"))
+    assertEquals(ArchiveLink(monthOfJuly, 1L), mv.get("previous_month"))
+    assertEquals(ArchiveLink(monthOfNovember, 1L), mv.get("next_month"))
   }
 
 }
