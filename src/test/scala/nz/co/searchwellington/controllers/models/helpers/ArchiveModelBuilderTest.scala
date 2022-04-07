a533a6deb3f1894487071d54811dca533f201641
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-04-07 08:33:11 +0100

    Clean up; deduplicate.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveModelBuilderTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/ArchiveModelBuilderTest.scala
@@ -2,22 +2,22 @@ package nz.co.searchwellington.controllers.models.helpers
 
 import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.controllers.RssUrlBuilder
-import nz.co.searchwellington.model.{ArchiveLink, SiteInformation}
 import nz.co.searchwellington.model.frontend.FrontendNewsitem
 import nz.co.searchwellington.model.helpers.ArchiveLinksService
+import nz.co.searchwellington.model.{ArchiveLink, SiteInformation}
 import nz.co.searchwellington.repositories.ContentRetrievalService
 import org.joda.time.{DateTime, DateTimeZone, Interval}
 import org.junit.Assert.{assertEquals, assertTrue}
-import org.junit.{Before, Test}
+import org.junit.Test
 import org.mockito.Mockito.{mock, when}
 import org.springframework.mock.web.MockHttpServletRequest
 import uk.co.eelpieconsulting.common.dates.DateFormatter
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
 import scala.jdk.CollectionConverters._
-import scala.concurrent.ExecutionContext.Implicits.global
 
-class ArchiveModelBuilderTest extends ReasonableWaits with ContentFields {
+class ArchiveModelBuilderTest extends ReasonableWaits with ContentFields with TestArchiveLinks {
 
   private val contentRetrievalService = mock(classOf[ContentRetrievalService])
   private val request = new MockHttpServletRequest
@@ -31,18 +31,15 @@ class ArchiveModelBuilderTest extends ReasonableWaits with ContentFields {
   val modelBuilder =  new ArchiveModelBuilder(contentRetrievalService, new ArchiveLinksService(),
     new DateFormatter(DateTimeZone.UTC), new RssUrlBuilder(new SiteInformation()))
 
-  @Before
-  def setup() {
-    request.setRequestURI("/archive/2020-jul")
-  }
-
   @Test
   def isValidForArchiveMonthUrl(): Unit = {
+    request.setRequestURI("/archive/2020-jul")
     assertTrue(modelBuilder.isValid(request))
   }
 
   @Test
   def archivePageMainContentIsTheArchiveMonthNewsitems(): Unit = {
+    request.setRequestURI("/archive/2020-jul")
     val july = new DateTime(2020, 7, 1, 0, 0, DateTimeZone.UTC)
     val monthOfJuly = new Interval(july, july.plusMonths(1))
     when(contentRetrievalService.getNewsitemsForInterval(monthOfJuly, loggedInUser)).thenReturn(Future.successful(monthNewsitems))
@@ -56,6 +53,7 @@ class ArchiveModelBuilderTest extends ReasonableWaits with ContentFields {
 
   @Test
   def extraContentIncludesLinksToArchiveMonths(): Unit = {
+    request.setRequestURI("/archive/2020-jul")
     val july = new DateTime(2020, 7, 1, 0, 0, DateTimeZone.UTC)
     val monthOfJuly = new Interval(july, july.plusMonths(1))
 
@@ -73,25 +71,19 @@ class ArchiveModelBuilderTest extends ReasonableWaits with ContentFields {
 
   @Test
   def shouldIncludePreviousAndNextMonthLinks(): Unit = {
-    val june = new DateTime(2020, 6, 1, 0, 0, DateTimeZone.UTC)
-    val july = june.plusMonths(1)
-    val august = july.plusMonths(1)
+    request.setRequestURI("/archive/2021-feb")
+    val feb = new DateTime(2021, 2, 1, 0, 0, DateTimeZone.UTC)
+    val monthOfFeb = new Interval(feb, feb.plusMonths(1))
 
-    val monthOfJune = new Interval(june, june.plusMonths(1))
-    val monthOfJuly = new Interval(july, july.plusMonths(1))
-    val monthOfAugust = new Interval(august, august.plusMonths(1))
-
-    val archiveLinks = Seq(ArchiveLink(monthOfJune, 1), ArchiveLink(monthOfJuly, 3), ArchiveLink(monthOfAugust, 4))
-
-    when(contentRetrievalService.getArchiveMonths(None)).thenReturn(Future.successful(archiveLinks))
+    when(contentRetrievalService.getArchiveMonths(None)).thenReturn(Future.successful(someArchiveMonths))
     when(contentRetrievalService.getArchiveTypeCounts(None)).thenReturn(Future.successful(Map[String, Long]()))
-    when(contentRetrievalService.getPublishersForInterval(monthOfJuly, None)).thenReturn(Future.successful(Seq.empty))
+    when(contentRetrievalService.getPublishersForInterval(monthOfFeb, None)).thenReturn(Future.successful(Seq.empty))
     when(contentRetrievalService.getLatestNewsitems(maxItems = 5, loggedInUser = None)).thenReturn(Future.successful(Seq.empty))
 
     val extras = Await.result(modelBuilder.populateExtraModelContent(request, None), TenSeconds)
 
-    assertEquals(ArchiveLink(monthOfJune, 1), extras.get("previous_month"))
-    assertEquals(ArchiveLink(monthOfAugust, 4), extras.get("next_month"))
+    assertEquals(someArchiveMonths.head, extras.get("previous_month"))
+    assertEquals(someArchiveMonths.last, extras.get("next_month"))
   }
 
 }
