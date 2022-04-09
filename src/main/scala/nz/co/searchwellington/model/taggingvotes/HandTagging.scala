973f64c3d85c38a6673ea4736a0460bebf3786d1
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-04-09 13:14:27 +0100

    Link to tagging user.

--- src/main/scala/nz/co/searchwellington/model/taggingvotes/HandTagging.scala
+++ src/main/scala/nz/co/searchwellington/model/taggingvotes/HandTagging.scala
@@ -4,8 +4,7 @@ import nz.co.searchwellington.model.{Tag, User}
 
 case class HandTagging(tag: Tag, taggingUser: User, reason: Option[String] = None) extends TaggingVote {
   override def explanation: String = {
-    "Hand tagged by " + taggingUser.getDisplayName +
-      reason.map(r => " (" + r + ")").getOrElse("")
+    "Hand tagged" + reason.map(r => " (" + r + ")").getOrElse("")
   }
 
   def user: Option[User] = Some(taggingUser)
