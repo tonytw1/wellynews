76a6f911be81b180f08cf30fc5245f1177746938
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-05-14 10:48:19 +0100

    Backfill test.

---
+++ src/test/scala/nz/co/searchwellington/commentfeeds/detectors/ExistingNewsitemCommentFeedDetectorTest.scala
@@ -0,0 +1,34 @@
+package nz.co.searchwellington.commentfeeds.detectors
+
+import nz.co.searchwellington.model.Newsitem
+import nz.co.searchwellington.repositories.mongo.MongoRepository
+import org.junit.Assert.{assertFalse, assertTrue}
+import org.junit.Test
+import org.mockito.Mockito.{mock, when}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class ExistingNewsitemCommentFeedDetectorTest {
+
+  private val mongoRepository = mock(classOf[MongoRepository])
+
+  @Test
+  def shouldDetectSlashFeedSuffiOfExistingNewsitemsAsCommentFeed(): Unit = {
+    when(mongoRepository.getResourceByUrl("https://wtmc.org.nz/trip-report/st-arnaud-range-and-the-camel")).thenReturn(Future.successful(Some(Newsitem())))
+
+    val existing = new ExistingNewsitemCommentFeedDetector(mongoRepository)
+
+    assertTrue(existing.isValid("https://wtmc.org.nz/trip-report/st-arnaud-range-and-the-camel/feed/"))
+  }
+
+  @Test
+  def shouldNotObjectToSlashFeedsWithNoExistingNewsitem(): Unit = {
+    when(mongoRepository.getResourceByUrl("https://wtmc.org.nz/trip-report/st-arnaud-range-and-the-camel")).thenReturn(Future.successful(None))
+
+    val existing = new ExistingNewsitemCommentFeedDetector(mongoRepository)
+
+    assertFalse(existing.isValid("https://wtmc.org.nz/trip-report/st-arnaud-range-and-the-camel/feed/"))
+  }
+
+}
