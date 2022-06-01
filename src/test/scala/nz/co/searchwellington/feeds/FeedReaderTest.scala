252492a14d05d52a8f9e849caeeec32c5f4adeef
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-06-01 20:39:32 +0100

    Wish list; tests for feed reader.

--- src/test/scala/nz/co/searchwellington/feeds/FeedReaderTest.scala
+++ src/test/scala/nz/co/searchwellington/feeds/FeedReaderTest.scala
@@ -1,15 +1,20 @@
 package nz.co.searchwellington.feeds
 
+import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.feeds.whakaoko.WhakaokoFeedReader
-import nz.co.searchwellington.model.{Feed, FeedAcceptancePolicy, User}
+import nz.co.searchwellington.feeds.whakaoko.model.FeedItem
+import nz.co.searchwellington.model.{Feed, FeedAcceptancePolicy, Newsitem, Resource, User}
 import nz.co.searchwellington.modification.ContentUpdateService
-import org.junit.Assert.fail
+import org.junit.Assert.{assertEquals, assertTrue, fail}
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.{ArgumentCaptor, Matchers}
+import org.mockito.Mockito.{mock, verify, when}
 
+import java.util.UUID
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class FeedReaderTest {
+class FeedReaderTest extends ReasonableWaits {
 
   private val feedItemAcceptanceDecider = mock(classOf[FeedItemAcceptanceDecider])
   private val contentUpdateService = mock(classOf[ContentUpdateService])
@@ -22,16 +27,23 @@ class FeedReaderTest {
 
   private val feedReader = new FeedReader(feedItemAcceptanceDecider, contentUpdateService, feedReaderUpdateService, whakaokoFeedReader, feeditemToNewsItemService)
 
+
   @Test
   def shouldNotAcceptNewsitemsFromSuggestOnlyFeeds(): Unit = {
-    feedReader.processFeed(suggestedFeed, loggedInUser)
+    val feedItems= Seq(FeedItem(id = UUID.randomUUID().toString, subscriptionId = UUID.randomUUID().toString, url = "http://localhost/1"))
+    when(whakaokoFeedReader.fetchFeedItems(suggestedFeed)).thenReturn(Future.successful(Right((feedItems, feedItems.size))))
+    when(contentUpdateService.update(Matchers.any(classOf[Feed]))(Matchers.any())).thenReturn(Future.successful(true))
 
-    fail()
+    val result = Await.result(feedReader.processFeed(suggestedFeed, loggedInUser), TenSeconds)
+
+    assertEquals(0, result)
+    val updatedFeed = ArgumentCaptor.forClass(classOf[Feed])
+    verify(contentUpdateService).update(updatedFeed.capture)(Matchers.any())
+    assertTrue(updatedFeed.getValue.last_read.nonEmpty)
   }
 
   @Test
   def shouldUpdateLastReadAndLatestItemForSuggestOnlyFeeds(): Unit = {
-
     feedReader.processFeed(suggestedFeed, loggedInUser)
 
     fail()
