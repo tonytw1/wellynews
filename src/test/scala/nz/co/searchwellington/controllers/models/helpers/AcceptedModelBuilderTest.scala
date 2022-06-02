4dd17559e1e7d59b688c08ede143ec8cd342d626
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-06-02 11:10:55 +0100

    Test arounf accepted days. Going to use accepted days as our first detour into java.time.

---
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/AcceptedModelBuilderTest.scala
@@ -0,0 +1,39 @@
+package nz.co.searchwellington.controllers.models.helpers
+
+import nz.co.searchwellington.ReasonableWaits
+import nz.co.searchwellington.controllers.RssUrlBuilder
+import nz.co.searchwellington.model.AcceptedDay
+import nz.co.searchwellington.repositories.ContentRetrievalService
+import nz.co.searchwellington.urls.UrlBuilder
+import org.joda.time.LocalDate
+import org.junit.Assert.{assertEquals, assertTrue}
+import org.junit.Test
+import org.mockito.Mockito.{mock, when}
+import org.springframework.mock.web.MockHttpServletRequest
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+
+class AcceptedModelBuilderTest extends ReasonableWaits with CommonSizes {
+
+  private val contentRetrievalService = mock(classOf[ContentRetrievalService])
+  private val rssUrlBuilder = mock(classOf[RssUrlBuilder])
+  private val urlBuilder = mock(classOf[UrlBuilder])
+  private val commonAttributesModelBuilder = mock(classOf[CommonAttributesModelBuilder])
+
+  private val builder = new AcceptedModelBuilder(contentRetrievalService, rssUrlBuilder, urlBuilder, commonAttributesModelBuilder)
+
+  @Test
+  def shouldIncludeLinkOfAcceptedCountsForRecentDays(): Unit = {
+    when(contentRetrievalService.getLatestNewsitems(maxItems = 5, loggedInUser = None)).thenReturn(Future.successful(Seq.empty))
+    val acceptDaysAggregation = Seq(("2022-06-01", 12L),("2022-05-31", 7L))
+    when(contentRetrievalService.getAcceptedDates(loggedInUser = None)).thenReturn(Future.successful(acceptDaysAggregation))
+
+    val extras = Await.result(builder.populateExtraModelContent(new MockHttpServletRequest(), None), TenSeconds)
+
+    val acceptedDays = extras.get("acceptedDays").asInstanceOf[java.util.List[AcceptedDay]]
+    assertTrue(Option(acceptedDays).nonEmpty)
+    assertEquals(AcceptedDay(new LocalDate(2022, 6, 1), 12), acceptedDays.get(0))
+  }
+
+}
