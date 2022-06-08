66cffc1034f769fa6dbee29ab66d48d54debf8ff
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-06-08 20:31:10 +0100

    Clean up; regex escaping.

--- src/main/scala/nz/co/searchwellington/commentfeeds/detectors/WellingtonScoopCommentFeedDetector.scala
+++ src/main/scala/nz/co/searchwellington/commentfeeds/detectors/WellingtonScoopCommentFeedDetector.scala
@@ -10,10 +10,10 @@ import java.util.regex.Pattern
 class WellingtonScoopCommentFeedDetector extends CommentFeedDetector {
 
   private val commentFeedUrlPattern = {
-    Pattern.compile("""^http://wellington.scoop.co.nz/\?feed=rss2\&p=(\d+)$""")
+    Pattern.compile("^http://wellington.scoop.co.nz/\\?feed=rss2&p=(\\d+)$")
   }
   private val httpsCommentFeedUrlPattern = {
-    Pattern.compile("""^https://wellington.scoop.co.nz/\?feed=rss2\&p=(\d+)$""")
+    Pattern.compile("^https://wellington.scoop.co.nz/\\?feed=rss2&p=(\\d+)$")
   }
 
   override def isValid(url: URL, source: Resource): Boolean = {
