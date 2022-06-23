88817925055927c7d433442618ca47ff051abb90
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-06-23 23:35:26 +0100

    Format local dates.

--- src/main/scala/nz/co/searchwellington/views/DateFormatter.scala
+++ src/main/scala/nz/co/searchwellington/views/DateFormatter.scala
@@ -1,16 +1,15 @@
 package nz.co.searchwellington.views
 
-import org.joda.time.LocalDate
+import java.time.LocalDate
 import org.springframework.stereotype.Component
+
+import java.time.format.DateTimeFormatter
 
 @Component
 class DateFormatter extends uk.co.eelpieconsulting.common.dates.DateFormatter("Europe/London") {
 
-
   def formatLocalDate(localDate: LocalDate): String = {
-
-    localDate.toString("d MMM yyyy")
+    localDate.format(DateTimeFormatter.ofPattern("d MMM yyyy"))
   }
-
 
 }
--- src/test/scala/nz/co/searchwellington/views/DateFormatterTest.scala
+++ src/test/scala/nz/co/searchwellington/views/DateFormatterTest.scala
@@ -1,12 +1,15 @@
 package nz.co.searchwellington.views
 
-import org.joda.time.LocalDate
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
+import java.time.LocalDate
+
 class DateFormatterTest {
+
   @Test
   def shouldFormatLocalDates() = {
-    assertEquals("7 Mar 2022", new DateFormatter().formatLocalDate(new LocalDate(2022, 3, 7)))
+    assertEquals("7 Mar 2022", new DateFormatter().formatLocalDate(LocalDate.of(2022, 3, 7)))
   }
+
 }
