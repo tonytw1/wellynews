31cbf7ad822cb52ef17431c30978119c7012f9a7
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-07-08 18:20:34 +0100

    Introducing initial schemea elements.

--- src/main/scala/nz/co/searchwellington/views/DateFormatter.scala
+++ src/main/scala/nz/co/searchwellington/views/DateFormatter.scala
@@ -1,9 +1,13 @@
 package nz.co.searchwellington.views
 
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+
 import java.time.LocalDate
 import org.springframework.stereotype.Component
 
 import java.time.format.DateTimeFormatter
+import java.util.Date
 
 @Component
 class DateFormatter extends uk.co.eelpieconsulting.common.dates.DateFormatter("Europe/London") {
@@ -12,6 +16,10 @@ class DateFormatter extends uk.co.eelpieconsulting.common.dates.DateFormatter("E
 
   def formatLocalDate(localDate: LocalDate): String = {
     localDate.format(localDateFormatter)
+  }
+
+  def isoDate(date: Date): String = {
+    ISODateTimeFormat.date().print(new DateTime(date))
   }
 
 }
