7a960b44d42698aa55a7c74392ca95322822a108
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-09-06 23:02:36 +0100

    Setting up for restoring last changed.

--- src/test/scala/nz/co/searchwellington/linkchecking/ContentHasChangedProcessorTest.scala
+++ src/test/scala/nz/co/searchwellington/linkchecking/ContentHasChangedProcessorTest.scala
@@ -1,12 +1,28 @@
 package nz.co.searchwellington.linkchecking
 
+import nz.co.searchwellington.model.Watchlist
+import org.joda.time.DateTime
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ContentHasChangedProcessorTest {
 
+  private val snapshotArchive = mock(classOf[InMemorySnapshotArchive])
+
+  private val processor = new ContentHasChangedProcesser(snapshotArchive)
+
   @Test
   def shouldUpdateResourceLastChangedDateWhenChangeIsDetected() {
+    val checkResource = Watchlist()
+
+    processor.process(checkResource, Some("SOME CONTENT"), DateTime.now)
+
+    // TODO assert that
+    // records snapshot for the current content
+    // updates the resource last change is a change is detected
     fail()
   }
 
