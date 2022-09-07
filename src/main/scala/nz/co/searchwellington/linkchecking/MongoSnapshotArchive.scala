44f720a70f1a6bd07f62c26d477d19a005235a4a
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-09-07 10:15:10 +0100

    Autowiring

--- src/main/scala/nz/co/searchwellington/linkchecking/MongoSnapshotArchive.scala
+++ src/main/scala/nz/co/searchwellington/linkchecking/MongoSnapshotArchive.scala
@@ -5,9 +5,11 @@ import nz.co.searchwellington.model.Snapshot
 import nz.co.searchwellington.repositories.mongo.MongoRepository
 import org.joda.time.DateTime
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 
 import scala.concurrent.{Await, ExecutionContext}
 
+@Component
 class MongoSnapshotArchive @Autowired()(mongoRepository: MongoRepository) extends SnapshotArchive with ReasonableWaits {
 
   override def storeSnapshot(url: String, content: String)(implicit ec: ExecutionContext): Unit = {
