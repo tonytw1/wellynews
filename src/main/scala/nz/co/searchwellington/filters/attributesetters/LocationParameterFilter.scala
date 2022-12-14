64dfc97afa2a648f1f9e07df8ff66861ec0af41f
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-12-14 17:44:00 +0000

    Clean up.

--- src/main/scala/nz/co/searchwellington/filters/attributesetters/LocationParameterFilter.scala
+++ src/main/scala/nz/co/searchwellington/filters/attributesetters/LocationParameterFilter.scala
@@ -44,7 +44,7 @@ class LocationParameterFilter @Autowired()(geoCodeService: GeoCodeService, osmId
 
     } else if (!Strings.isNullOrEmpty(request.getParameter("latitude")) && !Strings.isNullOrEmpty(request.getParameter("longitude"))) {
       val latLong = new LatLong(request.getParameter(LATITUDE).toDouble, request.getParameter(LONGITUDE).toDouble)
-      val latLongLabel = latLong.getLatitude() + ", " + latLong.getLongitude();
+      val latLongLabel = latLong.getLatitude + ", " + latLong.getLongitude;
       // TODO - is you wanted to, you could resolve for a name, but don't alter the user supplied lat/long values.
       // TODO lat, long isn't really an address - this should be something like a display method on latLong or the view which gives a sensible output when address is null.
       request.setAttribute(LocationParameterFilter.LOCATION, new Place(latLongLabel, latLong, null));
