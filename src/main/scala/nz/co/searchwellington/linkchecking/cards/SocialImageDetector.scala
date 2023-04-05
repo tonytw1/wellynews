fdfbc1874602a1fa9e32d0bc0dbd3d196e2d9ebc
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-04-05 22:49:52 +0100

    Ignore og:images which are not absolute URLs

--- src/main/scala/nz/co/searchwellington/linkchecking/cards/SocialImageDetector.scala
+++ src/main/scala/nz/co/searchwellington/linkchecking/cards/SocialImageDetector.scala
@@ -5,12 +5,15 @@ import org.apache.commons.logging.LogFactory
 import org.htmlparser.filters.{AndFilter, HasAttributeFilter, NodeClassFilter, OrFilter, TagNameFilter}
 import org.springframework.stereotype.Component
 
+import java.net.URI
 import scala.util.{Failure, Success, Try}
 
 @Component
 class SocialImageDetector {
 
   private val log = LogFactory.getLog(classOf[SocialImageDetector])
+
+  private val validImageUrlSchemes = Set("http", "https")
 
   private val ogImage = new OrFilter(
     new HasAttributeFilter("name", "og:image"),
@@ -26,10 +29,15 @@ class SocialImageDetector {
   def extractSocialImageUrlsFrom(pageContent: String): Option[Seq[String]] = {
     parserFor(pageContent).flatMap { parser =>
       Try {
-        val tags = parser.extractAllNodesThatMatch(metaImageTags).toNodeArray.toSeq.map(_.asInstanceOf[Tag])
-
-        val imageURLs = tags.flatMap(tag => Option(tag.getAttribute("content")))
-        imageURLs
+        val metaTags = parser.extractAllNodesThatMatch(metaImageTags).toNodeArray.toSeq.map(_.asInstanceOf[Tag])
+        for {
+          tag <- metaTags
+          content <- Option(tag.getAttribute("content"))
+          uri <- Try(java.net.URI.create(content)).toOption
+          fullyQualified <- onlyFullQualified(uri)
+        } yield {
+          fullyQualified.toURL.toExternalForm
+        }
       }
 
     } match {
@@ -38,6 +46,14 @@ class SocialImageDetector {
       case Failure(e) =>
         log.warn("Failed to detect social images", e)
         None
+    }
+  }
+
+  private def onlyFullQualified(uri: URI) = {
+    if (validImageUrlSchemes.contains(uri.getScheme)) {
+      Some(uri)
+    } else {
+      None
     }
   }
 
--- src/test/scala/nz/co/searchwellington/linkchecking/cards/SocialImageDetectorTest.scala
+++ src/test/scala/nz/co/searchwellington/linkchecking/cards/SocialImageDetectorTest.scala
@@ -1,6 +1,7 @@
 package nz.co.searchwellington.linkchecking.cards
 
 import org.apache.commons.io.IOUtils
+import org.junit.Assert.assertFalse
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -8,39 +9,37 @@ import java.nio.charset.StandardCharsets
 
 class SocialImageDetectorTest {
 
-  private val pageWithTwitterImage = IOUtils.toString(
-    this.getClass.getClassLoader.getResourceAsStream("rimutaka-incline-railway-news.html"),
-    StandardCharsets.UTF_8.name
-  )
-
-  private val pageWithOpenGraphImage = IOUtils.toString(
-    this.getClass.getClassLoader.getResourceAsStream("page-with-og-image.html"),
-    StandardCharsets.UTF_8.name
-  )
-
-  private val pageWithOpenGraphImageProperty = IOUtils.toString(
-    this.getClass.getClassLoader.getResourceAsStream("page-with-og-image-property.html"),
-    StandardCharsets.UTF_8.name
-  )
-
-  private val twitterPhotoDetector = new SocialImageDetector
+  private val socialImageDetector = new SocialImageDetector
 
   @Test
   def canDetectTwitterImageFromMetaTags(): Unit = {
-    val detected = twitterPhotoDetector.extractSocialImageUrlsFrom(pageWithTwitterImage)
+    val detected = socialImageDetector.extractSocialImageUrlsFrom(loadAsString("rimutaka-incline-railway-news.html"))
     assertEquals(Some("https://www.rimutaka-incline-railway.org.nz/sites/default/files/2020-12/20201017-a1328-IMG_6380.JPG"), detected.flatMap(_.headOption))
   }
 
   @Test
   def canDetectOgImageFromMetaNameTags(): Unit = {
-    val detected = twitterPhotoDetector.extractSocialImageUrlsFrom(pageWithOpenGraphImage)
+    val detected = socialImageDetector.extractSocialImageUrlsFrom(loadAsString("page-with-og-image.html"))
     assertEquals(Some("https://eyeofthefish.org/wp-content/uploads/2023/03/Gerard7.png"), detected.flatMap(_.headOption))
   }
 
   @Test
   def canDetectOgImageFromMetaPropertyTags(): Unit = {
-    val detected = twitterPhotoDetector.extractSocialImageUrlsFrom(pageWithOpenGraphImageProperty)
+    val detected = socialImageDetector.extractSocialImageUrlsFrom(loadAsString("page-with-og-image-property.html"))
     assertEquals(Some("https://www.wellingtonjudo.org.nz/wp-content/uploads/2018/05/tn_DSCF9240.jpg"), detected.flatMap(_.headOption))
+  }
+
+  private def loadAsString(filename: String) = {
+    IOUtils.toString(
+      this.getClass.getClassLoader.getResourceAsStream(filename),
+      StandardCharsets.UTF_8.name
+    )
+  }
+
+  @Test
+  def shouldIgnoreNoneAbsoluteOgImageUrls(): Unit = {
+    val detected = socialImageDetector.extractSocialImageUrlsFrom(loadAsString("page-with-relative-og-image.html"))
+    assertFalse(detected.get.contains("/assets/Uploads/ShareImage/Lionesses-Djane-left-and-Zahra-right.JPG"))
   }
 
 }
