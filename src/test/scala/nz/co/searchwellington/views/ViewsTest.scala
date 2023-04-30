435733d2c4cdef88bc27958ad8774a07d284b63a
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-04-30 09:46:50 +0100

    Document problematic Media Module URL.toURI call which breaks on fragments.

--- src/test/scala/nz/co/searchwellington/views/ViewsTest.scala
+++ src/test/scala/nz/co/searchwellington/views/ViewsTest.scala
@@ -2,6 +2,7 @@ package nz.co.searchwellington.views
 
 import nz.co.searchwellington.model.Tag
 import nz.co.searchwellington.model.frontend.FrontendNewsitem
+import org.apache.http.client.utils.URIBuilder
 import org.apache.velocity.spring.VelocityEngineFactoryBean
 import org.joda.time.DateTime
 import org.junit.jupiter.api.Assertions.{assertEquals, assertTrue}
@@ -13,6 +14,7 @@ import uk.co.eelpieconsulting.common.views.EtagGenerator
 import uk.co.eelpieconsulting.common.views.rss.RssView
 
 import java.io.StringWriter
+import java.net.URL
 import java.util.{Properties, UUID}
 import scala.jdk.CollectionConverters._
 
@@ -54,10 +56,19 @@ class ViewsTest {
   @Test
   def canRenderRssViewsWithURIFragmentsInImageUrl(): Unit = {
     val urlWithFragments = "https://i0.wp.com/guardiansofthebays.org.nz/wp-content/uploads/2022/09/Sunrise-over-Rongotai-and-the-airport.jpg?fit=1200%2C600&#038;ssl=1&#038;w=640"
+    val asUrl = new URL(urlWithFragments)
+
+    // This is the call that media module makes with is problematic.
+    asUrl.toURI
+
+    val builder = new URIBuilder().setScheme(asUrl.getProtocol).
+      setHost(asUrl.getHost).setPath(asUrl.getPath).setQuery(asUrl.getQuery)
+    val asURISafe = builder.build().toURL
+
     val model = new ModelMap()
     model.addAttribute("tag", new Tag(name = "transport", display_name = "Transport"))
     val newsitemWithOddImageURL = FrontendNewsitem(id = UUID.randomUUID().toString, name = s"Newsitem with odd image URI", description = "A test newsitem", publisherName = Some("A publisher"),
-      url = urlWithFragments, twitterImage = urlWithFragments)
+      url = urlWithFragments, twitterImage = asURISafe.toExternalForm)
 
     model.addAttribute("data", Seq(newsitemWithOddImageURL).asJava)
     val rssView: View = new RssView(new EtagGenerator(), "", "", "")
@@ -67,7 +78,7 @@ class ViewsTest {
 
     val responseBody = new String(response.getContentAsByteArray)
     assertTrue(responseBody.contains("<title>Newsitem with odd image URI</title>"))
-    assertTrue(responseBody.contains("media:thumbnail url=\"https://i0.wp.com/guardiansofthebays.org.nz/wp-content/uploads/2022/09/Sunrise-over-Rongotai-and-the-airport.jpg?fit=1200%2C600&#038;ssl=1&#038;w=640\""))
+    assertTrue(responseBody.contains("media:thumbnail url=\"https://i0.wp.com/guardiansofthebays.org.nz/wp-content/uploads/2022/09/Sunrise-over-Rongotai-and-the-airport.jpg?fit=1200%2C600\""))
   }
 
   // TODO can we get this from the actual bean?
