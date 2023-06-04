e0e2574fd8b688b58e4efc578b2ae32241741974
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-06-04 19:59:24 +0100

    Suggest only feeds list can be resolved from elastic.

--- src/main/scala/nz/co/searchwellington/feeds/suggesteditems/SuggestedFeedsService.scala
+++ src/main/scala/nz/co/searchwellington/feeds/suggesteditems/SuggestedFeedsService.scala
@@ -3,33 +3,25 @@ package nz.co.searchwellington.feeds.suggesteditems
 import io.opentelemetry.api.trace.Span
 import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.model.FeedAcceptancePolicy
-import nz.co.searchwellington.model.frontend.FrontendFeed
-import nz.co.searchwellington.model.mappers.FrontendResourceMapper
-import nz.co.searchwellington.repositories.mongo.MongoRepository
+import nz.co.searchwellington.model.frontend.{FrontendFeed, FrontendResource}
+import nz.co.searchwellington.repositories.ContentRetrievalService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Component
-class SuggestedFeedsService @Autowired()(mongoRepository: MongoRepository, frontendResourceMapper: FrontendResourceMapper)
-  extends ReasonableWaits {
+class SuggestedFeedsService @Autowired()(contentRetrievalService: ContentRetrievalService) extends ReasonableWaits {
 
   def getSuggestedFeedsOrderedByLatestFeeditemDate()(implicit ec: ExecutionContext, currentSpan: Span): Future[Seq[FrontendFeed]] = {
-    for {
-      allFeeds <- mongoRepository.getAllFeeds
-      suggestOnlyFeeds = allFeeds.filter(_.getAcceptancePolicy == FeedAcceptancePolicy.SUGGEST)
-      sortedByLatestItemDate <- {
-        val eventualSortedFrontend = Future.sequence(suggestOnlyFeeds.sortBy(_.latestItemDate).reverse.map(frontendResourceMapper.createFrontendResourceFrom(_)))
-        eventualSortedFrontend.map { rs =>
-          rs.flatMap {
-            case f: FrontendFeed => Some(f)
-            case _ => None
-          }
-        }
+    val eventualSuggestOnlyFeeds = contentRetrievalService.getFeeds(loggedInUser = None, acceptancePolicy = Some(FeedAcceptancePolicy.SUGGEST)).map { rs: Seq[FrontendResource] =>
+      rs.flatMap {
+        case f: FrontendFeed => Some(f)
+        case _ => None
       }
-    } yield {
-      sortedByLatestItemDate
+    }
+    eventualSuggestOnlyFeeds.map { rs =>
+      rs.sortBy(_.latestItemDate).reverse
     }
   }
 
