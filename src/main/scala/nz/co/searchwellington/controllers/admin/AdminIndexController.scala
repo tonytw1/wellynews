29f03426666bfe6dcec61acdddca68f0eab06287
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-06-04 12:38:32 +0100

    Unused

--- src/main/scala/nz/co/searchwellington/controllers/admin/AdminIndexController.scala
+++ src/main/scala/nz/co/searchwellington/controllers/admin/AdminIndexController.scala
@@ -4,17 +4,13 @@ import jakarta.servlet.http.HttpServletRequest
 import nz.co.searchwellington.ReasonableWaits
 import nz.co.searchwellington.controllers.{LoggedInUserFilter, RequiringLoggedInUser}
 import nz.co.searchwellington.model.User
-import nz.co.searchwellington.repositories.mongo.MongoRepository
-import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.ModelAndView
 
-@Controller class AdminIndexController @Autowired()(val loggedInUserFilter: LoggedInUserFilter, mongoRepository: MongoRepository)
+@Controller class AdminIndexController @Autowired()(val loggedInUserFilter: LoggedInUserFilter)
   extends RequiringLoggedInUser with ReasonableWaits {
-
-  private val log = LogFactory.getLog(classOf[AdminIndexController])
 
   @GetMapping(Array("/admin"))
   def acceptAllFrom(request: HttpServletRequest): ModelAndView = {
