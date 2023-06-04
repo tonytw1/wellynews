51f9c9416a4d19673365a3ad79f01afd1cc2c1cf
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-06-04 18:10:44 +0100

    Suggested feeds are sorted by the date of the most recent item available in Whakaoko.
    They were been sorted by the Whakaoko.latestItemDate field which is the date of the recent item date on the remote RSS feed;
    not the date of that item's persisted copy in Whakaoko.
    It makes sense to sort my feed.lastItemDate which is populated from the date of the most recent persisted Whakaoko feed item seen by the feed reader.

--- src/main/scala/nz/co/searchwellington/feeds/suggesteditems/SuggestedFeedsService.scala
+++ src/main/scala/nz/co/searchwellington/feeds/suggesteditems/SuggestedFeedsService.scala
@@ -2,68 +2,34 @@ package nz.co.searchwellington.feeds.suggesteditems
 
 import io.opentelemetry.api.trace.Span
 import nz.co.searchwellington.ReasonableWaits
-import nz.co.searchwellington.feeds.whakaoko.WhakaokoService
 import nz.co.searchwellington.model.FeedAcceptancePolicy
 import nz.co.searchwellington.model.frontend.FrontendFeed
 import nz.co.searchwellington.model.mappers.FrontendResourceMapper
 import nz.co.searchwellington.repositories.mongo.MongoRepository
-import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Component
-class SuggestedFeedsService @Autowired()(mongoRepository: MongoRepository, whakaokoService: WhakaokoService, frontendResourceMapper: FrontendResourceMapper)
+class SuggestedFeedsService @Autowired()(mongoRepository: MongoRepository, frontendResourceMapper: FrontendResourceMapper)
   extends ReasonableWaits {
 
-  private val log = LogFactory.getLog(classOf[SuggestedFeedsService])
-
   def getSuggestedFeedsOrderedByLatestFeeditemDate()(implicit ec: ExecutionContext, currentSpan: Span): Future[Seq[FrontendFeed]] = {
-    val eventualMaybeWhakaokoSubscriptions = whakaokoService.getSubscriptions
     for {
       allFeeds <- mongoRepository.getAllFeeds
       suggestOnlyFeeds = allFeeds.filter(_.getAcceptancePolicy == FeedAcceptancePolicy.SUGGEST)
-      maybeWhakaokoSubscriptions <- eventualMaybeWhakaokoSubscriptions
-      whakaokoSubscriptions = maybeWhakaokoSubscriptions.toOption.getOrElse(Seq.empty)
-      inboxFeeds <- {
-        val subscriptionsById = whakaokoSubscriptions.map { subscription =>
-          (subscription.id, subscription)
-        }.toMap
-
-        val feedsWithSubscriptions = suggestOnlyFeeds.flatMap { feed =>
-          for {
-            subscriptionId <- feed.whakaokoSubscription
-            subscription <- subscriptionsById.get(subscriptionId)
-          }  yield {
-            (feed, subscription)
+      sortedByLatestItemDate <- {
+        val eventualSortedFrontend = Future.sequence(suggestOnlyFeeds.sortBy(_.latestItemDate).reverse.map(frontendResourceMapper.createFrontendResourceFrom(_)))
+        eventualSortedFrontend.map { rs =>
+          rs.flatMap {
+            case f: FrontendFeed => Some(f)
+            case _ => None
           }
-        }
-
-        // Override the last change field with better information from the whakaoko subscription; TODO this field is really last accepted date or something
-        val frontendFeedsWithAmendedLatestItemDates = feedsWithSubscriptions.map { feed =>
-          frontendResourceMapper.createFrontendResourceFrom(feed._1).map {
-            case frontendFeed: FrontendFeed =>
-              val subscription = feed._2
-              val fromSubscription = subscription.latestItemDate.map(_.toDate).orNull
-
-              if (fromSubscription != frontendFeed.latestItemDate) {
-                log.info(s"Overriding latest item date for ${frontendFeed.getHeadline} from ${frontendFeed.latestItemDate} to $fromSubscription")
-              }
-
-              Some(frontendFeed.copy(latestItemDate = fromSubscription))
-            case _ =>
-              None
-          }
-        }
-
-        Future.sequence(frontendFeedsWithAmendedLatestItemDates).map { maybeFrontendFeeds =>
-          maybeFrontendFeeds.flatten.filter(_.latestItemDate != null).sortBy(_.latestItemDate).reverse
         }
       }
-
     } yield {
-      inboxFeeds
+      sortedByLatestItemDate
     }
   }
 
