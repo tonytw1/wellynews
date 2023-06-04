82fc2b206abb8b02b4f14f1db223cc23830c58c7
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-06-04 12:39:39 +0100

    Unused

--- src/main/scala/nz/co/searchwellington/controllers/models/helpers/CommonSizes.scala
+++ src/main/scala/nz/co/searchwellington/controllers/models/helpers/CommonSizes.scala
@@ -4,9 +4,6 @@ trait CommonSizes {
 
   val MAX_NEWSITEMS = 30   // TODO near always ping ponged back to the content retrieval service - should live there?
   val MAX_NUMBER_OF_GEOTAGGED_TO_SHOW = 30
-  val MAX_NUMBER_OF_COMMENTED_TO_SHOW = 30
   val MAX_WEBSITES = 500
-
-  val MAX_NUMBER_OF_COMMENTED_TO_SHOW_IN_RHS = 2
 
 }
