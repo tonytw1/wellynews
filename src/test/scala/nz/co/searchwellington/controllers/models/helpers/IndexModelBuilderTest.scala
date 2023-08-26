7feb002f8a36906cbb0865006d84ed541fbd5934
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-08-26 10:56:39 +0100

    Wishlist test. When we manually accept a feed item from the inbox but it's publication date is older than the last item on the index page, we want it to be nudged up so that it can still be picked up by RSS consumers.

--- src/test/scala/nz/co/searchwellington/controllers/models/helpers/IndexModelBuilderTest.scala
+++ src/test/scala/nz/co/searchwellington/controllers/models/helpers/IndexModelBuilderTest.scala
@@ -2,7 +2,7 @@ package nz.co.searchwellington.controllers.models.helpers
 
 import io.opentelemetry.api.trace.Span
 import nz.co.searchwellington.ReasonableWaits
-import nz.co.searchwellington.model.frontend.FrontendNewsitem
+import nz.co.searchwellington.model.frontend.{FrontendNewsitem, FrontendResource}
 import nz.co.searchwellington.model.helpers.ArchiveLinksService
 import nz.co.searchwellington.repositories.ContentRetrievalService
 import nz.co.searchwellington.urls.{RssUrlBuilder, UrlBuilder}
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito.{mock, when}
 import org.springframework.mock.web.MockHttpServletRequest
 
+import java.util
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
 import scala.jdk.CollectionConverters._
@@ -30,6 +31,11 @@ class IndexModelBuilderTest extends ReasonableWaits with ContentFields {
     request
   }
 
+  private val latestNewsitems = {
+    Range.inclusive(1, 30).map { i =>
+      FrontendNewsitem(id = i.toString, name = s"Newsitem title $i", date = Some(DateTime.now.minusHours(i).toDate))
+    }
+  }
 
   private val loggedInUser = None
 
@@ -56,17 +62,24 @@ class IndexModelBuilderTest extends ReasonableWaits with ContentFields {
 
   @Test
   def indexPageMainContentIsTheLatestNewsitems(): Unit = {
-    val latestNewsitems = {
-      Range.inclusive(1, 30).map { i =>
-        FrontendNewsitem(id = i.toString, name = s"Newsitem title $i", date = Some(DateTime.now.minusDays(i).toDate))
-      }
-    }
-
     when(contentRetrievalService.getLatestNewsitems(30, loggedInUser)).thenReturn(Future.successful(latestNewsitems))
 
     val mv = Await.result(modelBuilder.populateContentModel(request), TenSeconds).get
 
     assertEquals(latestNewsitems.asJava, mv.get(MAIN_CONTENT))
+  }
+
+  @Test
+  def recentlyAcceptedItemsLessThanOneWeekOldArePrioritisedSoThatTheyArePickedUpByFeedReaders(): Unit = {
+    val recentlyAcceptedItem = FrontendNewsitem(id = "123", name = s"Recently accepted but older publication date",
+      date = Some(new DateTime(latestNewsitems.last.date.get).minusDays(1).toDate), accepted = Some(DateTime.now.toDate))
+
+    when(contentRetrievalService.getLatestNewsitems(30, loggedInUser)).thenReturn(Future.successful(latestNewsitems))
+
+    val mv = Await.result(modelBuilder.populateContentModel(request), TenSeconds).get
+
+    val mainContent = mv.get(MAIN_CONTENT).asInstanceOf[util.List[FrontendResource]].asScala.toSeq
+    assertTrue(mainContent.contains(recentlyAcceptedItem))
   }
 
 }
