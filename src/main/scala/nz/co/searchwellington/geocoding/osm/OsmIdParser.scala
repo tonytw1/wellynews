6566cc80ed6130098e4f44b715a35e17c3832b6d
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-09-19 09:43:15 +0100

    Linting.

--- src/main/scala/nz/co/searchwellington/geocoding/osm/OsmIdParser.scala
+++ src/main/scala/nz/co/searchwellington/geocoding/osm/OsmIdParser.scala
@@ -10,7 +10,7 @@ import uk.co.eelpieconsulting.common.geo.model.OsmType
     if (split.length == 2) {
       val idString = split(0)
       val typeString = split(1) // TODO validate
-      return Some(new OsmId(idString.toLong, OsmType.valueOf(typeString)))
+      Some(new OsmId(idString.toLong, OsmType.valueOf(typeString)))
     } else {
       None
     }
--- src/test/scala/nz/co/searchwellington/gecoding/osm/OsmIdParserTest.scala
+++ src/test/scala/nz/co/searchwellington/gecoding/osm/OsmIdParserTest.scala
@@ -1,9 +1,9 @@
-package nz.co.searchwellington.gecoding.osm;
+package nz.co.searchwellington.gecoding.osm
 
 import nz.co.searchwellington.geocoding.osm.OsmIdParser
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import uk.co.eelpieconsulting.common.geo.model.OsmType;
+import uk.co.eelpieconsulting.common.geo.model.OsmType
 
 class OsmIdParserTest {
 
@@ -11,24 +11,24 @@ class OsmIdParserTest {
 
   @Test
   def canParseIdSlashTypeStyleOsmIdString() = {
-    val parsedOsmId = parser.parseOsmId("123456/WAY");
+    val parsedOsmId = parser.parseOsmId("123456/WAY")
 
-    assertEquals(123456L, parsedOsmId.get.getId);
-    assertEquals(OsmType.WAY, parsedOsmId.get.getType);
+    assertEquals(123456L, parsedOsmId.get.getId)
+    assertEquals(OsmType.WAY, parsedOsmId.get.getType)
   }
 
   @Test
   def canAccommodateVaryingLengthsOfTheOsmTypes() {
-    val parsedOsmId = parser.parseOsmId("123456/RELATION");
+    val parsedOsmId = parser.parseOsmId("123456/RELATION")
 
-    assertEquals(OsmType.RELATION, parsedOsmId.get.getType);
+    assertEquals(OsmType.RELATION, parsedOsmId.get.getType)
   }
 
   @Test
   def canAccommodateVaryingLengthsOfTheOsmIds() {
-    val parsedOsmId = parser.parseOsmId("123/WAY");
+    val parsedOsmId = parser.parseOsmId("123/WAY")
 
-    assertEquals(123L, parsedOsmId.get.getId);
+    assertEquals(123L, parsedOsmId.get.getId)
   }
 
 }
