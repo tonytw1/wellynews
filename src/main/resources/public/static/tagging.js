43a64ca66af97bcfa8cd1461190b7d4770e6f0b6
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-11-05 12:05:09 +0000

    Tag id.

--- src/main/resources/public/static/tagging.js
+++ src/main/resources/public/static/tagging.js
@@ -12,11 +12,13 @@ $(function() {
 		    $("#tags option").each(function( index ) {
                 var option = $("#tags option")[index];
                 var optionLabel = $(option).text();
+				var selectedTagId = $(option).value();
+				console.log("Selected tag id: " + selectedTagId);
                 if (optionLabel === tagName) {
                     $(option).prop("selected", true);
                     $(option)[0].scrollIntoView();
 					console.log($(option));
-					tagList.append('<li data-tag="' + $(option)[0] + '">' + tagName + "</li>");
+					tagList.append('<li data-tag="' + selectedTagId + '">' + tagName + "</li>");
 				}
             });
             $("#tagName").val("");
