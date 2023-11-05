760ea42d3d58b133bb9cf2eeae308d997c1d0819
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-11-05 11:51:57 +0000

    Debug.

--- src/main/resources/public/static/tagging.js
+++ src/main/resources/public/static/tagging.js
@@ -12,10 +12,10 @@ $(function() {
 		    $("#tags option").each(function( index ) {
                 var option = $("#tags option")[index];
                 var optionLabel = $(option).text();
-                if (optionLabel == tagName) {
+                if (optionLabel === tagName) {
                     $(option).prop("selected", true);
                     $(option)[0].scrollIntoView();
-
+					console.log($(option));
 					tagList.append('<l data-tag="' + $(option)[0] + '">' + tagName + "</li>");
 				}
             });
