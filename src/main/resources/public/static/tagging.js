86e80dc1ca8eb745d47a896688e7e466e4bbd30a
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-11-06 22:06:15 +0000

    jquery so val()

--- src/main/resources/public/static/tagging.js
+++ src/main/resources/public/static/tagging.js
@@ -12,7 +12,7 @@ $(function() {
 		    $("#tags option").each(function( index ) {
                 var option = $("#tags option")[index];
                 var optionLabel = $(option).text();
-				var selectedTagId = $(option).value();
+				var selectedTagId = $(option).val();
 				console.log("Selected tag id: " + selectedTagId);
                 if (optionLabel === tagName) {
                     $(option).prop("selected", true);
